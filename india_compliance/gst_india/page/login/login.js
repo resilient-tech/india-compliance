967dbf1d1a5daf0cf6b4a5c6586b758c3eb00efb
Pruthvi Patel <pruthvipatel145@gmail.com>
2022-02-25 16:28:50 +0530

    wip: auth page

---
+++ india_compliance/gst_india/page/login/login.js
@@ -0,0 +1,25 @@
+frappe.pages["login"].on_page_load = function (wrapper) {
+    new AspLogin(wrapper);
+};
+
+class AspLogin {
+    constructor(wrapper) {
+        this.wrapper = $(wrapper);
+        this.page = frappe.ui.make_app_page({
+            parent: wrapper,
+            title: "",
+            single_column: true,
+        });
+
+        this.main_section = this.wrapper.find(".layout-main-section");
+        this.wrapper.bind("show", () => {
+            this.show();
+        });
+    }
+
+    show() {
+        this.main_section.empty().append(frappe.render_template("login"));
+    }
+
+
+}
