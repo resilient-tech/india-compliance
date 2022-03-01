7debcc956c6a4c502e67e49ddc32ff052a328602
Pruthvi Patel <pruthvipatel145@gmail.com>
2022-03-01 16:56:19 +0530

    feat(ui): loading indicator

--- india_compliance/public/js/india_compliance_account/IndiaComplianceAccountApp.vue
+++ india_compliance/public/js/india_compliance_account/IndiaComplianceAccountApp.vue
@@ -1,24 +1,60 @@
 <template>
   <div class="india-compliance-account">
-    <AccountPage v-if="isAuthenticated" />
-    <AuthPage v-else />
+    <transition name="fade">
+      <div class="loading-container" v-if="isLoading">
+        <h1>India Compliance</h1>
+        <h5>by Resilient Tech</h5>
+        <Loading :radius="40" class="mt-3" />
+      </div>
+      <AccountPage v-else-if="isAuthenticated" />
+      <AuthPage v-else />
+    </transition>
   </div>
 </template>
 
 <script>
 import AccountPage from "./account/AccountPage.vue";
 import AuthPage from "./auth/AuthPage.vue";
+import Loading from "./components/Loading.vue";
+import authService from "./services/AuthService";
 
 export default {
   components: {
     AuthPage,
     AccountPage,
+    Loading,
   },
 
-  computed: {
-    isAuthenticated() {
-      return false;
-    },
+  data() {
+    return {
+      isAuthenticated: false,
+      isLoading: true,
+      account: {},
+    };
+  },
+
+  async mounted() {
+    this.isAuthenticated = await authService.isLoggedIn();
+    this.isLoading = false;
   },
 };
 </script>
+
+<style scoped>
+.loading-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: calc(100vh - 60px);
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
