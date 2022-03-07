fe20d009d2a0e24b267fef2243ddf6733e0856d8
vorasmit <mailsmitvora@gmail.com>
2022-03-07 17:20:08 +0530

    feat: added setting to validate hsn code (#16)
    * feat: added gst_setting to validate hsn code
    
    * chore: remove console log and print
    
    * fix: minor-errors
    
    * feat: highlight_gst_category, improve custom_fields for gst_category
    
    * feat: validate hsn in item. make gst_category hidden
    
    * fix: multiple issues
    
    * fix: remove unused import
    
    * fix: remove testing code
    
    * fix: remove unnecessary newline
    
    Co-authored-by: Sagar Vora <sagar@resilient.tech>

---
+++ india_compliance/gst_india/overrides/item.py
@@ -0,0 +1,34 @@
+import frappe
+from frappe import _
+
+
+def validate_hsn_code(doc, method=None):
+    # HSN Code is being validated only for sales items
+    if not doc.is_sales_item:
+        return
+
+    gst_settings = frappe.db.get_value(
+        "GST Settings",
+        "GST Settings",
+        ("validate_hsn_code", "min_hsn_digits"),
+        as_dict=True,
+    )
+
+    if not gst_settings.validate_hsn_code:
+        return
+
+    if not doc.gst_hsn_code:
+        frappe.throw(
+            _("HSN/SAC Code is required. Please enter a valid HSN/SAC code.").format(
+                doc.item_name
+            ),
+            frappe.MandatoryError,
+        )
+
+    if len(doc.gst_hsn_code) < int(gst_settings.min_hsn_digits):
+        frappe.throw(
+            _(
+                "HSN/SAC Code should be atleast {0} digits long. Please enter a valid"
+                " HSN/SAC code."
+            ).format(gst_settings.min_hsn_digits)
+        )
