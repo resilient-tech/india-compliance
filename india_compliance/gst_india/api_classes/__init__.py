9ab28a5a5f8b038fb8f82f925ac01f27d75676cf
Sagar Vora <sagar@resilient.tech>
2022-03-17 18:09:55 +0530

    feat: API Classes for GST APIs (#38)
    * refactor: move and updated GST APIs in app
    
    * Update india_compliance/apis/auth_api.py
    
    Co-authored-by: vorasmit <smitvora203@gmail.com>
    
    * refactor: update mask password and relevant changes
    
    * refactor: removed sandbox functionality
    
    * refactor: proper convention for e-Waybill and e-Invoice
    
    * refactor: seperate error codes in each apis
    
    * fix: error codes from auth only
    
    * chore: clean setup.py
    
    * refactor: all apis
    
    * chore: absolute paths
    
    * chore: change variable name
    
    * feat: Impl. E-Invoice API
    
    * chore: rename file
    
    * refactor: improve classes and utils
    
    * fix: set `require_password` flag
    
    * fix: better importing, naming
    
    * fix: consistent comment
    
    * fix: wrong function name
    
    * fix: remove unused property
    
    * chore: remove not used imports
    
    Co-authored-by: Daizy <modidaizy6730@gmail.com>
    Co-authored-by: DaizyModi <54097382+DaizyModi@users.noreply.github.com>
    Co-authored-by: vorasmit <smitvora203@gmail.com>
    Co-authored-by: Pruthvi Patel <pruthvipatel145@gmail.com>

---
+++ india_compliance/gst_india/api_classes/__init__.py
@@ -0,0 +1,4 @@
+from india_compliance.gst_india.api_classes.e_invoice import EInvoiceAPI
+from india_compliance.gst_india.api_classes.e_waybill import EWaybillAPI
+from india_compliance.gst_india.api_classes.public import PublicAPI
+from india_compliance.gst_india.api_classes.returns import GSTR2aAPI, GSTR2bAPI
