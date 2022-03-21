023402f6cf93bee30b4753b72926e5fb07f35cb6
Pruthvi Patel <pruthvipatel145@gmail.com>
2022-03-21 16:42:17 +0530

    refactor: `address.js`

--- india_compliance/gst_india/client_scripts/address.js
+++ india_compliance/gst_india/client_scripts/address.js
@@ -5,20 +5,20 @@ const DOCTYPE = "Address";
 update_invalid_gstin(DOCTYPE);
 
 frappe.ui.form.on(DOCTYPE, {
-    refresh: function (frm) {
-        if (!frm.doc.__islocal || !frm.doc.links || frm.doc.gstin) return;
+    async refresh(frm) {
+        if (!frm.is_new() || !frm.doc.links || frm.doc.gstin) return;
+
         const row = frm.doc.links[0];
         if (!["Customer", "Supplier", "Company"].includes(row.link_doctype)) return;
-        frappe.db
-            .get_value(row.link_doctype, row.link_name, [
-                "gstin",
-                "gst_category",
-            ])
-            .then((r) => {
-                if(!r.message) return;
-                frm.set_value("gstin", r.message.gstin || "");
-                frm.set_value("gst_category", r.message.gst_category || "");
-            });
+
+        const { message } = await frappe.db.get_value(
+            row.link_doctype,
+            row.link_name,
+            ["gstin", "gst_category"]
+        );
+
+        if (!message) return;
+        frm.set_value("gstin", message.gstin || "");
+        frm.set_value("gst_category", message.gst_category || "");
     },
 });
-
