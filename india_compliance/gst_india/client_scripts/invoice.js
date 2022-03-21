301442228a0ee5924774ae1e42fcfe1cdd05e1a9
Pruthvi Patel <pruthvipatel145@gmail.com>
2022-03-21 15:34:15 +0530

    refactor: `invoice.js`

--- india_compliance/gst_india/client_scripts/invoice.js
+++ india_compliance/gst_india/client_scripts/invoice.js
@@ -1,17 +1,16 @@
 function update_export_type(doctype) {
     frappe.ui.form.on(doctype, {
-        gst_category: function (frm) {
-            if (["SEZ", "Overseas"].includes(frm.doc.gst_category)) {
-                frappe.call({
-                    method: "india_compliance.gst_india.overrides.invoice.get_export_type",
-                    callback: function (r) {
-                        if (!r.message) return;
-                        frm.set_value("export_type", r.message);
-                    },
-                });
-            } else {
-                frm.set_value("export_type", "");
+        async gst_category(frm) {
+            if (!["SEZ", "Overseas"].includes(frm.doc.gst_category)) {
+                return frm.set_value("export_type", "");
             }
+
+            const { message } = await frappe.call(
+                "india_compliance.gst_india.overrides.invoice.get_export_type"
+            );
+
+            if (!message) return;
+            frm.set_value("export_type", message);
         },
     });
 }
