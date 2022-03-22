d48157fb5ee9768e2eef532a93e2ab112626a3f0
Pruthvi Patel <pruthvipatel145@gmail.com>
2022-03-22 18:07:24 +0530

    fix: `update_transporter_gstin`

--- india_compliance/gst_india/overrides/supplier.py
+++ india_compliance/gst_india/overrides/supplier.py
@@ -1,5 +1,5 @@
 import frappe
-from frappe import _
+from frappe import _, bold
 
 from india_compliance.gst_india.utils import validate_gstin
 
@@ -11,13 +11,16 @@ def update_transporter_gstin(doc, method=None):
     if not doc.is_transporter:
         return
 
-    if doc.gstin and doc.gstin != doc.gst_transporter_id:
+    if doc.gstin:
+        if doc.gstin != doc.gst_transporter_id:
+            frappe.msgprint(
+                _(
+                    "GSTIN has been updated in GST Transporter ID from {0} to {1} as per default GSTIN for this transporter."
+                ).format(bold(doc.gst_transporter_id), bold(doc.gstin)),
+                alert=True,
+                indicator="yellow",
+            )
         doc.gst_transporter_id = doc.gstin
-        frappe.msgprint(
-            _(
-                "GSTIN has been updated in GST Transporter ID from {0} to {1} as per default GSTIN for this transporter."
-            ).format(frappe.bold(doc.gst_transporter_id), frappe.bold(doc.gstin))
-        )
 
     elif doc.gst_transporter_id:
         validate_gstin(doc.gst_transporter_id, "Registered Regular")
