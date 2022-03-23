9be78aa7b0a39fa49b134f25b2c996bc18e30137
Pruthvi Patel <pruthvipatel145@gmail.com>
2022-03-23 12:37:13 +0530

    fix: remove check for `billing_gstin` and refactor `update_gst_category`

--- india_compliance/patches/post_install/update_gst_category.py
+++ india_compliance/patches/post_install/update_gst_category.py
@@ -2,17 +2,10 @@ import frappe
 
 
 def execute():
-    frappe.db.sql(
-        """UPDATE `tabSales Invoice` SET gst_category = 'Unregistered'
-        WHERE gst_category = 'Registered Regular'
-        AND IFNULL(customer_gstin, '') = ''
-        AND IFNULL(billing_address_gstin,'') = ''
-    """
-    )
-
-    frappe.db.sql(
-        """UPDATE `tabPurchase Invoice` SET gst_category = 'Unregistered'
-        WHERE gst_category = 'Registered Regular'
-        AND IFNULL(supplier_gstin, '') = ''
-    """
-    )
+    for doctype, field in {
+        "Sales Invoice": "customer_gstin",
+        "Purchase Invoice": "supplier_gstin",
+    }.items():
+        frappe.db.set_value(
+            doctype, {field, ("in", (None, ""))}, "gst_category", "Unregistered"
+        )
