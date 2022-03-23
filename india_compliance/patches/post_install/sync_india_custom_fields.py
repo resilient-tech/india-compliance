a72c34ebd093f2e027c09db1b012d4dc510e7dbc
Pruthvi Patel <pruthvipatel145@gmail.com>
2022-03-23 17:06:44 +0530

    fix: only delete `shipping_fields` from purchase invoice

--- india_compliance/patches/post_install/sync_india_custom_fields.py
+++ india_compliance/patches/post_install/sync_india_custom_fields.py
@@ -2,15 +2,12 @@ import frappe
 
 
 def execute():
-    for doctype in ("Sales Invoice", "Delivery Note", "Purchase Invoice"):
-        frappe.db.sql(
-            """DELETE FROM `tabCustom Field`
-                WHERE dt = %s
-                AND fieldname IN ('port_code', 'shipping_bill_number', 'shipping_bill_date')""",
-            doctype,
-        )
+    fields = ("port_code", "shipping_bill_number", "shipping_bill_date")
+    frappe.db.delete(
+        "Custom Field", {"dt": "Purchase Invoice", "fieldname": ("in", fields)}
+    )
 
     frappe.db.sql(
         """UPDATE tabAddress SET gst_state_number=concat("0", gst_state_number)
-            WHERE length(gst_state_number) = 1;"""
+            WHERE length(gst_state_number) = 1"""
     )
