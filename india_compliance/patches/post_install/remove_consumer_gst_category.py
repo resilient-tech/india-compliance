14b3fa680950b301c1ecc37196e9eed51fcbf541
Pruthvi Patel <pruthvipatel145@gmail.com>
2022-03-24 11:43:34 +0530

    fix: remove column check since it's post install patch

--- india_compliance/patches/post_install/remove_consumer_gst_category.py
+++ india_compliance/patches/post_install/remove_consumer_gst_category.py
@@ -3,7 +3,6 @@ import frappe
 
 def execute():
     for doctype in ("Customer", "Supplier", "Address"):
-        if frappe.db.has_column(doctype, "gst_category"):
-            frappe.db.set_value(
-                doctype, {"gst_category": "Consumer"}, "gst_category", "Unregistered"
-            )
+        frappe.db.set_value(
+            doctype, {"gst_category": "Consumer"}, "gst_category", "Unregistered"
+        )
