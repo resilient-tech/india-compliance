2c8cc7d180169f23fd017a534e95da82d057a273
Smit Vora <mailsmitvora@gmail.com>
2022-03-25 10:35:06 +0530

    feat: check e-Waybill applicability and show dashboard comment accordingly.

--- india_compliance/gst_india/client_scripts/sales_invoice.js
+++ india_compliance/gst_india/client_scripts/sales_invoice.js
@@ -27,11 +27,14 @@ frappe.ui.form.on(DOCTYPE, {
 	},
 
 	refresh: async function(frm) {
-		if(frm.doc.docstatus != 1 || frm.is_dirty()
-			|| frm.doc.ewaybill) return;
+		if(frm.doc.docstatus != 1 || frm.is_dirty()	|| frm.doc.ewaybill) return;
 
-		let {message} = await frappe.db.get_value("GST Settings", "GST Settings", ("enable_e_waybill", "api_secret"));
-		if (message.enable_e_waybill != 1 || message.api_secret) return;
+		let {message} = await frappe.db.get_value("GST Settings", "GST Settings", ("enable_e_waybill", "api_secret", "e_waybill_criteria"));
+		if (message.enable_e_waybill != 1 || !is_e_waybill_applicable(frm, message.e_waybill_criteria)) return;
+
+		frm.dashboard.add_comment("e-Waybill is applicable for this invoice and not yet generated or updated.", "yellow");
+
+		if (message.api_secret) return;
 
 		frm.add_custom_button('e-Waybill JSON', () => {
 			open_url_post(frappe.request.url, {
@@ -43,3 +46,12 @@ frappe.ui.form.on(DOCTYPE, {
 	}
 
 });
+
+
+function is_e_waybill_applicable(frm, e_waybill_criteria) {
+	if (frm.doc.base_grand_total < e_waybill_criteria) return false;
+	for (let item of frm.doc.items) {
+		if (!item.gst_hsn_code.startsWith("99")) return true;
+	}
+	return false;
+}
