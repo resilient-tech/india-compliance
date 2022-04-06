27e7fda934baa175fd09ca066c1427efa7f78dcd
Sagar Vora <sagar@resilient.tech>
2022-04-06 15:46:52 +0530

    fix: enqueue at front

--- india_compliance/gst_india/doctype/gst_settings/gst_settings.py
+++ india_compliance/gst_india/doctype/gst_settings/gst_settings.py
@@ -30,7 +30,7 @@ class GSTSettings(Document):
             self.auto_generate_e_waybill = 1
 
     def on_update(self):
-        frappe.enqueue(self.update_custom_fields)
+        frappe.enqueue(self.update_custom_fields, queue="short", at_front=True)
 
         # clear session boot cache
         frappe.cache().delete_keys("bootinfo")
