737618555cc47a3667a8035e63525c0e0b3bcb28
Pruthvi Patel <pruthvipatel145@gmail.com>
2022-04-22 18:46:41 +0530

    fix: delete_doc permission error (#76)

--- india_compliance/patches/post_install/delete_gst_e_invoice_print_format.py
+++ india_compliance/patches/post_install/delete_gst_e_invoice_print_format.py
@@ -2,4 +2,10 @@ import frappe
 
 
 def execute():
-    frappe.delete_doc_if_exists("Print Format", "GST E-Invoice")
+    frappe.delete_doc(
+        "Print Format",
+        "GST E-Invoice",
+        ignore_missing=True,
+        force=True,
+        ignore_permissions=True,
+    )
