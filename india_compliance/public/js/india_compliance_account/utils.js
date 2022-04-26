832785c6c985aeb673cbdbdeb04a7cb51b5e5ae1
Max Solanki <84378369+lapardnemihk1099@users.noreply.github.com>
2022-04-26 15:20:45 +0530

    feat: add max order qty check (#66)
    * chore: adding max order qty check and fix formating
    
    * chore: fixed api formatting and structure

--- india_compliance/public/js/india_compliance_account/utils.js
+++ india_compliance/public/js/india_compliance_account/utils.js
@@ -1,6 +1,3 @@
 export const getReadableNumber = function (num, precision = 2) {
-    return num.toLocaleString(undefined, {
-        minimumFractionDigits: precision,
-        maximumFractionDigits: precision,
-    });
+    return format_number(num, null, precision);
 };
