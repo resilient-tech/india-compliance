3969152c61437406bdd77528c7429d94185bee5b
Smit Vora <mailsmitvora@gmail.com>
2022-06-10 14:15:15 +0530

    fix: set_value if new-field available

--- india_compliance/patches/post_install/update_reverse_charge_and_export_type.py
+++ india_compliance/patches/post_install/update_reverse_charge_and_export_type.py
@@ -4,18 +4,22 @@ DOCTYPES = ("Purchase Invoice", "Sales Invoice")
 
 
 def execute():
-    update_and_process_field("reverse_charge", "Y")
-    update_and_process_field("export_type", "With Payment of Tax")
+    update_and_process_field("reverse_charge", "is_reverse_charge", "Y")
+    update_and_process_field("export_type", "is_export_with_gst", "With Payment of Tax")
 
 
-def update_and_process_field(field, values_to_update):
-    delete_old_fields(field)
+def update_and_process_field(old_field, new_field, values_to_update):
+    delete_old_fields(old_field)
     for doctype in DOCTYPES:
-        if field not in frappe.db.get_table_columns(doctype):
+        if old_field not in frappe.db.get_table_columns(doctype):
             continue
 
-        frappe.db.set_value(doctype, {field: values_to_update}, "is_reverse_charge", 1)
-        frappe.db.sql_ddl("alter table `tab{0}` drop column {1}".format(doctype, field))
+        if new_field in frappe.db.get_table_columns(doctype):
+            frappe.db.set_value(doctype, {old_field: values_to_update}, new_field, 1)
+
+        frappe.db.sql_ddl(
+            "alter table `tab{0}` drop column {1}".format(doctype, old_field)
+        )
 
 
 def delete_old_fields(column):
