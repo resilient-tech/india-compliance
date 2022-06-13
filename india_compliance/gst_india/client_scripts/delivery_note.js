d1401cf833646a0611da8220898009e538b3cbd9
Smit Vora <mailsmitvora@gmail.com>
2022-06-13 19:43:16 +0530

    fix: alert for missing fields in delivery note for e_waybill

--- india_compliance/gst_india/client_scripts/delivery_note.js
+++ india_compliance/gst_india/client_scripts/delivery_note.js
@@ -8,11 +8,25 @@ update_gst_vehicle_type(DOCTYPE);
 setup_e_waybill_actions(DOCTYPE);
 
 frappe.ui.form.on(DOCTYPE, {
-	setup(frm) {
-		frm.set_query('transporter', {
-			filters: {
-				'is_transporter': 1
-			}
-		});
-	}
-})
+    setup(frm) {
+        frm.set_query("transporter", {
+            filters: {
+                is_transporter: 1,
+            },
+        });
+    },
+    validate(frm) {
+        let settings = frappe.boot.gst_settings;
+        if (
+            frm.doc.docstatus ||
+            frm.doc.customer_address ||
+            !(settings.enable_e_waybill && settings.enable_e_waybill_from_dn)
+        )
+            return;
+
+        frappe.show_alert({
+            message: __("Customer Address is required to generate e-Waybill from Delivery Note"),
+            indicator: "yellow",
+        });
+    },
+});
