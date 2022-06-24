2053b3a45e24348e5ddcefe2edfdbf3007cc83d7
Sagar Vora <sagar@resilient.tech>
2022-06-24 13:03:56 +0530

    refactor!: remove HR related and other code to be kept back in ERPNext / other apps

--- india_compliance/income_tax_india/setup.py
+++ india_compliance/income_tax_india/setup.py
@@ -6,35 +6,3 @@ from india_compliance.income_tax_india.constants.custom_fields import CUSTOM_FIE
 
 def after_install():
     create_custom_fields(CUSTOM_FIELDS, update=True)
-    create_gratuity_rule_for_india()
-
-
-def create_gratuity_rule_for_india():
-    if not frappe.db.exists("DocType", "Gratuity Rule"):
-        return
-
-    _create_gratuity_rule_for_india()
-
-
-def _create_gratuity_rule_for_india():
-    if frappe.db.exists("Gratuity Rule", "Indian Standard Gratuity Rule"):
-        return
-
-    rule = frappe.new_doc("Gratuity Rule")
-    rule.update(
-        {
-            "name": "Indian Standard Gratuity Rule",
-            "calculate_gratuity_amount_based_on": "Current Slab",
-            "work_experience_calculation_method": "Round Off Work Experience",
-            "minimum_year_for_gratuity": 5,
-            "gratuity_rule_slabs": [
-                {
-                    "from_year": 0,
-                    "to_year": 0,
-                    "fraction_of_applicable_earnings": 15 / 26,
-                }
-            ],
-        }
-    )
-    rule.flags.ignore_mandatory = True
-    rule.save()
