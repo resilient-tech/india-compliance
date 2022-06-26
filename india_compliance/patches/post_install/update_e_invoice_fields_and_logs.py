f069c8f3b863035cb081bec502a9af26fce3e610
Sagar Vora <sagar@resilient.tech>
2022-06-26 18:03:31 +0530

    fix: remove unnecessary TODO

--- india_compliance/patches/post_install/update_e_invoice_fields_and_logs.py
+++ india_compliance/patches/post_install/update_e_invoice_fields_and_logs.py
@@ -305,9 +305,6 @@ def delete_e_invoice_fields():
     delete_custom_fields(FIELDS_TO_DELETE)
 
 
-# TODO: perhaps these need to be removed in an ERPNext patch?
-
-
 def delete_old_doctypes():
     for doctype in ("E Invoice Settings", "E Invoice User", "E Invoice Request Log"):
         frappe.delete_doc("DocType", doctype, force=True, ignore_permissions=True)
