82b5f709f46fc7e77f741b77eda394429dba00ea
Daizy <modidaizy6730@gmail.com>
2022-06-26 20:50:23 +0530

    fix: indentation changes

--- india_compliance/gst_india/report/gst_itemised_purchase_register/gst_itemised_purchase_register.py
+++ india_compliance/gst_india/report/gst_itemised_purchase_register/gst_itemised_purchase_register.py
@@ -6,61 +6,63 @@ from erpnext.accounts.report.item_wise_purchase_register.item_wise_purchase_regi
 
 
 def execute(filters=None):
-    additional_table_columns = [
-        dict(
-            fieldtype="Data",
-            label="Supplier GSTIN",
-            fieldname="supplier_gstin",
-            width=120,
-        ),
-        dict(
-            fieldtype="Data",
-            label="Company GSTIN",
-            fieldname="company_gstin",
-            width=120,
-        ),
-        dict(
-            fieldtype="Check",
-            label="Is Reverse Charge",
-            fieldname="is_reverse_charge",
-            width=120,
-        ),
-        dict(
-            fieldtype="Data",
-            label="GST Category",
-            fieldname="gst_category",
-            width=120,
-        ),
-        dict(
-            fieldtype="Data",
-            label="E-Commerce GSTIN",
-            fieldname="ecommerce_gstin",
-            width=130,
-        ),
-        dict(fieldtype="Data", label="HSN Code", fieldname="gst_hsn_code", width=120),
-        dict(
-            fieldtype="Data",
-            label="Supplier Invoice No",
-            fieldname="bill_no",
-            width=120,
-        ),
-        dict(
-            fieldtype="Date",
-            label="Supplier Invoice Date",
-            fieldname="bill_date",
-            width=100,
-        ),
-    ]
-
-    additional_query_columns = [
-        "supplier_gstin",
-        "company_gstin",
-        "is_reverse_charge",
-        "gst_category",
-        "ecommerce_gstin",
-        "gst_hsn_code",
-        "bill_no",
-        "bill_date",
-    ]
-
-    return _execute(filters, additional_table_columns, additional_query_columns)
+    return _execute(
+        filters,
+        additional_table_columns=[
+            dict(
+                fieldtype="Data",
+                label="Supplier GSTIN",
+                fieldname="supplier_gstin",
+                width=120,
+            ),
+            dict(
+                fieldtype="Data",
+                label="Company GSTIN",
+                fieldname="company_gstin",
+                width=120,
+            ),
+            dict(
+                fieldtype="Check",
+                label="Is Reverse Charge",
+                fieldname="is_reverse_charge",
+                width=120,
+            ),
+            dict(
+                fieldtype="Data",
+                label="GST Category",
+                fieldname="gst_category",
+                width=120,
+            ),
+            dict(
+                fieldtype="Data",
+                label="E-Commerce GSTIN",
+                fieldname="ecommerce_gstin",
+                width=130,
+            ),
+            dict(
+                fieldtype="Data", label="HSN Code", fieldname="gst_hsn_code", width=120
+            ),
+            dict(
+                fieldtype="Data",
+                label="Supplier Invoice No",
+                fieldname="bill_no",
+                width=120,
+            ),
+            dict(
+                fieldtype="Date",
+                label="Supplier Invoice Date",
+                fieldname="bill_date",
+                width=100,
+            ),
+        ],
+        additional_query_columns=[
+            "supplier_gstin",
+            "company_gstin",
+            "is_reverse_charge",
+            "gst_category",
+            "ecommerce_gstin",
+            "gst_hsn_code",
+            "bill_no",
+            "bill_date",
+        ],
+    )
