b7a5dc24720726046bab4ecf4964e9a10006a1ef
Smit Vora <mailsmitvora@gmail.com>
2022-07-03 12:02:11 +0530

    test: refactor item and taxes

--- india_compliance/gst_india/test_records.py
+++ india_compliance/gst_india/test_records.py
@@ -22,22 +22,12 @@ def create_sales_invoice(**args):
     if args.customer_address:
         si.customer_address = args.customer_address
 
-    si.append(
-        "items",
-        {
-            "item_code": args.item_code or "_Test Trading Goods 1",
-            "qty": args.qty or 1,
-            "uom": args.uom or "Nos",
-            "rate": args.rate or 100,
-            "cost_center": args.cost_center or f"Main - {abbr}",
-        },
-    )
+    append_items(si, args, abbr)
 
     if args.is_in_state:
-        si.append("taxes", get_taxes("CGST", abbr, True))
-        si.append("taxes", get_taxes("SGST", abbr, True))
+        append_taxes(si, ["CGST", "SGST"], abbr, True)
     elif args.is_in_state is False:
-        si.append("taxes", get_taxes("IGST", abbr, True, 18))
+        append_taxes(si, "IGST", abbr, True, 18)
 
     if not args.do_not_save:
         si.insert()
@@ -64,27 +54,17 @@ def create_purchase_invoice(**args):
         }
     )
 
-    pi.append(
-        "items",
-        {
-            "item_code": args.item or args.item_code or "_Test Trading Goods 1",
-            "qty": args.qty or 5,
-            "rate": args.rate or 50,
-            "cost_center": f"Main - {abbr}",
-        },
-    )
+    append_items(pi, args, abbr)
 
     if args.is_in_state or args.is_in_state_rcm:
-        pi.append("taxes", get_taxes("CGST", abbr))
-        pi.append("taxes", get_taxes("SGST", abbr))
+        append_taxes(pi, ["CGST", "SGST"], abbr)
     elif args.is_in_state is False or args.is_in_state_rcm is False:
-        pi.append("taxes", get_taxes("IGST", abbr, rate=18))
+        append_taxes(pi, "IGST", abbr, rate=18)
 
     if args.is_in_state_rcm:
-        pi.append("taxes", get_taxes("CGST RCM", abbr))
-        pi.append("taxes", get_taxes("SGST RCM", abbr))
+        append_taxes(pi, ["CGST RCM", "SGST RCM"], abbr)
     elif args.is_in_state_rcm is False:
-        pi.append("taxes", get_taxes("IGST RCM", abbr, rate=18))
+        append_taxes(pi, "IGST RCM", abbr, rate=18)
 
     if not args.do_not_save:
         pi.insert()
@@ -94,21 +74,38 @@ def create_purchase_invoice(**args):
     return pi
 
 
-def get_taxes(account, abbr, is_sales=False, rate=9):
+def append_taxes(obj, accounts, abbr, is_sales=False, rate=9):
+
+    if isinstance(accounts, str):
+        accounts = [accounts]
+
     if is_sales:
         account_type = "Output Tax"
     else:
         account_type = "Input Tax"
 
-    taxes = {
-        "charge_type": "On Net Total",
-        "account_head": f"{account_type} {account} - {abbr}",
-        "description": f"{account}",
-        "rate": rate,
-        "cost_center": f"Main - {abbr}",
-    }
+    for account in accounts:
+        taxes = {
+            "charge_type": "On Net Total",
+            "account_head": f"{account_type} {account} - {abbr}",
+            "description": f"{account}",
+            "rate": rate,
+            "cost_center": f"Main - {abbr}",
+        }
 
-    if account.endswith("RCM"):
-        taxes["add_deduct_tax"] = "Deduct"
+        if account.endswith("RCM"):
+            taxes["add_deduct_tax"] = "Deduct"
 
-    return taxes
+        obj.append("taxes", taxes)
+
+
+def append_items(obj, args, abbr):
+    obj.append(
+        "items",
+        {
+            "item_code": args.item_code or "_Test Trading Goods 1",
+            "qty": args.qty or 5,
+            "rate": args.rate or 50,
+            "cost_center": f"Main - {abbr}",
+        },
+    )
