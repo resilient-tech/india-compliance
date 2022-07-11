f42aa849fc02a80a6912ad682ecc064d66c4947f
Daizy <modidaizy6730@gmail.com>
2022-07-11 15:51:28 +0530

    fix: moved requirement to pyproject.toml file

--- setup.py
+++ setup.py
@@ -1,19 +1,5 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-with open("requirements.txt") as f:
-    install_requires = f.read().strip().split("\n")
+name = "india_compliance"
 
-# get version from __version__ variable in india_compliance/__init__.py
-from india_compliance import __version__ as version
-
-setup(
-    name="india_compliance",
-    version=version,
-    description="ERPNext app to simplify compliance with Indian Rules and Regulations",
-    author="Resilient Tech",
-    author_email="hello@indiacompliance.app",
-    packages=find_packages(),
-    zip_safe=False,
-    include_package_data=True,
-    install_requires=install_requires,
-)
+setup()
