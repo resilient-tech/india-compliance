f43681ee3b52964602ba35f8155c781e5089b439
Sagar Vora <sagar@resilient.tech>
2022-07-18 10:56:49 +0530

    fix: check conf even if api secret set in GST settings

--- india_compliance/boot.py
+++ india_compliance/boot.py
@@ -9,13 +9,11 @@ def set_bootinfo(bootinfo):
     bootinfo["gst_party_types"] = GST_PARTY_TYPES
 
     gst_settings = frappe.get_cached_doc("GST Settings").as_dict()
-    if gst_settings.api_secret:
-        gst_settings.api_secret = "***"
-    else:
-        bootinfo["ic_api_enabled_from_conf"] = bool(frappe.conf.ic_api_secret)
+    gst_settings.api_secret = "***" if gst_settings.api_secret else ""
 
     for key in ("gst_accounts", "credentials"):
         gst_settings.pop(key, None)
 
     bootinfo["gst_settings"] = gst_settings
     bootinfo["india_state_options"] = list(STATE_NUMBERS)
+    bootinfo["ic_api_enabled_from_conf"] = bool(frappe.conf.ic_api_secret)
