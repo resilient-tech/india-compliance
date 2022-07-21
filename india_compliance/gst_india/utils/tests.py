e2e6dd16d66acdad020c400e7708c41459d82cdb
vorasmit <mailsmitvora@gmail.com>
2022-07-21 18:18:09 +0530

    refactor: existing test cases (#173)
    * test: refactored gstr-3b-report test case
    
    * test: refactored hsn wise summary test case
    
    * test: add test utility to create records
    
    * test: refactor item and taxes
    
    * test: refactor create taxes
    
    * test: fix test cases
    
    * fix: get source state to validate is_inter_state
    
    * ci: server tests on develop
    
    * ci: python version to 3.10
    
    * fix: gst_category set to data field and none for transactions by default
    
    * fix: rename test_records to testutils
    
    * fix: changes during review
    
    Co-authored-by: Daizy <modidaizy6730@gmail.com>
    Co-authored-by: Sagar Vora <sagar@resilient.tech>

---
+++ india_compliance/gst_india/utils/tests.py
@@ -0,0 +1,106 @@
+import frappe
+from frappe.utils import getdate
+
+from india_compliance.gst_india.constants import SALES_DOCTYPES
+
+
+def create_sales_invoice(**data):
+    data["doctype"] = "Sales Invoice"
+    return create_transaction(**data)
+
+
+def create_purchase_invoice(**data):
+    data["doctype"] = "Purchase Invoice"
+    return create_transaction(**data)
+
+
+def create_transaction(**data):
+    data = frappe._dict(data)
+    transaction = frappe.get_doc(data)
+
+    if not transaction.company:
+        transaction.company = "_Test Indian Registered Company"
+
+    if not transaction.posting_date:
+        transaction.posting_date = getdate()
+
+    if transaction.doctype in SALES_DOCTYPES:
+        if not transaction.customer:
+            transaction.customer = "_Test Registered Customer"
+
+    else:
+        if not transaction.supplier:
+            transaction.supplier = "_Test Registered Supplier"
+
+        if (
+            transaction.doctype == "Purchase Invoice"
+            and not transaction.eligibility_for_itc
+        ):
+            transaction.eligibility_for_itc = "All Other ITC"
+
+    company_abbr = frappe.get_cached_value("Company", data.company, "abbr") or "_TIRC"
+    append_item(transaction, data, company_abbr)
+
+    # Append taxes
+    if data.is_in_state or data.is_in_state_rcm:
+        _append_taxes(transaction, ["CGST", "SGST"], company_abbr, rate=9)
+
+    if data.is_out_state or data.is_out_state_rcm:
+        _append_taxes(transaction, "IGST", company_abbr, rate=18)
+
+    if data.is_in_state_rcm:
+        _append_taxes(transaction, ["CGST RCM", "SGST RCM"], company_abbr, rate=9)
+
+    if data.is_out_state_rcm:
+        _append_taxes(transaction, "IGST RCM", company_abbr, rate=18)
+
+    if not data.do_not_save:
+        transaction.insert()
+
+        if not data.do_not_submit:
+            transaction.submit()
+
+    return transaction
+
+
+def append_item(transaction, data=None, company_abbr="_TIRC"):
+    if not data:
+        data = frappe._dict()
+
+    return transaction.append(
+        "items",
+        {
+            "item_code": data.item_code or "_Test Trading Goods 1",
+            "qty": data.qty or 1,
+            "rate": data.rate or 100,
+            "cost_center": f"Main - {company_abbr}",
+            "is_nil_exempt": data.is_nil_exempt,
+            "is_non_gst": data.is_non_gst,
+            "item_tax_template": data.item_tax_template,
+            "gst_hsn_code": data.gst_hsn_code,
+        },
+    )
+
+
+def _append_taxes(transaction, accounts, company_abbr="_TIRC", rate=9):
+    if isinstance(accounts, str):
+        accounts = [accounts]
+
+    if transaction.doctype in SALES_DOCTYPES:
+        account_type = "Output Tax"
+    else:
+        account_type = "Input Tax"
+
+    for account in accounts:
+        tax = {
+            "charge_type": "On Net Total",
+            "account_head": f"{account_type} {account} - {company_abbr}",
+            "description": account,
+            "rate": rate,
+            "cost_center": f"Main - {company_abbr}",
+        }
+
+        if account.endswith("RCM"):
+            tax["add_deduct_tax"] = "Deduct"
+
+        transaction.append("taxes", tax)
