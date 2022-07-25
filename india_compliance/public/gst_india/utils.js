241c22880c90de4783074102891d10dfb0bb7752
Sagar Vora <sagar@resilient.tech>
2022-07-26 02:32:29 +0530

    style: define utils in object

--- india_compliance/public/gst_india/utils.js
+++ india_compliance/public/gst_india/utils.js
@@ -1,42 +1,44 @@
-frappe.provide("ic");
-
 window.gst_settings = frappe.boot.gst_settings;
 
-ic.get_gstin_query = function (party, party_type = "Company") {
-    if (!party) {
-        frappe.show_alert({
-            message: __("Please select {0} to get GSTIN options", [__(party_type)]),
-            indicator: "yellow",
-        });
-        return;
-    }
+frappe.provide("ic");
 
-    return {
-        query: "india_compliance.gst_india.utils.get_gstin_list",
-        params: { party, party_type },
-    };
-};
+Object.assign(ic, {
+    get_gstin_query(party, party_type = "Company") {
+        if (!party) {
+            frappe.show_alert({
+                message: __("Please select {0} to get GSTIN options", [__(party_type)]),
+                indicator: "yellow",
+            });
+            return;
+        }
 
-ic.get_party_type = function (doctype) {
-    return in_list(frappe.boot.sales_doctypes, doctype) ? "Customer" : "Supplier";
-};
+        return {
+            query: "india_compliance.gst_india.utils.get_gstin_list",
+            params: { party, party_type },
+        };
+    },
 
-ic.set_state_options = function (frm) {
-    const state_field = frm.get_field("state");
-    const country = frm.get_field("country").value;
-    if (country !== "India") {
-        state_field.set_data([]);
-        return;
-    }
+    get_party_type(doctype) {
+        return in_list(frappe.boot.sales_doctypes, doctype) ? "Customer" : "Supplier";
+    },
 
-    state_field.set_data(frappe.boot.india_state_options || []);
-};
+    set_state_options(frm) {
+        const state_field = frm.get_field("state");
+        const country = frm.get_field("country").value;
+        if (country !== "India") {
+            state_field.set_data([]);
+            return;
+        }
 
-ic.can_enable_api = function (settings) {
-    return settings.api_secret || frappe.boot.ic_api_enabled_from_conf;
-};
+        state_field.set_data(frappe.boot.india_state_options || []);
+    },
 
-ic.is_api_enabled = function (settings) {
-    if (!settings) settings = gst_settings;
-    return settings.enable_api && ic.can_enable_api(settings);
-};
+    can_enable_api(settings) {
+        return settings.api_secret || frappe.boot.ic_api_enabled_from_conf;
+    },
+
+    is_api_enabled(settings) {
+        if (!settings) settings = gst_settings;
+        return settings.enable_api && ic.can_enable_api(settings);
+    },
+});
