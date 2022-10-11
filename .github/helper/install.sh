aa69ddd4bf58b3e8c764aedb0f25b8245da64820
Daizy <modidaizy6730@gmail.com>
2022-10-11 10:52:39 +0530

    ci: trial commit to check pdf version

--- .github/helper/install.sh
+++ .github/helper/install.sh
@@ -37,12 +37,8 @@ FLUSH PRIVILEGES;
 
 
 install_wkhtmltopdf() {
-    sudo apt-get install xvfb libfontconfig wkhtmltopdf
-
-    # wget -O /tmp/wkhtmltox.tar.xz wkhtmltox-0.12.6-3.archlinux-x86_64.pkg.tar.xz
-    # tar -xf /tmp/wkhtmltox.tar.xz -C /tmp
-    # sudo mv /tmp/wkhtmltox/bin/wkhtmltopdf /usr/local/bin/wkhtmltopdf
-    # sudo chmod o+x /usr/local/bin/wkhtmltopdf
+    wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.focal_amd64.deb
+    sudo dpkg -i ./wkhtmltox_0.12.6-1.focal_amd64.deb
 }
 install_wkhtmltopdf &
 
