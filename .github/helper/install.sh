6d6def5764a003d312b44c3b955e30c2ff79d04c
Daizy <modidaizy6730@gmail.com>
2022-10-11 11:18:02 +0530

    fix: use apt to install deb package

--- .github/helper/install.sh
+++ .github/helper/install.sh
@@ -38,7 +38,7 @@ FLUSH PRIVILEGES;
 
 install_wkhtmltopdf() {
     wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.focal_amd64.deb
-    sudo dpkg -i ./wkhtmltox_0.12.6-1.focal_amd64.deb
+    sudo apt install ./wkhtmltox_0.12.6-1.focal_amd64.deb
 }
 install_wkhtmltopdf &
 
