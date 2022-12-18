daecbdc7b61fbc347a83efab63ca4bceed6b9279
Smit Vora <smitvora203@gmail.com>
2022-12-18 09:15:26 +0000

    fix: irn not visible for Registered Composition

--- india_compliance/gst_india/constants/custom_fields.py
+++ india_compliance/gst_india/constants/custom_fields.py
@@ -600,10 +600,7 @@ E_INVOICE_FIELDS = {
             "insert_after": "customer",
             "no_copy": 1,
             "print_hide": 1,
-            "depends_on": (
-                'eval:in_list(["Registered Regular", "SEZ", "Overseas", "Deemed'
-                ' Export"], doc.gst_category)'
-            ),
+            "depends_on": 'eval:doc.gst_category != "Unregistered"',
             "translatable": 0,
         },
         {
