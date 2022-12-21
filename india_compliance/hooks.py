89a4ed54239e4740d6ef310855a58dacfebf3135
Daizy Modi <modidaizy5217@gmail.com>
2022-12-21 13:20:06 +0530

    fix: removed custom field and property setters after uninstall app
    (cherry picked from commit cfaada28b718bd0c7a6ba72c994158f01fffbf8e)

--- india_compliance/hooks.py
+++ india_compliance/hooks.py
@@ -14,6 +14,8 @@ after_install = "india_compliance.install.after_install"
 before_tests = "india_compliance.tests.before_tests"
 boot_session = "india_compliance.boot.set_bootinfo"
 
+after_uninstall = "india_compliance.uninstall.after_uninstall"
+
 app_include_js = "gst_india.bundle.js"
 
 doctype_js = {
