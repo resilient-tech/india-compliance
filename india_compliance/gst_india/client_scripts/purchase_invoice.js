357f9cc21d9b93de182870f5b730ac16918e7129
Smit Vora <smitvora203@gmail.com>
2022-12-24 14:49:16 +0000

    fix: show alert only if e-Waybill is applicable for Purchase Invoice

--- india_compliance/gst_india/client_scripts/purchase_invoice.js
+++ india_compliance/gst_india/client_scripts/purchase_invoice.js
@@ -2,17 +2,21 @@ const DOCTYPE = "Purchase Invoice";
 setup_e_waybill_actions(DOCTYPE);
 
 frappe.ui.form.on(DOCTYPE, {
-	after_save(frm) {
-		if (
-			frm.doc.docstatus ||
-			frm.doc.supplier_address ||
-			!(gst_settings.enable_e_waybill && gst_settings.enable_e_waybill_from_pi)
-		)
-			return;
+    after_save(frm) {
+        if (
+            frm.doc.docstatus ||
+            frm.doc.supplier_address ||
+            !(gst_settings.enable_e_waybill && gst_settings.enable_e_waybill_from_pi) ||
+            !(frm.doc.is_return || frm.doc.gst_category === "Unregistered")
+        )
+            return;
 
-		frappe.show_alert({
-			message: __("Supplier Address is required to create e-Waybill"),
-			indicator: "yellow",
-		}, 10);
-	},
+        frappe.show_alert(
+            {
+                message: __("Supplier Address is required to create e-Waybill"),
+                indicator: "yellow",
+            },
+            10
+        );
+    },
 });
