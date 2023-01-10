d985167a722cb9cd8d43de07b5e7b0ccf33ad009
Dany Robert <rtdany10@gmail.com>
2023-01-10 10:47:54 +0000

    chore: run pre-commit

--- india_compliance/patches/post_install/add_hsn_to_item_variant_copy_fields.py
+++ india_compliance/patches/post_install/add_hsn_to_item_variant_copy_fields.py
@@ -1,10 +1,11 @@
 import frappe
 
 """
-This patch makes sure hsn code is copied over to variant item when created from template 
+This patch makes sure hsn code is copied over to variant item when created from template
 """
 
+
 def execute():
-    frappe.get_doc("Item Variant Settings").append("fields", {
-        "field_name": "gst_hsn_code"
-    }).save()
+    frappe.get_doc("Item Variant Settings").append(
+        "fields", {"field_name": "gst_hsn_code"}
+    ).save()
