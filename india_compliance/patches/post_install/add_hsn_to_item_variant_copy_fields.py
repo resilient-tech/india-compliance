7aa5e930b0c04fa3bd3fda9597aa226c25dffdea
Dany Robert <rtdany10@gmail.com>
2023-01-10 10:54:57 +0000

    fix: avoid duplicate field entry

--- india_compliance/patches/post_install/add_hsn_to_item_variant_copy_fields.py
+++ india_compliance/patches/post_install/add_hsn_to_item_variant_copy_fields.py
@@ -6,6 +6,7 @@ This patch makes sure hsn code is copied over to variant item when created from 
 
 
 def execute():
-    frappe.get_doc("Item Variant Settings").append(
-        "fields", {"field_name": "gst_hsn_code"}
-    ).save()
+    if not frappe.db.exists("Variant Field", {"field_name": "gst_hsn_code"}):
+        frappe.get_doc("Item Variant Settings").append(
+            "fields", {"field_name": "gst_hsn_code"}
+        ).save()
