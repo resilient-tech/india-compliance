80dbaac14619340ff0e6502ad2dba52f6941d29b
Smit Vora <smitvora203@gmail.com>
2023-01-19 05:31:11 +0000

    chore: improve code readability

--- india_compliance/install.py
+++ india_compliance/install.py
@@ -30,7 +30,7 @@ POST_INSTALL_PATCHES = (
     "remove_deprecated_docs",
     "remove_old_fields",
     "update_custom_role_for_e_invoice_summary",
-    "add_hsn_to_item_variant_copy_fields",
+    "add_item_variant_fields_to_copy",
 )
 
 
