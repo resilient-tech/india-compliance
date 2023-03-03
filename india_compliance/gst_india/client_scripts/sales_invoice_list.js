a47dd39c79cb24eb34342e5bc3b6875999d1c14d
Sagar Vora <sagar@resilient.tech>
2023-03-03 19:20:47 +0530

    fix: multiple fixes to SI bulk actions

--- india_compliance/gst_india/client_scripts/sales_invoice_list.js
+++ india_compliance/gst_india/client_scripts/sales_invoice_list.js
@@ -5,27 +5,29 @@ frappe.listview_settings[DOCTYPE].onload = function (list_view) {
         erpnext_onload(list_view);
     }
 
-    if (!frappe.perm.has_perm(DOCTYPE, 0, "submit"))
-        return;
+    if (!frappe.perm.has_perm(DOCTYPE, 0, "submit")) return;
 
     if (gst_settings.enable_e_waybill)
-        add_bulk_action(list_view, "e-Waybill JSON", generate_e_waybill_json);
+        add_bulk_action_for_submitted_invoices(
+            list_view,
+            __("Generate e-Waybill JSON"),
+            generate_e_waybill_json
+        );
 
     if (ic.is_e_invoice_enabled())
-        add_bulk_action(list_view, "e-Invoice", generate_e_invoice);
+        add_bulk_action_for_submitted_invoices(
+            list_view,
+            __("Enqueue Bulk e-Invoice Generation"),
+            enqueue_bulk_e_invoice_generation
+        );
 };
 
-function add_bulk_action(list_view, label, callback) {
-    list_view.page.add_actions_menu_item(
-        __("Generate {0}", [__(label)]),
-        () => {
-            const selected_docs = list_view.get_checked_items();
-            const docnames = list_view.get_checked_items(true);
-            validate_draft_invoices(selected_docs, label);
-            callback(docnames);
-        },
-        false
-    );
+function add_bulk_action_for_submitted_invoices(list_view, label, callback) {
+    list_view.page.add_actions_menu_item(label, async () => {
+        const selected_docs = list_view.get_checked_items();
+        const submitted_docs = await validate_if_submitted(selected_docs);
+        if (submitted_docs) callback(submitted_docs);
+    });
 }
 
 async function generate_e_waybill_json(docnames) {
@@ -37,34 +39,58 @@ async function generate_e_waybill_json(docnames) {
     trigger_file_download(ewb_data, get_e_waybill_file_name());
 }
 
-function generate_e_invoice(docnames) {
-    frappe.xcall(
-        "india_compliance.gst_india.overrides.sales_invoice.generate_e_invoice",
+async function enqueue_bulk_e_invoice_generation(docnames) {
+    const now = frappe.datetime.system_datetime();
+
+    const job_id = await frappe.xcall(
+        "india_compliance.gst_india.utils.e_invoice.enqueue_bulk_e_invoice_generation",
         { docnames }
     );
 
-    const today = frappe.datetime.get_today();
-    const route = frappe.utils.generate_route({
+    const api_requests_link = frappe.utils.generate_route({
         type: "doctype",
         name: "Integration Request",
         route_options: {
             integration_request_service: "India Compliance API",
-            creation: `["Between",["${today}", "${today}"]]`,
+            creation: `[">", "${now}"]`,
         },
     });
+
     frappe.msgprint(
         __(
-            'Bulk Generation is queued. Check the progress in <a href="{0}">Integration Request</a> Log.',
-            [route]
+            "Bulk e-Invoice Generation has been queued. You can track the <a href='{0}'>Background Job</a> and <a href='{1}'>API Requests</a>.",
+            [frappe.utils.get_form_link("RQ Job", job_id), api_requests_link]
         )
     );
 }
 
-function validate_draft_invoices(selected_docs, label) {
-    const draft_invoices = selected_docs.filter(doc => doc.docstatus == 0);
-    if (!draft_invoices.length) return;
+async function validate_if_submitted(selected_docs) {
+    const valid_docs = [];
+    const invalid_docs = [];
 
-    frappe.throw(
-        __("{0} can only be generated from a submitted document.", [__(label)])
-    );
+    for (const doc of selected_docs) {
+        if (doc.docstatus != 1) {
+            invalid_docs.push(doc.name);
+        } else {
+            valid_docs.push(doc.name);
+        }
+    }
+
+    if (!invalid_docs.length) return valid_docs;
+
+    if (!valid_docs.length) {
+        frappe.throw(__("This action can only be performed on submitted documents"));
+    }
+
+    const confirmed = await new Promise(resolve => {
+        frappe.confirm(
+            __(
+                "This action can only be performed on submitted documents. Do you want to continue without the following documents?<br><br><strong>{0}</strong>",
+                [invalid_docs.join("<br>")]
+            ),
+            () => resolve(true)
+        );
+    });
+
+    return confirmed ? valid_docs : false;
 }
