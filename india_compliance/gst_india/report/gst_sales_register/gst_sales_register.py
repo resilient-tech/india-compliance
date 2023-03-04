cf5875abf1e347576953641bb72729c70ff544f8
Sagar Vora <sagar@resilient.tech>
2023-03-04 14:11:25 +0530

    chore: improve column adding logic in GST sales reports
    (cherry picked from commit 4fb9c36bd4f9f47fec93399d3491f04f2bd79eba)

--- india_compliance/gst_india/report/gst_sales_register/gst_sales_register.py
+++ india_compliance/gst_india/report/gst_sales_register/gst_sales_register.py
@@ -1,65 +1,58 @@
 # Copyright (c) 2013, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 import frappe
+from frappe import _
 from erpnext.accounts.report.sales_register.sales_register import _execute
 
-EXPORT_TYPE_COLUMNS = dict(
-    fieldtype="Check",
-    label="Is Export With GST",
-    fieldname="is_export_with_gst",
-    width=120,
-)
+EXPORT_TYPE_COLUMN = {
+    "fieldtype": "Check",
+    "label": _("Is Export With GST"),
+    "fieldname": "is_export_with_gst",
+    "width": 120,
+}
 
-REVERSE_CHARGE_COLUMNS = dict(
-    fieldtype="Check",
-    label="Is Reverse Charge",
-    fieldname="is_reverse_charge",
-    width=120,
-)
+REVERSE_CHARGE_COLUMN = {
+    "fieldtype": "Check",
+    "label": _("Is Reverse Charge"),
+    "fieldname": "is_reverse_charge",
+    "width": 120,
+}
 
 ADDITIONAL_TABLE_COLUMNS = [
-    dict(
-        fieldtype="Data",
-        label="Billing Address GSTIN",
-        fieldname="billing_address_gstin",
-        width=140,
-    ),
-    dict(
-        fieldtype="Data",
-        label="Company GSTIN",
-        fieldname="company_gstin",
-        width=120,
-    ),
-    dict(
-        fieldtype="Data",
-        label="Place of Supply",
-        fieldname="place_of_supply",
-        width=120,
-    ),
-    dict(
-        fieldtype="Data",
-        label="GST Category",
-        fieldname="gst_category",
-        width=120,
-    ),
-    dict(
-        fieldtype="Data",
-        label="E-Commerce GSTIN",
-        fieldname="ecommerce_gstin",
-        width=130,
-    ),
-]
-
-ADDITIONAL_QUERY_COLUMNS = [
-    "billing_address_gstin",
-    "company_gstin",
-    "place_of_supply",
-    "gst_category",
-    "ecommerce_gstin",
+    {
+        "fieldtype": "Data",
+        "label": _("Billing Address GSTIN"),
+        "fieldname": "billing_address_gstin",
+        "width": 140,
+    },
+    {
+        "fieldtype": "Data",
+        "label": _("Company GSTIN"),
+        "fieldname": "company_gstin",
+        "width": 120,
+    },
+    {
+        "fieldtype": "Data",
+        "label": _("Place of Supply"),
+        "fieldname": "place_of_supply",
+        "width": 120,
+    },
+    {
+        "fieldtype": "Data",
+        "label": _("GST Category"),
+        "fieldname": "gst_category",
+        "width": 120,
+    },
+    {
+        "fieldtype": "Data",
+        "label": _("E-Commerce GSTIN"),
+        "fieldname": "ecommerce_gstin",
+        "width": 130,
+    },
 ]
 
 
-def execute(filters=None):
+def get_additional_table_columns():
     overseas_enabled, reverse_charge_enabled = frappe.get_cached_value(
         "GST Settings",
         "GST Settings",
@@ -67,14 +60,25 @@ def execute(filters=None):
     )
 
     additional_table_columns = ADDITIONAL_TABLE_COLUMNS.copy()
-    additional_query_columns = ADDITIONAL_QUERY_COLUMNS.copy()
 
     if reverse_charge_enabled:
-        additional_table_columns.insert(3, REVERSE_CHARGE_COLUMNS)
-        additional_query_columns.insert(3, "is_reverse_charge")
+        additional_table_columns.insert(-2, REVERSE_CHARGE_COLUMN)
 
     if overseas_enabled:
-        additional_table_columns.insert(-2, EXPORT_TYPE_COLUMNS)
-        additional_query_columns.insert(-2, "is_export_with_gst")
+        additional_table_columns.insert(-2, EXPORT_TYPE_COLUMN)
 
-    return _execute(filters, additional_table_columns, additional_query_columns)
+    return additional_table_columns
+
+
+def get_column_names(additional_table_columns):
+    return [column["fieldname"] for column in additional_table_columns]
+
+
+def execute(filters=None):
+    additional_table_columns = get_additional_table_columns()
+
+    return _execute(
+        filters,
+        additional_table_columns,
+        get_column_names(additional_table_columns),
+    )
