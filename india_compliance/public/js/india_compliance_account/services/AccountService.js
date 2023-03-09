4829e7e5f234c3ffe6247c1b2a0ff339f6d8727b
Sagar Vora <sagar@resilient.tech>
2023-03-09 15:05:18 +0530

    refactor: `window.ic` => `window.india_compliance`
    (cherry picked from commit 38a53480c70a2c820049f023cbabbdaa3ea8aad6)

--- india_compliance/public/js/india_compliance_account/services/AccountService.js
+++ india_compliance/public/js/india_compliance_account/services/AccountService.js
@@ -1,12 +1,12 @@
 export async function get_details(type) {
-    return ic.gst_api.call(`account.get_${type}_details`, {
+    return india_compliance.gst_api.call(`account.get_${type}_details`, {
         method: "GET",
         with_api_secret: true,
     });
 }
 
 export async function update_billing_details(new_billing_details) {
-    return ic.gst_api.call("account.update_billing_details", {
+    return india_compliance.gst_api.call("account.update_billing_details", {
         method: "POST",
         body: { new_billing_details },
         with_api_secret: true,
@@ -14,7 +14,7 @@ export async function update_billing_details(new_billing_details) {
 }
 
 export async function create_order(credits, amount) {
-    return ic.gst_api.call("account.create_order", {
+    return india_compliance.gst_api.call("account.create_order", {
         method: "POST",
         body: { credits, amount },
         with_api_secret: true,
@@ -22,7 +22,7 @@ export async function create_order(credits, amount) {
 }
 
 export async function verify_payment(orderId) {
-    return ic.gst_api.call("account.verify_payment", {
+    return india_compliance.gst_api.call("account.verify_payment", {
         method: "POST",
         body: { order_id: orderId },
         with_api_secret: true,
