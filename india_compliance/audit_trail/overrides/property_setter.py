ba725cbe7e5f9f1dff0c90bbcc8a890a862843e4
Sagar Vora <sagar@resilient.tech>
2023-03-31 20:04:25 +0530

    fix: better validation for property setter
    (cherry picked from commit a2c058f55106de0c51a62fe62bcadb5d3f5191fa)

--- india_compliance/audit_trail/overrides/property_setter.py
+++ india_compliance/audit_trail/overrides/property_setter.py
@@ -1,5 +1,6 @@
 import frappe
 from frappe import _
+from frappe.utils import cint
 
 from india_compliance.audit_trail.utils import (
     get_audit_trail_doctypes,
@@ -9,34 +10,43 @@ from india_compliance.audit_trail.utils import (
 
 def validate(doc, method=None):
     if (
-        not is_audit_trail_enabled()
-        or doc.flags.for_audit_trail
-        or frappe.flags.in_install
+        frappe.flags.in_install
         or frappe.flags.in_migrate
+        or not is_audit_trail_enabled()
     ):
         return
 
-    validate_protected_property_setter(doc)
-    if old_doc := doc.get_doc_before_save():
-        validate_protected_property_setter(old_doc)
+    is_protected = is_protected_property_setter(doc)
+    if doc.is_new() and (not is_protected or cint(doc.value) == 1):
+        return
+
+    if is_protected:
+        throw_cannot_change_property_error(doc)
+
+    old_doc = doc.get_doc_before_save()
+    if is_protected_property_setter(old_doc):
+        throw_cannot_change_property_error(old_doc)
 
 
 def on_trash(doc, method=None):
-    if not is_audit_trail_enabled():
+    if not is_audit_trail_enabled() or not is_protected_property_setter(doc):
         return
 
-    validate_protected_property_setter(doc)
+    throw_cannot_change_property_error(doc)
 
 
-def validate_protected_property_setter(doc):
-    if (
+def throw_cannot_change_property_error(doc):
+    frappe.throw(
+        _(
+            "Cannot change the Track Changes property for {0}, since it has been"
+            " enabled to maintain Audit Trail"
+        ).format(_(doc.doc_type))
+    )
+
+
+def is_protected_property_setter(doc):
+    return (
         doc.doctype_or_field == "DocType"
         and doc.property == "track_changes"
         and doc.doc_type in get_audit_trail_doctypes()
-    ):
-        frappe.throw(
-            _(
-                "Cannot change the Track Changes property for {0}, since it has been"
-                " enabled to maintain Audit Trail"
-            ).format(_(doc.doc_type))
-        )
+    )
