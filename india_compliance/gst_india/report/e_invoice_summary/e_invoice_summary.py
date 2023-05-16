560b790ff89897f163ea9d453d418e0051e825c3
Smit Vora <smitvora203@gmail.com>
2023-05-16 09:35:54 +0530

    fix: added exceptions and conditions
    (cherry picked from commit 3c56e32f6498dc03d97ccb81588e640444a19e58)

--- india_compliance/gst_india/report/e_invoice_summary/e_invoice_summary.py
+++ india_compliance/gst_india/report/e_invoice_summary/e_invoice_summary.py
@@ -46,8 +46,7 @@ def get_data(filters=None):
     sales_invoice = frappe.qb.DocType("Sales Invoice")
     e_invoice_log = frappe.qb.DocType("e-Invoice Log")
 
-    conditions = e_invoice_conditions(filters)
-
+    conditions = e_invoice_conditions()
     query = (
         frappe.qb.from_(sales_invoice)
         .left_join(e_invoice_log)
@@ -59,20 +58,18 @@ def get_data(filters=None):
             Case().when(sales_invoice.is_return == 1, "Y").else_("N").as_("is_return"),
             sales_invoice.base_grand_total,
             sales_invoice.name.as_("sales_invoice"),
+            sales_invoice.irn,
             e_invoice_log.acknowledgement_number,
             e_invoice_log.acknowledged_on,
-            e_invoice_log.irn,
         )
         .where(
             sales_invoice.posting_date[
                 filters.get("from_date") : filters.get("to_date")
             ]
         )
+        .where(sales_invoice.company == filters.get("company"))
         .where(conditions)
     )
-
-    if filters.get("e_invoice_not_available") == 1:
-        query = query.where(((sales_invoice.irn == "") | (sales_invoice.irn.isnull())))
 
     if filters.get("status"):
         query = query.where(sales_invoice.einvoice_status == filters.get("status"))
@@ -80,28 +77,52 @@ def get_data(filters=None):
     if filters.get("customer"):
         query = query.where(sales_invoice.customer == filters.get("customer"))
 
+    if filters.get("exceptions") == "e-Invoice Not Generated":
+        query = query.where(((sales_invoice.irn == "") | (sales_invoice.irn.isnull())))
+
+    if filters.get("exceptions") != "Invoice Cancelled but not e-Invoice":
+        query = query.where(sales_invoice.docstatus == 1)
+
+    else:
+        valid_irns = frappe.get_all(
+            "Sales Invoice",
+            pluck="irn",
+            filters={
+                "docstatus": 1,
+                "company": filters.get("company"),
+                # logical optimization
+                "posting_date": [">=", filters.get("from_date")],
+                "irn": ["is", "set"],
+            },
+        )
+
+        # invoice is cancelled but irn is not cancelled
+        query = (
+            query.where(sales_invoice.docstatus == 2)
+            .where((sales_invoice.irn != "") | (sales_invoice.irn.notnull()))
+            .where(sales_invoice.irn.notin(valid_irns))
+        )
+
     query = query.orderby(sales_invoice.posting_date)
 
     return query.run(as_dict=True)
 
 
-def e_invoice_conditions(filters):
+def e_invoice_conditions():
     sales_invoice = frappe.qb.DocType("Sales Invoice")
-    sub_query = validate_sales_invoice_item(filters)
+    sub_query = validate_sales_invoice_item()
 
     conditions = []
-    if filters.get("company"):
-        conditions.append(sales_invoice.company == filters.get("company"))
-    if sales_invoice.docstatus == 1:
-        conditions.append(sales_invoice.docstatus == 1)
-    if sales_invoice.gst_category != "Unregistered":
-        conditions.append(sales_invoice.gst_category != "Unregistered")
-    conditions.append(~sales_invoice.name.isin(sub_query))
+    conditions.append(
+        sales_invoice.company_gstin != sales_invoice.billing_address_gstin
+    )
+    conditions.append(sales_invoice.gst_category != "Unregistered")
+    conditions.append(sales_invoice.name.notin(sub_query))
 
     return reduce(lambda a, b: a & b, conditions)
 
 
-def validate_sales_invoice_item(filters=None):
+def validate_sales_invoice_item():
     sales_invoice_item = frappe.qb.DocType("Sales Invoice Item")
 
     sub_query = (
@@ -132,7 +153,7 @@ def get_columns():
         {
             "fieldtype": "Data",
             "fieldname": "einvoice_status",
-            "label": _("Status"),
+            "label": _("e-Invoice Status"),
             "width": 100,
         },
         {
