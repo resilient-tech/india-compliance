5aa89372a6a5a6cab289f4fba6b6749dfe23339a
Smit Vora <smitvora203@gmail.com>
2023-05-16 08:39:57 +0530

    fix: linters and formats
    (cherry picked from commit 17357805a587a5475582f28197597b33d100fe2b)

--- india_compliance/gst_india/report/e_invoice_summary/e_invoice_summary.js
+++ india_compliance/gst_india/report/e_invoice_summary/e_invoice_summary.js
@@ -1,61 +1,64 @@
 // Copyright (c) 2016, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
-/* eslint-disable */
 
 frappe.query_reports["e-Invoice Summary"] = {
-  "filters": [
-    {
-      "fieldtype": "Link",
-      "options": "Company",
-      "reqd": 1,
-      "fieldname": "company",
-      "label": __("Company"),
-      "default": frappe.defaults.get_user_default("Company"),
-    },
-    {
-      "fieldtype": "Link",
-      "options": "Customer",
-      "fieldname": "customer",
-      "label": __("Customer")
-    },
-    {
-      "fieldtype": "Date",
-      "reqd": 1,
-      "fieldname": "from_date",
-      "label": __("From Date"),
-      "default": frappe.datetime.add_months(frappe.datetime.get_today(), -1),
-    },
-    {
-      "fieldtype": "Date",
-      "reqd": 1,
-      "fieldname": "to_date",
-      "label": __("To Date"),
-      "default": frappe.datetime.get_today(),
-    },
-    {
-      "fieldtype": "Select",
-      "fieldname": "status",
-      "label": __("e-Invoice Status"),
-      "options": "\nPending\nGenerated\nCancelled\nFailed"
-    },
-    {
-      "fieldtype": "Check",
-      "fieldname": "status_checkbox",
-      "label": __("e-Invoice not available"),
-      "default": 1,
-    }
-  ],
+    filters: [
+        {
+            fieldtype: "Link",
+            options: "Company",
+            reqd: 1,
+            fieldname: "company",
+            label: __("Company"),
+            default: frappe.defaults.get_user_default("Company"),
+        },
+        {
+            fieldtype: "Link",
+            options: "Customer",
+            fieldname: "customer",
+            label: __("Customer"),
+        },
+        {
+            fieldtype: "Date",
+            reqd: 1,
+            fieldname: "from_date",
+            label: __("From Date"),
+            default: frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+        },
+        {
+            fieldtype: "Date",
+            reqd: 1,
+            fieldname: "to_date",
+            label: __("To Date"),
+            default: frappe.datetime.get_today(),
+        },
+        {
+            fieldtype: "Select",
+            fieldname: "status",
+            label: __("e-Invoice Status"),
+            options: "\nPending\nGenerated\nCancelled\nFailed",
+        },
+        {
+            fieldtype: "Check",
+            fieldname: "e_invoice_not_available",
+            label: __("e-Invoice not available"),
+            default: 1,
+        },
+    ],
 
-  "formatter": function (value, row, column, data, default_formatter) {
-    value = default_formatter(value, row, column, data);
+    formatter: function (value, row, column, data, default_formatter) {
+        value = default_formatter(value, row, column, data);
 
-    if (column.fieldname == "einvoice_status" && value) {
-      if (value == 'Pending') value = `<span class="bold" style="color: var(--text-on-orange)">${value}</span>`;
-      else if (value == 'Generated') value = `<span class="bold" style="color: var(--text-on-green)">${value}</span>`;
-      else if (value == 'Cancelled') value = `<span class="bold" style="color: var(--text-on-red)">${value}</span>`;
-      else if (value == 'Failed') value = `<span class="bold"  style="color: var(--text-on-red)">${value}</span>`;
-    }
+        if (column.fieldname == "einvoice_status" && value) {
+            if (value == "Pending")
+                value = `<span class="bold" style="color: var(--text-on-orange)">${value}</span>`;
+            else if (value == "Generated")
+                value = `<span class="bold" style="color: var(--text-on-green)">${value}</span>`;
+            else if (value == "Cancelled")
+                value = `<span class="bold" style="color: var(--text-on-red)">${value}</span>`;
+            else if (value == "Failed")
+                value = `<span class="bold"  style="color: var(--text-on-red)">${value}</span>`;
+        }
 
-    return value;
-  }
+        return value;
+    },
 };
