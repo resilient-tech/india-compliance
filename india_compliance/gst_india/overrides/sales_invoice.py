d0762986901704058701cb88d36271b54793ec06
Sagar Vora <sagar@resilient.tech>
2023-05-17 20:51:19 +0530

    feat: ability to specify Origin Port / Border Checkpost Address for the purpose of e-Waybill (#586)
    Co-authored-by: Smit Vora <smitvora203@gmail.com>
    (cherry picked from commit 3b5c8b2ff5f48de30556cb23f385621496a78c07)

--- india_compliance/gst_india/overrides/sales_invoice.py
+++ india_compliance/gst_india/overrides/sales_invoice.py
@@ -11,8 +11,14 @@ from india_compliance.gst_india.utils.e_invoice import validate_e_invoice_applic
 
 
 def onload(doc, method=None):
-    if not doc.get("ewaybill") and not doc.get("irn"):
-        return
+    if not doc.get("ewaybill"):
+        if doc.gst_category == "Overseas" and is_e_waybill_applicable(doc):
+            doc.set_onload(
+                "shipping_address_in_india", is_shipping_address_in_india(doc)
+            )
+
+        if not doc.get("irn"):
+            return
 
     gst_settings = frappe.get_cached_value(
         "GST Settings",
@@ -53,6 +59,7 @@ def validate(doc, method=None):
 
     validate_invoice_number(doc)
     validate_fields_and_set_status_for_e_invoice(doc)
+    validate_port_address(doc)
 
 
 def validate_invoice_number(doc):
@@ -91,6 +98,34 @@ def validate_fields_and_set_status_for_e_invoice(doc):
         doc.einvoice_status = "Pending"
 
 
+def validate_port_address(doc):
+    if (
+        doc.gst_category != "Overseas"
+        or not is_e_waybill_applicable(doc)
+        or doc.port_address
+        or is_shipping_address_in_india(doc)
+    ):
+        return
+
+    label = doc.meta.get_label("port_address")
+
+    frappe.msgprint(
+        _(
+            "{0} must be specified for generating e-Waybills against export of goods"
+            " (if Shipping Address is not in India)"
+        ).format(frappe.bold(label)),
+        title=_("{0} Not Set").format(label),
+        indicator="yellow",
+    )
+
+
+def is_shipping_address_in_india(doc):
+    if doc.shipping_address_name and (
+        frappe.db.get_value("Address", doc.shipping_address_name, "country") == "India"
+    ):
+        return True
+
+
 def on_submit(doc, method=None):
     if getattr(doc, "_submitted_from_ui", None) or not doc.company_gstin:
         return
@@ -113,30 +148,36 @@ def on_submit(doc, method=None):
 
         return
 
-    elif (
-        not gst_settings.enable_e_waybill
-        or not gst_settings.auto_generate_e_waybill
-        or doc.company_gstin == doc.billing_address_gstin
-        or doc.ewaybill
-        or doc.is_return
-        or doc.is_debit_note
-        or abs(doc.base_grand_total) < gst_settings.e_waybill_threshold
-        or not any(
+    if gst_settings.auto_generate_e_waybill and is_e_waybill_applicable(
+        doc, gst_settings
+    ):
+        frappe.enqueue(
+            "india_compliance.gst_india.utils.e_waybill.generate_e_waybill",
+            enqueue_after_commit=True,
+            queue="short",
+            doctype=doc.doctype,
+            docname=doc.name,
+        )
+
+
+def is_e_waybill_applicable(doc, gst_settings=None):
+    if not gst_settings:
+        gst_settings = frappe.get_cached_doc("GST Settings")
+
+    return bool(
+        gst_settings.enable_e_waybill
+        and doc.company_gstin != doc.billing_address_gstin
+        and not doc.ewaybill
+        and not doc.is_return
+        and not doc.is_debit_note
+        and abs(doc.base_grand_total) >= gst_settings.e_waybill_threshold
+        and any(
             item
             for item in doc.items
             if item.gst_hsn_code
             and not item.gst_hsn_code.startswith("99")
             and item.qty != 0
         )
-    ):
-        return
-
-    frappe.enqueue(
-        "india_compliance.gst_india.utils.e_waybill.generate_e_waybill",
-        enqueue_after_commit=True,
-        queue="short",
-        doctype=doc.doctype,
-        docname=doc.name,
     )
 
 
