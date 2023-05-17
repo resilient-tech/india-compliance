3b5c8b2ff5f48de30556cb23f385621496a78c07
Sagar Vora <sagar@resilient.tech>
2023-05-17 20:51:19 +0530

    feat: ability to specify Origin Port / Border Checkpost Address for the purpose of e-Waybill (#586)
    Co-authored-by: Smit Vora <smitvora203@gmail.com>

--- india_compliance/gst_india/constants/custom_fields.py
+++ india_compliance/gst_india/constants/custom_fields.py
@@ -295,6 +295,22 @@ CUSTOM_FIELDS = {
     ],
     "Sales Invoice": [
         {
+            "fieldname": "port_address",
+            "label": "Origin Port / Border Checkpost Address Name",
+            "fieldtype": "Link",
+            "options": "Address",
+            "print_hide": 1,
+            "description": (
+                "Address of the place / port in India from where goods are being"
+                " exported <br>(for generating e-Waybill against export of goods)"
+            ),
+            "insert_after": "shipping_address",
+            "depends_on": (
+                "eval:doc.company_gstin && doc.gst_category === 'Overseas' &&"
+                " gst_settings.enable_e_waybill"
+            ),
+        },
+        {
             "fieldname": "invoice_copy",
             "label": "Invoice Copy",
             "length": 30,
--- india_compliance/gst_india/utils/e_waybill.py
+++ india_compliance/gst_india/utils/e_waybill.py
@@ -457,6 +457,7 @@ def update_transaction(doc, values):
         "lr_date": values.lr_date,
         "mode_of_transport": values.mode_of_transport,
         "gst_vehicle_type": values.gst_vehicle_type,
+        "port_address": values.port_address,
     }
 
     doc.db_set(data)
@@ -579,9 +580,8 @@ class EWaybillData(GSTTransactionData):
         Validates:
         - Required fields
         - Atleast one item with HSN for goods is required
-        - Overseas Returns are not allowed
         - Basic transporter details must be present
-        - Grand Total Amount must be greater than Criteria
+        - Transaction does not have any non-GST items
         - Sales Invoice with same company and billing gstin
         """
 
@@ -607,13 +607,6 @@ class EWaybillData(GSTTransactionData):
                 ),
                 title=_("Invalid Data"),
             )
-
-        # TODO: check if this validation is required
-        # if self.doc.is_return and self.doc.gst_category == "Overseas":
-        #     frappe.throw(
-        #         msg=_("Return/Credit Note is not supported for Overseas e-Waybill"),
-        #         title=_("Incorrect Usage"),
-        #     )
 
         if not self.doc.gst_transporter_id:
             self.validate_mode_of_transport()
@@ -747,9 +740,14 @@ class EWaybillData(GSTTransactionData):
 
     def set_party_address_details(self):
         transaction_type = 1
+        ship_to_address = (
+            self.doc.port_address
+            if (self.doc.gst_category == "Overseas" and self.doc.port_address)
+            else self.doc.shipping_address_name
+        )
+
         has_different_shipping_address = (
-            self.doc.shipping_address_name
-            and self.doc.customer_address != self.doc.shipping_address_name
+            ship_to_address and self.doc.customer_address != ship_to_address
         )
 
         has_different_dispatch_address = (
@@ -767,9 +765,7 @@ class EWaybillData(GSTTransactionData):
 
         if has_different_shipping_address and has_different_dispatch_address:
             transaction_type = 4
-            self.shipping_address = self.get_address_details(
-                self.doc.shipping_address_name
-            )
+            self.shipping_address = self.get_address_details(ship_to_address)
             self.dispatch_address = self.get_address_details(
                 self.doc.dispatch_address_name
             )
@@ -782,9 +778,7 @@ class EWaybillData(GSTTransactionData):
 
         elif has_different_shipping_address:
             transaction_type = 2
-            self.shipping_address = self.get_address_details(
-                self.doc.shipping_address_name
-            )
+            self.shipping_address = self.get_address_details(ship_to_address)
 
         self.transaction_details.transaction_type = transaction_type
 
