ef876fcba798add5ae40f597c2d93dee425dfc88
DaizyModi <modidaizy5217@gmail.com>
2023-05-23 15:19:54 +0530

    fix: append row for bill of entry in report GST Purchase Register

--- india_compliance/gst_india/report/gst_purchase_register/gst_purchase_register.py
+++ india_compliance/gst_india/report/gst_purchase_register/gst_purchase_register.py
@@ -6,6 +6,9 @@ import frappe
 from frappe import _
 from erpnext.accounts.report.purchase_register.purchase_register import _execute
 
+from india_compliance.gst_india.report.bill_of_entry_summary.bill_of_entry_summary import (
+    update_purchase_invoice_query,
+)
 from india_compliance.gst_india.utils import get_gst_accounts_by_type
 
 
@@ -51,44 +54,46 @@ def execute(filters=None):
 
 
 def update_bill_of_entry_data(filters, data, columns):
-    doctype = "Bill of Entry"
-    boe_tax_accounts = insert_additional_columns(data, columns)
+    bill_of_entries = get_bill_of_entry(filters)
+
+    if not bill_of_entries:
+        return columns, data
+
+    insert_additional_columns(data, columns)
     input_accounts = get_gst_accounts_by_type(filters.get("company"), "Input")
 
-    for idx, _column in enumerate(columns):
-        column_label = (
-            _column.split(":")[0].strip()
-            if isinstance(_column, str)
-            else _column.get("label")
-        )
-
-        for row in data:
-            if column_label in boe_tax_accounts:
-                row.insert(idx, 0)
-
-            purchase_invoice_no = (
-                row[0] if isinstance(row, list) else row.get("invoice")
+    for inv, boe_data in bill_of_entries.items():
+        row = [inv]
+        for _column in columns:
+            column_label = (
+                _column.split(":")[0].strip()
+                if isinstance(_column, str)
+                else _column.get("label")
             )
 
-            if boe_doc := get_bill_of_entry(doctype, purchase_invoice_no):
-                for tax in boe_doc.taxes:
-                    if (
-                        column_label == tax.account_head
-                        and tax.account_head == input_accounts.igst_account
-                    ):
-                        row[idx] += tax.tax_amount
+            if column_label == "Invoice":
+                continue
 
-                    elif (
-                        column_label == tax.account_head
-                        and tax.account_head == input_accounts.cess_account
-                    ):
-                        row[idx] += tax.tax_amount
+            if column_label in input_accounts.values():
+                for account in boe_data.account_detail:
+                    if column_label == account.account_head:
+                        if account.account_head == input_accounts.igst_account:
+                            row.append(account.tax_amount)
+                            break
 
-                    elif column_label == "Total Tax":
-                        row[idx] += tax.tax_amount
+                        elif account.account_head == input_accounts.cess_account:
+                            row.append(account.tax_amount)
+                            break
+                    else:
+                        row.append(0)
+                        break
+            elif column_label == "Total Tax":
+                row.append(boe_data.total_taxes)
 
-                if column_label in ("Grand Total", "Rounded Total"):
-                    row[idx] += boe_doc.total_taxes
+            else:
+                row.append("")
+
+        data.append(row)
 
 
 def get_additional_tax_accounts(data):
@@ -152,6 +157,11 @@ def insert_additional_columns(data, columns, is_itemised_report=False):
                         "width": 100,
                     },
                 )
+
+            for row in data:
+                if account in boe_tax_accounts:
+                    row[frappe.scrub(account + " Rate")] = 0
+                    row[frappe.scrub(account + " Amount")] = 0
         else:
             total_tax_column_index = columns.index("Total Tax:Currency/currency:120")
 
@@ -161,6 +171,10 @@ def insert_additional_columns(data, columns, is_itemised_report=False):
                     total_tax_column_index,
                     _(account + ":Currency/currency:120"),
                 )
+
+            for row in data:
+                if account in boe_tax_accounts:
+                    row.insert(total_tax_column_index, 0)
 
     return boe_tax_accounts
 
@@ -176,10 +190,57 @@ def is_column_exists(columns, key):
     return False
 
 
-def get_bill_of_entry(doctype, invoice):
-    boe = frappe.db.exists(doctype, {"purchase_invoice": invoice, "docstatus": 1})
-    if not boe:
-        return
+def get_bill_of_entry(filters):
+    bill_of_entry = frappe.qb.DocType("Bill of Entry")
+    bill_of_entry_taxes = frappe.qb.DocType("Bill of Entry Taxes")
 
-    doc = frappe.get_cached_doc(doctype, boe)
-    return doc
+    query = (
+        frappe.qb.from_(bill_of_entry)
+        .inner_join(bill_of_entry_taxes)
+        .on(bill_of_entry.name == bill_of_entry_taxes.parent)
+        .select(
+            bill_of_entry.name,
+            bill_of_entry.posting_date,
+            bill_of_entry.purchase_invoice,
+            bill_of_entry.total_customs_duty,
+            bill_of_entry.total_taxes,
+            bill_of_entry.total_amount_payable,
+            bill_of_entry_taxes.account_head,
+            bill_of_entry_taxes.rate,
+            bill_of_entry_taxes.tax_amount,
+        )
+        .where(bill_of_entry.docstatus == 1)
+        .where(
+            bill_of_entry.bill_of_entry_date[
+                filters.get("from_date") : filters.get("to_date")
+            ]
+        )
+        .where(bill_of_entry.company == filters.get("company"))
+    )
+
+    query = update_purchase_invoice_query(query)
+
+    boe_data = query.run(as_dict=1)
+
+    boe_details = frappe._dict()
+
+    for data in boe_data:
+        detail = frappe._dict(
+            account_head=data.account_head, rate=data.rate, tax_amount=data.tax_amount
+        )
+        inv_data = boe_details.setdefault(
+            data.purchase_invoice,
+            frappe._dict(
+                invoice=data.purchase_invoice,
+                bill_of_entry=data.name,
+                total_customs_duty=data.total_customs_duty,
+                total_taxes=data.total_taxes,
+                total_amount_payable=data.total_amount_payable,
+                account_detail=[],
+            ),
+        )
+
+        if detail not in inv_data.account_detail:
+            inv_data.account_detail.append(detail)
+
+    return boe_details
