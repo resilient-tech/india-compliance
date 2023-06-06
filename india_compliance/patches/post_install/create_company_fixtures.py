d12c65bf95c702e73f7f9dcfe5d6fb713a94269b
Smit Vora <smitvora203@gmail.com>
2023-06-06 17:20:16 +0530

    fix(patch): hardcode root_typr for RCM to avoid duplicate error

--- india_compliance/patches/post_install/create_company_fixtures.py
+++ india_compliance/patches/post_install/create_company_fixtures.py
@@ -23,4 +23,46 @@ def execute():
 
         # GST fixtures
         if not frappe.db.exists("GST Account", {"company": company}):
+            update_root_for_rcm(company)
             create_gst_fixtures(company)
+
+
+def update_root_for_rcm(company):
+    # Root type for RCM had been updated to "Liability".
+    # This will ensure DuplicateEntryError is not raised for RCM accounts.
+
+    # Hardcoded for exact account names only
+    rcm_accounts = frappe.get_all(
+        "Account",
+        filters={
+            "root_type": "Asset",
+            "name": ("like", "Input Tax %GST RCM%"),
+            "company": company,
+        },
+        pluck="name",
+    )
+
+    if not rcm_accounts:
+        return
+
+    output_account = frappe.db.get_value(
+        "Account",
+        {"name": ("like", "Output Tax %GST%"), "company": company},
+        ["parent_account", "root_type"],
+        as_dict=True,
+    )
+
+    if not output_account:
+        abbr = frappe.db.get_value("Company", company, "abbr")
+        output_account = {
+            "parent_account": f"Duties and Taxes - {abbr}",
+            "root_type": "Liability",
+        }
+
+    # update reverse charge accounts
+    frappe.db.set_value(
+        "Account",
+        {"name": ("in", rcm_accounts)},
+        output_account,
+        update_modified=False,
+    )
