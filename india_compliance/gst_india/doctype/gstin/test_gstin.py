f61e043273e7d30813500435d894e1581e32acb6
Sagar Vora <sagar@resilient.tech>
2023-07-11 11:04:35 +0530

    Revert "feat: fetch and save GSTIN details to validate Status in Transactions (backport #793)"

--- india_compliance/gst_india/doctype/gstin/test_gstin.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2023, Resilient Tech and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestGSTIN(FrappeTestCase):
-    pass
