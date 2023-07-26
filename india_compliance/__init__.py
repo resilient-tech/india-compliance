de784ffdc4e721dbaff9cf0e13441c7097e4056a
Sagar Vora <sagar@resilient.tech>
2023-07-26 14:09:26 +0530

    fix: update versions

--- india_compliance/__init__.py
+++ india_compliance/__init__.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 __version__ = "14.9.0"
-=======
-__version__ = "15.0.0-dev"
 
-MIN_FRAPPE_VERSION = "15.0.0-dev"
-MIN_ERPNEXT_VERSION = "15.0.0-dev"
->>>>>>> b8127ceb (fix: improve version patch)
+MIN_FRAPPE_VERSION = "14.42.0"
+MIN_ERPNEXT_VERSION = "14.32.0"
