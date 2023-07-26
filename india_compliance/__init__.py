4ef8cfbbd7749a382a783f4b267684734b5e5ac2
Sagar Vora <sagar@resilient.tech>
2023-07-26 11:32:50 +0000

    fix: improve version check patch (#894)
    (cherry picked from commit 542209c1919babf43444f7c00429a78ea0e6a147)
    
    # Conflicts:
    #	india_compliance/__init__.py

--- india_compliance/__init__.py
+++ india_compliance/__init__.py
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 __version__ = "14.13.0"
 
 MIN_FRAPPE_VERSION = "14.42.0"
 MIN_ERPNEXT_VERSION = "14.32.0"
+=======
+__version__ = "15.0.0-dev"
+>>>>>>> 542209c1 (fix: improve version check patch (#894))
