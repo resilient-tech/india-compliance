d79acff7dd0a0cad395f02012e9c46d39b96c3be
Smit Vora <smitvora203@gmail.com>
2023-08-23 19:33:50 +0530

    chore: better title

--- india_compliance/gst_india/client_scripts/e_waybill_actions.js
+++ india_compliance/gst_india/client_scripts/e_waybill_actions.js
@@ -532,7 +532,7 @@ function show_mark_e_waybill_as_cancelled_dialog(frm) {
     });
 
     const d = new frappe.ui.Dialog({
-        title: __("Update Cancel Details"),
+        title: __("Update e-Waybill Cancel Details"),
         fields: fields,
         primary_action_label: __("Update"),
         primary_action(values) {
