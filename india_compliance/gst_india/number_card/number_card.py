85d6ba01ac4e1ba5439988fb3ad2f4663756d179
Lakshit Jain <108322669+ljain112@users.noreply.github.com>
2023-09-05 11:20:42 +0530

    fix: show e-Invoice summary for all the companies (#1015)

--- india_compliance/gst_india/number_card/number_card.py
+++ india_compliance/gst_india/number_card/number_card.py
@@ -1,46 +1,55 @@
 import frappe
 from frappe.utils import add_months, getdate
-import erpnext
 
 from india_compliance.gst_india.report.e_invoice_summary.e_invoice_summary import (
-    get_data,
+    get_data_for_all_companies,
 )
 
 
 @frappe.whitelist()
-def get_pending_e_invoices_count():
+def get_pending_e_invoices_count(filters=None):
     if not frappe.has_permission("Sales Invoice"):
         return 0
 
-    last_quarter_date = add_months(getdate(), -3)
-    filters = {
-        "company": erpnext.get_default_company(),
-        "exceptions": "e-Invoice Not Generated",
-        "from_date": last_quarter_date,
-        "to_date": getdate(),
-    }
+    default_filters = get_default_filters(filters)
+    default_filters["exceptions"] = "e-Invoice Not Generated"
 
-    return get_e_invoice_summary_count(filters)
+    return get_e_invoice_summary_count(default_filters)
 
 
 @frappe.whitelist()
-def get_active_e_invoice_count_for_cancelled_invoices():
+def get_active_e_invoice_count_for_cancelled_invoices(filters=None):
     if not frappe.has_permission("Sales Invoice"):
         return 0
 
-    last_quarter_date = add_months(getdate(), -3)
-    filters = {
-        "company": erpnext.get_default_company(),
-        "exceptions": "Invoice Cancelled but not e-Invoice",
-        "from_date": last_quarter_date,
-        "to_date": getdate(),
-    }
+    default_filters = get_default_filters(filters)
+    default_filters["exceptions"] = "Invoice Cancelled but e-Invoice Active"
 
-    return get_e_invoice_summary_count(filters)
+    return get_e_invoice_summary_count(default_filters)
+
+
+def get_default_filters(filters=None):
+    if not filters:
+        filters = {}
+
+    if isinstance(filters, str):
+        filters = frappe.json.loads(filters)
+
+    last_quarter_date = add_months(getdate(), -3)
+    default_filters = frappe._dict(
+        {
+            "from_date": last_quarter_date,
+            "to_date": getdate(),
+        }
+    )
+    if filters.get("company"):
+        default_filters["company"] = filters.get("company")
+
+    return default_filters
 
 
 def get_e_invoice_summary_count(filters):
-    e_invoice_summary = get_data(filters)
+    e_invoice_summary = get_data_for_all_companies(filters)
 
     return {
         "value": len(e_invoice_summary),
