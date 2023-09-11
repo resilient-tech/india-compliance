5ed6d34357fca1e302fc1dacfa69ce17874476e9
Smit Vora <smitvora203@gmail.com>
2023-09-11 11:33:21 +0530

    chore: resolve conflicts

--- india_compliance/gst_india/page/india_compliance_account/india_compliance_account.js
+++ india_compliance/gst_india/page/india_compliance_account/india_compliance_account.js
@@ -4,9 +4,5 @@ frappe.pages["india-compliance-account"].on_page_load = async function (wrapper)
         "india_compliance_account.bundle.css",
     ]);
 
-<<<<<<< HEAD
-    new india_compliance.pages.IndiaComplianceAccountPage(wrapper);
-=======
     icAccountPage = new india_compliance.pages.IndiaComplianceAccountPage(wrapper, PAGE_NAME);
->>>>>>> 6bc8b301 (feat: Purchase Reconciliation Tool (#192))
 };
