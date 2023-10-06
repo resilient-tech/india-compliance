ba4a53e007cc4ee0fb385eb524cbfbd722657401
Dany Robert <danyrt@wahni.com>
2023-10-06 09:15:18 +0000

    chore: alert user to (re)run gstin updation patch

--- india_compliance/gst_india/overrides/gl_entry.py
+++ india_compliance/gst_india/overrides/gl_entry.py
@@ -4,6 +4,9 @@ from frappe import _
 from india_compliance.gst_india.overrides.transaction import (
     is_indian_registered_company,
 )
+from india_compliance.gst_india.report.gst_balance.gst_balance import (
+    get_pending_voucher_types,
+)
 from india_compliance.gst_india.utils import get_all_gst_accounts
 
 
@@ -15,9 +18,15 @@ def validate(doc, method=None):
     if doc.account not in gst_accounts:
         return
 
-    frappe.throw(
-        _("Company GSTIN is a mandatory field for accounting of GST Accounts.")
-    )
+    msg = _("Company GSTIN is a mandatory field for accounting of GST Accounts.")
+    if get_pending_voucher_types(doc.company)[0]:
+        # on older transaction where the patch hasn't run succesfully
+        msg += " "
+        msg += _(
+            "Run Update GSTIN patch from GST Balance report to update GSTIN in all transactions."
+        )
+
+    frappe.throw(msg)
 
 
 def update_gl_dict_with_regional_fields(doc, gl_dict):
