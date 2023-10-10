f52e841fd79dd46f7fe03fb24fd99bae7945db02
Smit Vora <smitvora203@gmail.com>
2023-10-10 09:59:23 +0530

    fix(ui): show sandbox mode indicator in transaction doctypes
    (cherry picked from commit 5f7bb242cf20b289cb24c9a5235dc3873929d009)

--- india_compliance/gst_india/client_scripts/delivery_note.js
+++ india_compliance/gst_india/client_scripts/delivery_note.js
@@ -2,6 +2,11 @@ const DOCTYPE = "Delivery Note";
 setup_e_waybill_actions(DOCTYPE);
 
 frappe.ui.form.on(DOCTYPE, {
+    onload(frm) {
+        if (!gst_settings.enable_e_waybill) return;
+        show_sandbox_mode_indicator();
+    },
+
     after_save(frm) {
         if (
             frm.doc.docstatus ||
