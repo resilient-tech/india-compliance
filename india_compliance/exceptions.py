5c838004b80a9c660c705712ef3bb73ddf574df6
Vishakh Desai <vishakhdesai@gmail.com>
2023-12-18 12:43:04 +0530

    fix: handle GSPGSTDOWN error

--- india_compliance/exceptions.py
+++ india_compliance/exceptions.py
@@ -1,4 +1,10 @@
-class GatewayTimeoutError(Exception):
+class GSPServerError(Exception):
+    def __init__(self, message="The GSP GST server is down", *args, **kwargs):
+        self.message = message
+        super().__init__(self.message, *args, **kwargs)
+
+
+class GatewayTimeoutError(GSPServerError):
     def __init__(self, message="The server took too long to respond", *args, **kwargs):
         self.message = message
         super().__init__(self.message, *args, **kwargs)
