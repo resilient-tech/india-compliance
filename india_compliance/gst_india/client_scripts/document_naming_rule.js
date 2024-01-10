8be9e3649ae8d95719ae1bb14c1226d299844cb7
ljain112 <ljain112@gmail.com>
2024-01-10 18:16:50 +0530

    fix: use frappe.require

--- india_compliance/gst_india/client_scripts/document_naming_rule.js
+++ india_compliance/gst_india/client_scripts/document_naming_rule.js
@@ -1,7 +1,7 @@
-frappe.require("assets/india_compliance/js/transaction.js");
-
-frappe.ui.form.on("Document Naming Rule", {
-    document_type(frm) {
-        show_gst_invoice_no_banner(frm);
-    },
+frappe.require("assets/india_compliance/js/transaction.js", function () {
+    frappe.ui.form.on("Document Naming Rule", {
+        async document_type(frm) {
+            await show_gst_invoice_no_banner(frm);
+        },
+    });
 });
--- india_compliance/gst_india/client_scripts/document_naming_settings.js
+++ india_compliance/gst_india/client_scripts/document_naming_settings.js
@@ -1,7 +1,7 @@
-frappe.require("assets/india_compliance/js/transaction.js");
-
-frappe.ui.form.on("Document Naming Settings", {
-    transaction_type(frm) {
-        show_gst_invoice_no_banner(frm);
-    },
+frappe.require("assets/india_compliance/js/transaction.js", function () {
+    frappe.ui.form.on("Document Naming Settings", {
+        transaction_type(frm) {
+            show_gst_invoice_no_banner(frm);
+        },
+    });
 });
