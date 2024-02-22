80d84e686f0e9ed01efef0165b3c581d40b70be8
Ninad Parikh <parikhninad613@gmail.com>
2024-02-22 18:51:54 +0530

    test: test to validate property setter(refactored)

--- india_compliance/audit_trail/overrides/test_property_setter.py
+++ india_compliance/audit_trail/overrides/test_property_setter.py
@@ -10,13 +10,17 @@ class TestPropertySetter(FrappeTestCase):
     ):
         frappe.db.set_single_value("Accounts Settings", "enable_audit_trail", 1)
         doc = frappe.get_doc(
-            "Property Setter",
             {
+                "doctype": "Property Setter",
                 "doctype_or_field": "DocType",
                 "doc_type": "Purchase Invoice",
                 "property": "track_changes",
+                "value": 1,
             },
         )
+        doc.save()
+        doc.value = 0
+
         self.assertRaisesRegex(
             frappe.ValidationError,
             re.compile(r"^(Cannot change the Track Changes property for*)"),
@@ -30,38 +34,3 @@ class TestPropertySetter(FrappeTestCase):
         )
         frappe.db.set_single_value("Accounts Settings", "enable_audit_trail", 0)
         doc.delete()
-
-    def test_validate_property_setter_where_audit_trail_enabled_and_doc_is_not_protected(
-        self,
-    ):
-        frappe.db.set_single_value("Accounts Settings", "enable_audit_trail", 0)
-        doc = frappe.get_doc(
-            "Property Setter",
-            {
-                "doctype_or_field": "DocType",
-                "doc_type": "Purchase Invoice",
-                "property": "track_changes",
-            },
-        )
-        doc.save()
-        frappe.db.set_single_value("Accounts Settings", "enable_audit_trail", 1)
-        doc.doc_type = "Address"
-        self.assertRaisesRegex(
-            frappe.ValidationError,
-            re.compile(r"^(Cannot change the Track Changes property for*)"),
-            doc.save,
-        )
-
-    def test_property_setter_where_frm_is_new_and_doc_not_protected(self):
-        frappe.db.set_single_value("Accounts Settings", "enable_audit_trail", 1)
-        doc = frappe.get_doc(
-            "Property Setter",
-            {
-                "doctype_or_field": "DocField",
-                "doc_type": "Purchase Receipt",
-                "field_name": "in_words",
-                "property": "print_hide",
-                "value": 0,
-            },
-        )
-        doc.save()
