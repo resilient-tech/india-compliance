3cf630eb2dfc2722bbee47d8c38a1d6f68d7ae71
priyanshshah2442 <priyanshshah2442@gmail.com>
2024-02-23 18:08:23 +0530

    test: test case for gst settings

--- india_compliance/gst_india/doctype/gst_settings/test_gst_settings.py
+++ india_compliance/gst_india/doctype/gst_settings/test_gst_settings.py
@@ -1,8 +1,131 @@
 # Copyright (c) 2018, Frappe Technologies Pvt. Ltd. and Contributors
 # See license.txt
+import re
 
-import unittest
+import frappe
+from frappe.tests.utils import FrappeTestCase, change_settings
+from frappe.utils.data import getdate
 
 
-class TestGSTSettings(unittest.TestCase):
-    pass
+class TestGSTSettings(FrappeTestCase):
+
+    @change_settings("GST Settings", {"enable_api": 1})
+    def test_api_key_enabled(self):
+        doc = frappe.get_doc("GST Settings")
+        doc.save()
+
+    def test_validate_duplicate_account(self):
+        doc = frappe.get_doc("GST Settings")
+        doc.append(
+            "gst_accounts",
+            {
+                "company": "_Test Indian Registered Company",
+                "account_type": "Input",
+                "cgst_account": "Input Tax CGST - _TIRC",
+                "sgst_account": "Input Tax SGST - _TIRC",
+                "igst_account": "Input Tax IGST - _TIRC",
+            },
+        )
+        self.assertRaisesRegex(
+            frappe.ValidationError,
+            re.compile(r"^(Row #\d+: Account .* appears multiple times)"),
+            doc.save,
+        )
+        # Validate Duplicate Account Types for each Company
+
+    def test_validate_duplicate_account_type_for_each_company(self):
+        doc = frappe.get_doc("GST Settings")
+        for row in doc.gst_accounts:
+            if (
+                row.company == "_Test Indian Registered Company"
+                and row.account_type == "Reverse Charge"
+            ):
+                row.account_type = "Output"
+                self.assertRaisesRegex(
+                    frappe.ValidationError,
+                    re.compile(
+                        r"^(Row #\d+: Account Type .* appears multiple times for .*)"
+                    ),
+                    doc.save,
+                )
+
+    @change_settings(
+        "GST Settings",
+        {"enable_e_invoice": 1, "apply_e_invoice_only_for_selected_companies": 0},
+    )
+    def test_validate_e_invoice_applicability_date(self):
+        doc = frappe.get_doc("GST Settings")
+        doc.enable_e_invoice = 1
+        doc.e_invoice_applicable_from = ""
+        doc.apply_e_invoice_only_for_selected_companies = 0
+        doc.flags.ignore_mandatory = True
+
+        self.assertRaisesRegex(
+            frappe.ValidationError,
+            re.compile(r"^(.*is mandatory for enabling e-Invoice)"),
+            doc.save,
+        )
+
+        doc = frappe.get_doc("GST Settings")
+        doc.e_invoice_applicable_from = getdate("2020-01-01")
+
+        self.assertRaisesRegex(
+            frappe.ValidationError,
+            re.compile(r"^(.*date cannot be before.*)"),
+            doc.save,
+        )
+
+        doc = frappe.get_doc("GST Settings")
+        doc.apply_e_invoice_only_for_selected_companies = 1
+        company = {
+            "company": "_Test Indian Registered Company",
+            "applicable_from": getdate("01-04-2024"),
+        }
+        doc.append("e_invoice_applicable_companies", company)
+        doc.save()
+
+    @change_settings("GST Settings", {"enable_api": 1})
+    def test_validate_credentials(self):
+        doc = frappe.get_doc("GST Settings")
+        doc.append(
+            "credentials",
+            {
+                "company": "_Test Indian Registered Company",
+                "service": "e-Waybill / e-Invoice",
+                "gstin": "24AAQCA8719H1ZC",
+                "username": "testing1",
+            },
+        )
+        self.assertRaisesRegex(
+            frappe.MandatoryError,
+            re.compile(
+                r"^(Row #\d+: Password is required when setting a GST Credential"
+                " for.*)"
+            ),
+            doc.save,
+        )
+
+        doc = frappe.get_doc("GST Settings")
+        doc.append(
+            "credentials",
+            {
+                "company": "_Test Indian Registered Company",
+                "service": "Returns",
+                "gstin": "24AAQCA8719H1ZC",
+                "username": "testing2",
+            },
+        )
+        doc.save()
+
+        doc = frappe.get_doc("GST Settings")
+        doc.append(
+            "credentials",
+            {
+                "company": "_Test Indian Registered Company",
+                "service": "e-Waybill / e-Invoice",
+                "gstin": "24AAQCA8719H1ZC",
+                "username": "testing2",
+                "password": "TestPass@123",
+            },
+        )
+        doc.save()
