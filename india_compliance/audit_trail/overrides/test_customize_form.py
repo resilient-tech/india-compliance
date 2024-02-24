1d2b55db3e8460b16abd5a0cba5ca38f16e61d7b
Ninad Parikh <parikhninad613@gmail.com>
2024-02-24 16:46:51 +0530

    test: test case to validate customize form

---
+++ india_compliance/audit_trail/overrides/test_customize_form.py
@@ -0,0 +1,22 @@
+import re
+
+import frappe
+from frappe.custom.doctype.customize_form.test_customize_form import TestCustomizeForm
+
+
+class TestPropertySetter(TestCustomizeForm):
+    def test_validate_customize_form(self):
+        frappe.db.set_single_value("Accounts Settings", "enable_audit_trail", 0)
+
+        customize_frm = self.get_customize_form()
+        customize_frm.doc_type = "Purchase Invoice"
+        customize_frm.save_customization()
+
+        frappe.db.set_single_value("Accounts Settings", "enable_audit_trail", 1)
+
+        customize_frm.track_changes = 0
+        self.assertRaisesRegex(
+            frappe.ValidationError,
+            re.compile(r"^(Cannot disable Track Changes for*)"),
+            customize_frm.save_customization,
+        )
