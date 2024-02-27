c48655a33eb543a6d08fb12fa239907c37edf591
priyanshshah2442 <108476017+priyanshshah2442@users.noreply.github.com>
2024-02-27 16:20:35 +0530

    fix: update tds details in tax withholding category (#1688)
    * fix: update tds details in tax withholding category
    
    * fix: linters
    
    * fix: formatting
    
    * fix: update tds_details
    
    * fix: update fields and rates in tax withholding category
    
    * fix: changes as per review
    
    * fix: if condition
    
    * refactor: variable names, update tax category algo, etc
    
    * fix: run patch in post_install
    
    * chore: resolve conflicts
    
    ---------
    
    Co-authored-by: ljain112 <ljain112@gmail.com>
    (cherry picked from commit cea0a670478d8d36840829139400d8eff37f415e)
    
    # Conflicts:
    #	india_compliance/patches.txt

--- india_compliance/income_tax_india/constants/custom_fields.py
+++ india_compliance/income_tax_india/constants/custom_fields.py
@@ -1,3 +1,8 @@
+from india_compliance.income_tax_india.constants import TDS_ENTITY_TYPE, TDS_SECTIONS
+
+tds_section_options = "\n" + "\n".join(sorted(TDS_SECTIONS))
+tds_entity_type_options = "\n" + "\n".join(sorted(TDS_ENTITY_TYPE))
+
 party_fields = [
     {
         "fieldname": "pan",
@@ -23,5 +28,25 @@ CUSTOM_FIELDS = {
                 "If the asset is put to use for less than 180 days in the first year, the first year's depreciation rate will be reduced by 50%."
             ),
         }
+    ],
+    "Tax Withholding Category": [
+        {
+            "label": "Section",
+            "fieldname": "tds_section",
+            "insert_after": "round_off_tax_amount",
+            "fieldtype": "Autocomplete",
+            "options": tds_section_options,
+            "translatable": 0,
+            "mandatory_depends_on": "eval:doc.entity_type",
+        },
+        {
+            "label": "Entity",
+            "fieldname": "entity_type",
+            "insert_after": "tax_on_excess_amount",
+            "fieldtype": "Select",
+            "options": tds_entity_type_options,
+            "translatable": 0,
+            "mandatory_depends_on": "eval:doc.tds_section",
+        },
     ],
 }
--- india_compliance/income_tax_india/setup.py
+++ india_compliance/income_tax_india/setup.py
@@ -1,7 +1,13 @@
-from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
+from frappe.custom.doctype.custom_field.custom_field import (
+    create_custom_fields as _create_custom_fields,
+)
 
 from india_compliance.income_tax_india.constants.custom_fields import CUSTOM_FIELDS
 
 
 def after_install():
-    create_custom_fields(CUSTOM_FIELDS, ignore_validate=True)
+    create_custom_fields()
+
+
+def create_custom_fields():
+    _create_custom_fields(CUSTOM_FIELDS, ignore_validate=True)
