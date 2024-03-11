f7c8798a01ab8d5c8dedfe540cf67c19febd8634
Smit Vora <smitvora203@gmail.com>
2024-03-11 13:29:22 +0530

    feat: gstr-1 filed log

---
+++ india_compliance/gst_india/doctype/gstr_1_filed_log/gstr_1_filed_log.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Resilient Tech and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("GSTR-1 Filed Log", {
+// 	refresh(frm) {
+
+// 	},
+// });
