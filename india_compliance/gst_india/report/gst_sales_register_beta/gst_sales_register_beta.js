3836f750fd6a03c9857628dccc93b6b5d2334723
Sagar Vora <sagar@resilient.tech>
2024-03-22 12:52:03 +0530

    fix: use `set_data` and other minor changes

--- india_compliance/gst_india/report/gst_sales_register_beta/gst_sales_register_beta.js
+++ india_compliance/gst_india/report/gst_sales_register_beta/gst_sales_register_beta.js
@@ -1,18 +1,19 @@
 // Copyright (c) 2024, Resilient Tech and contributors
 // For license information, please see license.txt
 const invoice_type = {
-    "B2B,SEZ,DE": ["", "4A", "4B", "6B", "6C"],
+    "B2B,SEZ,DE": ["4A", "4B", "6B", "6C"],
     "B2C (Large)": ["5"],
-    "Exports": ["", "EXPWP", "EXPWOP"],
+    "Exports": ["EXPWP", "EXPWOP"],
     "B2C (Others)": ["7"],
-    "Nil Rated,Exempted,Non-GST": ["", "Nil-Rated", "Exempted", "Non-GST"],
+    "Nil Rated,Exempted,Non-GST": ["Nil-Rated", "Exempted", "Non-GST"],
     "Credit / Debit notes (Registered)": ["9B"],
     "Credit / Debit notes (Unregistered)": ["9B"],
 }
 
 let invoice_category=""
 frappe.query_reports["GST Sales Register Beta"] = {
-    "filters": [
+    onload: set_sub_category_options,
+    filters: [
         {
             fieldname: "company",
             label: __("Company"),
@@ -37,7 +38,7 @@ frappe.query_reports["GST Sales Register Beta"] = {
             fieldname: "company_gstin",
             label: __("Company GSTIN"),
             fieldtype: "Autocomplete",
-            get_query: function () {
+            get_query() {
                 const company = frappe.query_report.get_filter_value("company");
                 return india_compliance.get_gstin_query(company);
             },
@@ -67,27 +68,20 @@ frappe.query_reports["GST Sales Register Beta"] = {
             fieldname: "invoice_category",
             label: __("Invoice Category"),
             options: "\nB2B,SEZ,DE\nB2C (Large)\nExports\nB2C (Others)\nNil Rated,Exempted,Non-GST\nCredit / Debit notes (Registered)\nCredit / Debit Notes (Unregistered)",
-            on_change:(report)=>{
+            on_change(report) {
 				report.set_filter_value('invoice_sub_category', "");
-
+                set_sub_category_options(report);
             }
         },
         {
             fieldtype: "Autocomplete",
             fieldname: "invoice_sub_category",
             label: __("Invoice Sub Category"),
-            get_data:function(){
-                const invoice_category=frappe.query_report.get_filter_value("invoice_category");
-                return invoice_type[invoice_category];
-            },
-
-            // options:get_sub_category_options(frappe.query_report)
-
         }
     ]
 };
 
-function get_sub_category_options(report) {
-    console.log(report)
-    return invoice_type['B2B,SEZ,DE']
+function set_sub_category_options(report) {
+    const invoice_category = frappe.query_report.get_filter_value("invoice_category");
+    report.get_filter('invoice_sub_category').set_data(invoice_type[invoice_category] || []);
 }
