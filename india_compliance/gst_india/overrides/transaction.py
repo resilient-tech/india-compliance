118a40f784c0284bd31a214569e7532dec63f464
Smit Vora <smitvora203@gmail.com>
2024-03-29 20:43:25 +0530

    fix: set flags for specific updates

--- india_compliance/gst_india/overrides/transaction.py
+++ india_compliance/gst_india/overrides/transaction.py
@@ -1400,7 +1400,14 @@ def ignore_gst_validations(doc):
         return True
 
 
+def before_update_after_submit_item(doc, method=None):
+    frappe.flags.through_update_item = True
+
+
 def before_update_after_submit(doc, method=None):
+    if not frappe.flags.through_update_item:
+        return
+
     if ignore_gst_validations(doc):
         return
 
