12e417ec04e5d79eeaa398568682fd5598f9e041
Smit Vora <smitvora203@gmail.com>
2024-03-29 20:37:17 +0530

    chore: remove unnecessary return
    (cherry picked from commit 34e36147e7c1dfee0b350a2d700efd9e011ed13d)

--- india_compliance/gst_india/overrides/transaction.py
+++ india_compliance/gst_india/overrides/transaction.py
@@ -1498,7 +1498,7 @@ def ignore_logs_on_trash(doc, method=None):
 =======
 def before_update_after_submit(doc, method=None):
     if ignore_gst_validations(doc):
-        return False
+        return
 
     validate_items(doc)
 
