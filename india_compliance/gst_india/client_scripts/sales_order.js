6ec174752a146200c17887776c6c251ee103e070
priyanshshah2442 <priyanshshah2442@gmail.com>
2024-05-21 18:37:32 +0530

    fix: as per review

--- india_compliance/gst_india/client_scripts/sales_order.js
+++ india_compliance/gst_india/client_scripts/sales_order.js
@@ -8,12 +8,3 @@ frappe.ui.form.on(DOCTYPE, {
         india_compliance.toggle_and_set_supply_liable_to(frm)
     }
 })
-
-function set_supply_liable_to(frm) {
-    if (frm.doc.is_reverse_charge) {
-        frm.set_value("supply_liable_to", "Reverse Charge u/s 9(5)")
-    }
-    else {
-        frm.set_value("supply_liable_to", "Collect Tax u/s 52")
-    }
-}
--- india_compliance/patches/v14/enable_sales_through_ecommerce_operator.py
+++ india_compliance/patches/v14/enable_sales_through_ecommerce_operator.py
@@ -3,8 +3,8 @@ import frappe
 
 def execute():
     gst_settings = frappe.get_cached_doc("GST Settings")
-    for doctype in ["Sales Invoice", "Sales Order", "Delivery Note"]:
-        if frappe.db.exists(doctype, {"ecommerce_gstin": ["!=", ""]}):
-            gst_settings.enable_sales_through_ecommerce_operators = 1
-            gst_settings.save()
-            return
+
+    if frappe.db.exists("Sales Invoice", {"ecommerce_gstin": ["!=", ""]}):
+        gst_settings.enable_sales_through_ecommerce_operators = 1
+        gst_settings.save()
+        return
