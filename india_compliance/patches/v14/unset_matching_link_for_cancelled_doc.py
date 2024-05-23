e3dd0562fcad156189590124e549f9a0a5739e01
Sanket322 <shahsanket322003@gmail.com>
2024-05-23 14:02:41 +0530

    fix: patch improved

--- india_compliance/patches/v14/unset_matching_link_for_cancelled_doc.py
+++ india_compliance/patches/v14/unset_matching_link_for_cancelled_doc.py
@@ -1,24 +1,21 @@
 import frappe
 
+DOCTYPES = ("Purchase Invoice", "Bill of Entry")
+
 
 def execute():
-    import_logs = frappe.get_all(
-        "GST Inward Supply",
-        filters={
-            "link_doctype": ("in", ("Purchase Invoice", "Bill of Entry")),
-        },
-        fields=["link_name", "link_doctype", "name"],
-    )
-
-    for log in import_logs:
-        doc_status = frappe.db.get_value(
-            log.get("link_doctype"), log.get("link_name"), "docstatus"
-        )
-        if doc_status != 2:
-            continue
-
+    for doctype in DOCTYPES:
+        cancelled_doc = get_cancelled_doc(doctype)
         frappe.db.set_value(
             "GST Inward Supply",
-            log.get("name"),
+            {"link_name": ("in", cancelled_doc), "link_doctype": doctype},
             {"link_name": "", "link_doctype": "", "match_status": ""},
         )
+
+
+def get_cancelled_doc(doctype):
+    return frappe.get_all(
+        doctype,
+        filters={"docstatus": 2},
+        pluck="name",
+    )
