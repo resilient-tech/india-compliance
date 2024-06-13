9eb2b1173002990af185a40a3353b6aa5d913858
Smit Vora <smitvora203@gmail.com>
2024-06-13 16:33:08 +0530

    chore: rename func, comments

--- india_compliance/gst_india/report/gstr_1/gstr_1.py
+++ india_compliance/gst_india/report/gstr_1/gstr_1.py
@@ -112,7 +112,7 @@ class Gstr1Report:
         elif self.filters.get("type_of_business") == "HSN":
             self.data = get_hsn_data(self.filters, self.columns, self.gst_accounts)
         elif self.filters.get("type_of_business") == "Section 14":
-            self.data = self.get_section_14_data()
+            self.data = self.get_data_for_supplies_through_ecommerce_operators()
         elif self.invoices:
             for inv, items_based_on_rate in self.items_based_on_tax_rate.items():
                 invoice_details = self.invoices.get(inv)
@@ -561,7 +561,7 @@ class Gstr1Report:
                     0, []
                 ).extend(items)
 
-    def get_section_14_data(self):
+    def get_data_for_supplies_through_ecommerce_operators(self):
         si = frappe.qb.DocType("Sales Invoice")
         si_item = frappe.qb.DocType("Sales Invoice Item")
         taxes = frappe.qb.DocType("Sales Taxes and Charges")
@@ -571,6 +571,7 @@ class Gstr1Report:
         cess_account = get_escaped_name(self.gst_accounts.cess_account)
         cess_non_advol = get_escaped_name(self.gst_accounts.cess_non_advol_account)
 
+        # subquery to get total taxable value
         taxable_value_query = (
             frappe.qb.from_(si_item)
             .select(
@@ -580,6 +581,7 @@ class Gstr1Report:
             .groupby(si_item.parent)
         )
 
+        # subquery to get total taxes
         taxes_query = (
             frappe.qb.from_(taxes)
             .select(
