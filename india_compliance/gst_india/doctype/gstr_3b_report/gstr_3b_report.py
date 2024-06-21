2052a982b1b61f54cd56dd6e4ebe1f13d1d35c18
Sanket322 <113279972+Sanket322@users.noreply.github.com>
2024-06-21 17:49:52 +0530

    fix: add gst_tax_type field (backport #2241)
    * fix: add gst_tax_type field
    
    * fix: added patch
    
    * fix: corrections based on test-cases
    
    * fix: changes as per review
    
    * resolved test-case
    
    * fix: fixing some codacy issues
    
    * refactor: patch, and usage of tax types
    
    * fix: changes as per review
    
    * fix: changes as per review
    
    * chore: rename and file position
    
    ---------
    
    Co-authored-by: ljain112 <ljain112@gmail.com>
    (cherry picked from commit cf1a43e107aeda1d6a395af61350bfadf29b0ab5)

--- india_compliance/gst_india/doctype/gstr_3b_report/gstr_3b_report.py
+++ india_compliance/gst_india/doctype/gstr_3b_report/gstr_3b_report.py
@@ -17,7 +17,6 @@ from india_compliance.gst_india.overrides.transaction import is_inter_state_supp
 from india_compliance.gst_india.report.gstr_3b_details.gstr_3b_details import (
     IneligibleITC,
 )
-from india_compliance.gst_india.utils import get_gst_accounts_by_type
 
 VALUES_TO_UPDATE = ["iamt", "camt", "samt", "csamt"]
 
@@ -230,7 +229,6 @@ class GSTR3BReport(Document):
     def update_imports_from_bill_of_entry(self, itc_details):
         boe = frappe.qb.DocType("Bill of Entry")
         boe_taxes = frappe.qb.DocType("Bill of Entry Taxes")
-        gst_accounts = get_gst_accounts_by_type(self.company, "Input")
 
         def _get_tax_amount(account_type):
             return (
@@ -245,12 +243,12 @@ class GSTR3BReport(Document):
                     )
                     & boe.company_gstin.eq(self.gst_details.get("gstin"))
                     & boe.docstatus.eq(1)
-                    & boe_taxes.account_head.eq(gst_accounts[account_type])
+                    & boe_taxes.gst_tax_type.eq(account_type)
                 )
                 .run()
             )[0][0] or 0
 
-        igst, cess = _get_tax_amount("igst_account"), _get_tax_amount("cess_account")
+        igst, cess = _get_tax_amount("igst"), _get_tax_amount("cess")
         itc_details.setdefault("Import Of Goods", {"iamt": 0, "csamt": 0})
         itc_details["Import Of Goods"]["iamt"] += igst
         itc_details["Import Of Goods"]["csamt"] += cess
@@ -316,12 +314,13 @@ class GSTR3BReport(Document):
     def set_item_wise_tax_details(self, docs):
         self.invoice_item_wise_tax_details = {}
         item_wise_details = {}
-        account_head_gst_map = {}
-
-        for key, values in self.account_heads.items():
-            for value in values:
-                if value is not None:
-                    account_head_gst_map[value] = key
+        gst_tax_type_map = {
+            "cgst": "camt",
+            "sgst": "samt",
+            "igst": "iamt",
+            "cess": "csamt",
+            "cess_non_advol": "csamt",
+        }
 
         item_defaults = frappe._dict(
             {
@@ -357,7 +356,7 @@ class GSTR3BReport(Document):
 
             # Process tax details
             for tax in details["taxes"]:
-                gst_tax_type = account_head_gst_map.get(tax.account_head)
+                gst_tax_type = gst_tax_type_map.get(tax.gst_tax_type)
 
                 if not gst_tax_type:
                     continue
@@ -451,7 +450,7 @@ class GSTR3BReport(Document):
         tax_details = frappe.db.sql(
             f"""
             SELECT
-                parent, account_head, item_wise_tax_detail
+                parent, item_wise_tax_detail, gst_tax_type
             FROM `tab{tax_template}`
             WHERE
                 parenttype = %s and docstatus = 1
