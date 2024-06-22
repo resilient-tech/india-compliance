f2fb04f1c9458e4ccbde023ddc6530b24240dd71
Smit Vora <smitvora203@gmail.com>
2024-06-22 16:03:40 +0530

    chore: revert changes related to print format as not supported in v1

--- india_compliance/gst_india/overrides/company.py
+++ india_compliance/gst_india/overrides/company.py
@@ -226,14 +226,3 @@ def create_default_company_account(
         frappe.db.set_value(
             "Company", company, default_fieldname, account.name, update_modified=False
         )
-<<<<<<< HEAD
-=======
-
-
-@frappe.whitelist()
-def get_default_print_options(for_bank=1) -> list:
-    if int(for_bank):
-        return ["Account No.", "Bank Name", "Branch", "IFSC Code", "UPI ID"]
-    else:
-        return ["MSME No.", "MSME Type", "LLPIN", "LUT No."]
->>>>>>> dd9d551c (fix: parse param correctly in py)
