6b2b66797433800d4b7e2e7376061177ce1e92be
Smit Vora <mailsmitvora@gmail.com>
2024-06-22 12:13:04 +0530

    feat: support ecommerce sale from GSTR-1 (backport: #2125) (#2281)
    * fix: custom fields and validations for section 14
    
    * fix: failing linters
    
    * fix: failing linter in js file
    
    * fix: update patch numbering
    
    * fix: minor changes ton validations
    
    * fix: supply_liable field to virtual field and remove all backend validations
    
    * fix: depends on for supply_liable_to field
    
    * fix: added section-14 in gstr-1 report
    
    * fix: download json and excel for section-14
    
    * fix: failing linters
    
    * fix: as per review
    
    * fix: as per review
    
    * fix: modified query for section_14 data
    
    * fix: as per review
    
    * fix: changes as per review
    
    * fix: gstr-1 columns
    
    * fix: re-write section-14 query
    
    * fix: query for section-14
    
    * fix: escape account name for query
    
    * refactor: field names and functions related to ecommerce supply type in GST compliance modules
    
    * fix: formatting fix
    
    * chore: better label
    
    * refactor: section-14 query
    
    * chore: rename func, comments
    
    * fix: supecommerce categories
    
    ---------
    
    Co-authored-by: priyanshshah2442 <priyanshshah2442@gmail.com>
    Co-authored-by: ljain112 <ljain112@gmail.com>
    (cherry picked from commit 7bd55826f3fd080fa3e37a4afc2ae8e26d7635ef)
    
    # Conflicts:
    #	india_compliance/gst_india/overrides/transaction.py

--- india_compliance/gst_india/overrides/transaction.py
+++ india_compliance/gst_india/overrides/transaction.py
@@ -33,6 +33,7 @@ from india_compliance.gst_india.utils import (
     validate_gst_category,
     validate_gstin,
 )
+from india_compliance.gst_india.utils.gstr_1 import SUPECOM
 from india_compliance.income_tax_india.overrides.tax_withholding_category import (
     get_tax_withholding_accounts,
 )
@@ -1516,7 +1517,6 @@ def validate_transaction(doc, method=None):
     validate_ecommerce_gstin(doc)
 
     validate_gst_category(doc.gst_category, gstin)
-
     valid_accounts = validate_gst_accounts(doc, is_sales_transaction) or ()
     update_taxable_values(doc, valid_accounts)
     validate_item_wise_tax_detail(doc, valid_accounts)
@@ -1530,10 +1530,12 @@ def before_print(doc, method=None, print_settings=None):
     ):
         return
 
+    set_ecommerce_supply_type(doc)
     set_gst_breakup(doc)
 
 
 def onload(doc, method=None):
+
     if (
         ignore_gst_validations(doc, throw=False)
         or not doc.place_of_supply
@@ -1541,6 +1543,7 @@ def onload(doc, method=None):
     ):
         return
 
+    set_ecommerce_supply_type(doc)
     set_gst_breakup(doc)
 
 
@@ -1618,6 +1621,7 @@ def before_update_after_submit(doc, method=None):
     update_gst_details(doc)
 
 
+<<<<<<< HEAD
 # Note: This is kept for backwards compatibility with Frappe versions < 14.21.0
 def ignore_logs_on_trash(doc, method=None):
     if (
@@ -1630,3 +1634,19 @@ def ignore_logs_on_trash(doc, method=None):
         "e-Waybill Log",
         "e-Invoice Log",
     )
+=======
+def set_ecommerce_supply_type(doc):
+    """
+    - Set GSTR-1 E-commerce section for virtual field ecommerce_supply_type
+    """
+    if doc.doctype not in ("Sales Order", "Sales Invoice", "Delivery Note"):
+        return
+
+    if not doc.ecommerce_gstin:
+        return
+
+    if doc.is_reverse_charge:
+        doc.ecommerce_supply_type = SUPECOM.US_9_5.value
+    else:
+        doc.ecommerce_supply_type = SUPECOM.US_52.value
+>>>>>>> 7bd55826 (feat: support ecommerce sale from GSTR-1 (backport: #2125) (#2281))
