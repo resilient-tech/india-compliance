c1606f7c3f9760b6d45e0c2b48f05cbe2189f935
Lakshit Jain <108322669+ljain112@users.noreply.github.com>
2024-06-24 15:50:16 +0530

    fix(ineligible itc): update GL's with valuation hook, refactor (#2206)
    - Update valuation using regional override
    - Using GST Details for calculating ineligible amount
    - Modified Report
    
    Depends On: https://github.com/frappe/erpnext/pull/41842
    
    Closes: #2193
    Frappe Support: https://support.frappe.io/app/hd-ticket/15474
    
    (cherry picked from commit f3133e3884332c45b8feb48035d0b5910760e21e)
    
    # Conflicts:
    #	india_compliance/patches/check_version_compatibility.py

--- india_compliance/gst_india/overrides/transaction.py
+++ india_compliance/gst_india/overrides/transaction.py
@@ -211,7 +211,7 @@ def is_indian_registered_company(doc):
     return True
 
 
-def validate_mandatory_fields(doc, fields, error_message=None):
+def validate_mandatory_fields(doc, fields, error_message=None, throw=True):
     if isinstance(fields, str):
         fields = (fields,)
 
@@ -223,6 +223,9 @@ def validate_mandatory_fields(doc, fields, error_message=None):
             continue
 
         if doc.flags.ignore_mandatory:
+            return False
+
+        if not throw:
             return False
 
         frappe.throw(
