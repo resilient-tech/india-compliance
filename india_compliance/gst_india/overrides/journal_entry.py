1dad658c97338be408a6d3295968e89731b45cdc
Sanket322 <113279972+Sanket322@users.noreply.github.com>
2024-07-05 16:06:45 +0530

    fix: GST Tax Type in Journal Entry (#2328)
    closes : #2300
    
    <sub><a href="https://huly.app/guest/resilienttech?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJsaW5rSWQiOiI2NjgzYmJjYjJhNmFiOWVlZjU1OWM1YzkiLCJndWVzdCI6InRydWUiLCJlbWFpbCI6IiNndWVzdEBoYy5lbmdpbmVlcmluZyIsIndvcmtzcGFjZSI6Inctc21pdHZvcmEyMDMtcmVzaWxpZW50dGVjLTY2N2U0MjkxLWEwNWMwNjY4N2EtNjM4MjY3IiwicHJvZHVjdElkIjoiIn0.KwTSSRuPF5gUkqxvuhvwcuuMjyz0ZdciGDt-h4dgNRU">Huly&reg;: <b>IC-2441</b></a></sub>
    
    (cherry picked from commit 41b349a42e01ae86927cfaa0ebd424e6d7802f8a)

--- india_compliance/gst_india/overrides/journal_entry.py
+++ india_compliance/gst_india/overrides/journal_entry.py
@@ -4,7 +4,20 @@ from frappe import _
 from india_compliance.gst_india.overrides.transaction import (
     is_indian_registered_company,
 )
-from india_compliance.gst_india.utils import get_all_gst_accounts
+from india_compliance.gst_india.utils import get_gst_account_gst_tax_type_map
+
+
+def before_validate(doc, method=None):
+    if not doc.accounts:
+        return
+
+    gst_tax_account_map = get_gst_account_gst_tax_type_map()
+    if not gst_tax_account_map:
+        return
+
+    for tax in doc.accounts:
+        # Setting as None if not GST Account
+        tax.gst_tax_type = gst_tax_account_map.get(tax.account)
 
 
 def validate(doc, method=None):
@@ -13,9 +26,8 @@ def validate(doc, method=None):
 
     # validate company_gstin
     contains_gst_account = False
-    gst_accounts = get_all_gst_accounts(doc.company)
     for row in doc.accounts:
-        if row.account in gst_accounts:
+        if row.gst_tax_type:
             contains_gst_account = True
             break
 
