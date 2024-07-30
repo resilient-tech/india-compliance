4ea41fe86b7909c31f8b06d0eb0954aaa4ba273c
Ninad1306 <ninad_1063@yahoo.com>
2024-07-30 18:15:02 +0530

    fix: redundant doctype

--- india_compliance/gst_india/doctype/reference_document/reference_document.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, Resilient Tech and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class ReferenceDocument(Document):
-    pass
