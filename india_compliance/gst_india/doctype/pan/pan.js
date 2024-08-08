531efe23459a14059089a3b76d741b80af089e43
Smit Vora <smitvora203@gmail.com>
2024-08-08 14:04:04 +0530

    fix: pan doc consistent with GSTIN
    (cherry picked from commit 1c125a99e62d7973bdf87c4b0c4c1465ea5d513f)

--- india_compliance/gst_india/doctype/pan/pan.js
+++ india_compliance/gst_india/doctype/pan/pan.js
@@ -1,8 +1,10 @@
 // Copyright (c) 2024, Resilient Tech and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on("Pan", {
+frappe.ui.form.on("PAN", {
     refresh(frm) {
+        frm.disable_form();
+
         frm.add_custom_button(__("Refresh PAN Status"), () => {
             frm.call("update_pan_status");
         });
