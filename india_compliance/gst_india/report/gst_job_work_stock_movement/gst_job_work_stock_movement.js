fa1e72035ae40ba675594e7082319f54e586d9cc
Smit Vora <smitvora203@gmail.com>
2024-08-20 09:08:17 +0530

    fix(ux): enhance ux with default filters and other fixes
    (cherry picked from commit 37cb3632b429d955d96943b695a68a1ab77e61fa)

--- india_compliance/gst_india/report/gst_job_work_stock_movement/gst_job_work_stock_movement.js
+++ india_compliance/gst_india/report/gst_job_work_stock_movement/gst_job_work_stock_movement.js
@@ -5,31 +5,54 @@ frappe.query_reports["GST Job Work Stock Movement"] = {
 	filters: [
 		{
 			fieldname: "company",
-			label: "Company",
+			label: __("Company"),
 			fieldtype: "Link",
 			options: "Company",
+			default: frappe.defaults.get_user_default("Company"),
+			on_change: report => {
+				report.set_filter_value({
+					company_gstin: "",
+				});
+				report.refresh();
+			},
+			get_query: function () {
+				return {
+					filters: {
+						country: "India",
+					},
+				};
+			},
 		},
 		{
 			fieldname: "company_gstin",
-			label: "Company GSTIN",
-			fieldtype: "Data",
+			label: __("Company GSTIN"),
+			fieldtype: "Autocomplete",
+			get_query() {
+				const company = frappe.query_report.get_filter_value("company");
+				return india_compliance.get_gstin_query(company);
+			},
 		},
 		{
 			fieldname: "from_date",
-			label: "From Date",
+			label: __("From Date"),
 			fieldtype: "Date",
+			default: india_compliance.last_half_year("start"),
+			reqd: 1,
 		},
 		{
 			fieldname: "to_date",
-			label: "To Date",
+			label: __("To Date"),
 			fieldtype: "Date",
+			default: india_compliance.last_half_year("end"),
+			reqd: 1,
 		},
 		{
 			fieldname: "category",
-			label: "Invoice Category",
+			label: __("Invoice Category"),
 			fieldtype: "Select",
-			options: ["", "Table 4", "Table 5A"],
+			options: ["Sent for Job Work (Table 4)", "Received back from Job Worker (Table 5A)"],
 			reqd: 1
 		},
 	],
 };
+
