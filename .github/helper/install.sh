a732f51bd2f9b9ad7772aeb7f626e91f8b3e060d
Sanket322 <shahsanket322003@gmail.com>
2024-08-22 17:42:06 +0530

    fix: running all test cases

--- .github/helper/install.sh
+++ .github/helper/install.sh
@@ -60,10 +60,6 @@ sed -i 's/redis_socketio:/# redis_socketio:/g' Procfile
 
 bench get-app erpnext --branch "$BRANCH_TO_CLONE" --resolve-deps
 
-if [ "$RUN_RESTORE" == "false" ]; then
-    bench --site test_site restore ${GITHUB_WORKSPACE}/20240822_162707-a1_localhost-database.sql.gz
-fi
-
 bench get-app india_compliance "${GITHUB_WORKSPACE}"
 bench setup requirements --dev
 
@@ -71,7 +67,11 @@ wait $wkpid
 
 bench use test_site
 bench start &
-# bench reinstall --yes
+bench reinstall --yes
+
+if [ "${RUN_RESTORE:-false}" == "true" ]; then
+    bench --site test_site restore ${GITHUB_WORKSPACE}/20240822_162707-a1_localhost-database.sql.gz
+fi
 
 bench --verbose install-app india_compliance
 bench --site test_site add-to-hosts
