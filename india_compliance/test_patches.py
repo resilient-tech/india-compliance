7ac6c00215075b601695e8d42d7876d86287418d
Sanket322 <shahsanket322003@gmail.com>
2024-08-23 14:46:03 +0530

    fix: test patches of patch.txt exists

--- india_compliance/test_patches.py
+++ india_compliance/test_patches.py
@@ -1,4 +1,5 @@
 import frappe
+from frappe.modules.patch_handler import get_patches_from_app
 from frappe.tests.utils import FrappeTestCase
 
 from india_compliance.install import POST_INSTALL_PATCHES
@@ -12,3 +13,22 @@ class TestPatches(FrappeTestCase):
                     f"india_compliance.patches.post_install.{patch}.execute"
                 )
             )
+
+    def test_patches_exists(self):
+        patches = get_patches_from_app("india_compliance")
+
+        for patch in patches:
+            if patch.startswith("execute:"):
+                import_path = patch.split("execute:")[1]
+
+                if not import_path.startswith("from"):
+                    continue
+
+                components = import_path.split("from")[1].split()
+                module = components[0]
+                function_name = components[2].replace(";", "").replace(",", "")
+                patch_path = module + "." + function_name
+            else:
+                patch_path = f"{patch.split(maxsplit=1)[0]}.execute"
+
+            frappe.get_attr(patch_path)
