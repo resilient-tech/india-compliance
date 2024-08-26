b2d0e33ee9bae242cc26c3103296de0b6c2f883b
Ninad1306 <ninad_1063@yahoo.com>
2024-08-26 11:23:14 +0530

    fix: minor fix related to test_case

--- india_compliance/gst_india/overrides/test_purchase_invoice.py
+++ india_compliance/gst_india/overrides/test_purchase_invoice.py
@@ -1,8 +1,13 @@
 import unittest
 
+import frappe
+from frappe.tests.utils import change_settings
+from erpnext.accounts.doctype.account.test_account import create_account
+
 from india_compliance.gst_india.utils.tests import append_item, create_purchase_invoice
 
 
+@change_settings("GST Settings", {"enable_overseas_transactions": 1})
 class TestPurchaseInvoice(unittest.TestCase):
     def test_itc_classification(self):
         pinv = create_purchase_invoice(
@@ -27,6 +32,16 @@ class TestPurchaseInvoice(unittest.TestCase):
         pinv.save()
         self.assertEqual(pinv.itc_classification, "All Other ITC")
 
+        company = "_Test Indian Registered Company"
+        account = create_account(
+            account_name="Unrealized Profit",
+            parent_account="Current Assets - _TIRC",
+            company=company,
+        )
+
+        frappe.db.set_value(
+            "Company", company, "unrealized_profit_loss_account", account
+        )
         pinv = create_purchase_invoice(
             supplier="Test Internal with ISD Supplier",
             qty=-1,
