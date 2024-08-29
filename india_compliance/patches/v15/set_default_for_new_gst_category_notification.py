746528df873b7396c0ddde2a041b8d97991bce96
Smit Vora <smitvora203@gmail.com>
2024-08-29 13:04:25 +0530

    fix: show notification conditionally

--- india_compliance/patches/v15/set_default_for_new_gst_category_notification.py
+++ india_compliance/patches/v15/set_default_for_new_gst_category_notification.py
@@ -7,6 +7,12 @@ def execute():
     if not frappe.db.exists("Company", {"country": "India"}):
         return
 
+    # Users affected by creation of new GST category
+    if not frappe.db.exists(
+        "Purchase Invoice", {"itc_classification": "Input Service Distributor"}
+    ):
+        return
+
     for user in get_users_with_role("Accounts Manager"):
         frappe.defaults.set_user_default(
             "needs_new_gst_category_notification", 1, user=user
