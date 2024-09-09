eb584ece780a6a5f286e344e90778282f8eee2a6
Smit Vora <smitvora203@gmail.com>
2024-09-09 08:21:46 +0530

    chore: consistent formatting with `spaces`

--- india_compliance/gst_india/doctype/gstr_3b_report/gstr_3b_report.js
+++ india_compliance/gst_india/doctype/gstr_3b_report/gstr_3b_report.js
@@ -2,57 +2,57 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('GSTR 3B Report', {
-	refresh : function(frm) {
-		frm.doc.__unsaved = 1;
-		if(!frm.is_new()) {
-			frm.set_intro(__("Please save the report again to rebuild or update"));
-			frm.add_custom_button(__('Download JSON'), function() {
-				var w = window.open(
-					frappe.urllib.get_full_url(
-						"/api/method/india_compliance.gst_india.doctype.gstr_3b_report.gstr_3b_report.make_json?"
-						+"name="+encodeURIComponent(frm.doc.name)));
+    refresh: function (frm) {
+        frm.doc.__unsaved = 1;
+        if (!frm.is_new()) {
+            frm.set_intro(__("Please save the report again to rebuild or update"));
+            frm.add_custom_button(__('Download JSON'), function () {
+                var w = window.open(
+                    frappe.urllib.get_full_url(
+                        "/api/method/india_compliance.gst_india.doctype.gstr_3b_report.gstr_3b_report.make_json?"
+                        + "name=" + encodeURIComponent(frm.doc.name)));
 
-				if(!w) {
-					frappe.msgprint(__("Please enable pop-ups")); return;
-				}
-			});
-			frm.add_custom_button(__('View Form'), function() {
-				frappe.call({
-					"method" : "india_compliance.gst_india.doctype.gstr_3b_report.gstr_3b_report.view_report",
-					"args" : {
-						name : frm.doc.name,
-					},
-					"callback" : function(r){
+                if (!w) {
+                    frappe.msgprint(__("Please enable pop-ups")); return;
+                }
+            });
+            frm.add_custom_button(__('View Form'), function () {
+                frappe.call({
+                    "method": "india_compliance.gst_india.doctype.gstr_3b_report.gstr_3b_report.view_report",
+                    "args": {
+                        name: frm.doc.name,
+                    },
+                    "callback": function (r) {
 
-						let data = r.message;
+                        let data = r.message;
 
-						frappe.ui.get_print_settings(false, print_settings => {
+                        frappe.ui.get_print_settings(false, print_settings => {
 
-							frappe.render_grid({
-								template: 'gstr_3b_report',
-								title: __(this.doctype),
-								print_settings: print_settings,
-								data: data,
-								columns:[]
-							});
-						});
-					}
-				});
-			});
-		}
+                            frappe.render_grid({
+                                template: 'gstr_3b_report',
+                                title: __(this.doctype),
+                                print_settings: print_settings,
+                                data: data,
+                                columns: []
+                            });
+                        });
+                    }
+                });
+            });
+        }
 
-		let current_year = new Date().getFullYear();
-		let options = [current_year, current_year-1, current_year-2];
-		frm.set_df_property('year', 'options', options);
+        let current_year = new Date().getFullYear();
+        let options = [current_year, current_year - 1, current_year - 2];
+        frm.set_df_property('year', 'options', options);
 
-		frappe.realtime.on("gstr3b_report_generation", function(){
-			frm.reload_doc();
-		});
+        frappe.realtime.on("gstr3b_report_generation", function () {
+            frm.reload_doc();
+        });
 
         append_form(frm);
-	},
+    },
 
-	setup: function (frm) {
+    setup: function (frm) {
         frm.set_query("company_gstin", function (doc) {
             if (!doc.company) {
                 frappe.throw(__("Please set Company"));
