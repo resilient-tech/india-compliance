37ab29753ace61790ee38051d741d40dd7f04fc0
Ninad1306 <ninad_1063@yahoo.com>
2024-09-09 16:36:15 +0530

    fix: month or quarter for gstr3b_details

--- india_compliance/gst_india/report/gstr_3b_details/gstr_3b_details.py
+++ india_compliance/gst_india/report/gstr_3b_details/gstr_3b_details.py
@@ -8,6 +8,8 @@ from frappe.query_builder.custom import ConstantColumn
 from frappe.query_builder.functions import Extract, Ifnull, IfNull, LiteralValue, Sum
 from frappe.utils import cint, get_first_day, get_last_day
 
+from india_compliance.gst_india.utils import get_period
+
 
 def execute(filters=None):
     if not filters.get("section"):
@@ -52,11 +54,12 @@ class BaseGSTR3BDetails:
             },
         ]
         self.data = []
+        self.month_or_quarter_no = get_period(self.filters.month_or_quarter)
         self.from_date = get_first_day(
-            f"{cint(self.filters.year)}-{cint(self.filters.month)}-01"
+            f"{cint(self.filters.year)}-{self.month_or_quarter_no[0]}-01"
         )
         self.to_date = get_last_day(
-            f"{cint(self.filters.year)}-{cint(self.filters.month)}-01"
+            f"{cint(self.filters.year)}-{self.month_or_quarter_no[1]}-01"
         )
         self.company = self.filters.company
         self.company_gstin = self.filters.company_gstin
@@ -272,14 +275,20 @@ class GSTR3B_ITC_Details(BaseGSTR3BDetails):
 
     def get_ineligible_itc_from_purchase(self):
         ineligible_itc = IneligibleITC(
-            self.company, self.company_gstin, self.filters.month, self.filters.year
+            self.company,
+            self.company_gstin,
+            self.month_or_quarter_no,
+            self.filters.year,
         ).get_for_purchase("Ineligible As Per Section 17(5)")
 
         return self.process_ineligible_itc(ineligible_itc)
 
     def get_ineligible_itc_from_boe(self):
         ineligible_itc = IneligibleITC(
-            self.company, self.company_gstin, self.filters.month, self.filters.year
+            self.company,
+            self.company_gstin,
+            self.month_or_quarter_no,
+            self.filters.year,
         ).get_for_bill_of_entry()
 
         return self.process_ineligible_itc(ineligible_itc)
@@ -461,7 +470,7 @@ class IneligibleITC:
         return query.groupby(dt[group_by]).run(as_dict=True)
 
     def get_common_query(self, doctype, dt, dt_item):
-        query = (
+        return (
             frappe.qb.from_(dt)
             .join(dt_item)
             .on(dt.name == dt_item.parent)
@@ -477,18 +486,10 @@ class IneligibleITC:
             .where(dt.docstatus == 1)
             .where(dt.company_gstin == self.gstin)
             .where(dt.company == self.company)
-            .where(Extract(DatePart.year, dt.posting_date).eq(self.year))
-        )
-
-        if isinstance(self.month_or_quarter, tuple):
-            query = query.where(
+            .where(
                 Extract(DatePart.month, dt.posting_date).between(
                     self.month_or_quarter[0], self.month_or_quarter[1]
                 )
             )
-        else:
-            query = query.where(
-                Extract(DatePart.month, dt.posting_date).eq(self.month_or_quarter)
-            )
-
-        return query
+            .where(Extract(DatePart.year, dt.posting_date).eq(self.year))
+        )
