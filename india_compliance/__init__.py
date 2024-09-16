937a2657761bb521c93364dfa87a2ad82e52e48a
Smit Vora <smitvora203@gmail.com>
2024-09-16 21:21:55 +0530

    chore: resolve conflicts

--- india_compliance/__init__.py
+++ india_compliance/__init__.py
@@ -1,10 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 __version__ = "15.0.2"
-=======
-from .exceptions import *
-
-=======
->>>>>>> 7e1c4af5 (fix: make a request to check if auth token is valid before enqueue)
-__version__ = "16.0.0-dev"
->>>>>>> 7afa94c7 (fix: enhanced otp handeling taxpayer apis)
