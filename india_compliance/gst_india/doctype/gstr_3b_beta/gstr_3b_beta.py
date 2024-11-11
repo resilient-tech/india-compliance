2c6c20b04f59f05be5fd931f34c2f0458c1532bb
Ninad1306 <ninad_1063@yahoo.com>
2024-11-11 16:50:52 +0530

    fix: data generation refactor for backend

--- india_compliance/gst_india/doctype/gstr_3b_beta/gstr_3b_beta.py
+++ india_compliance/gst_india/doctype/gstr_3b_beta/gstr_3b_beta.py
@@ -1,65 +1,56 @@
 # Copyright (c) 2024, Resilient Tech and contributors
 # For license information, please see license.txt
 
-from collections import defaultdict
+# from collections import defaultdict
 
 import frappe
 from frappe.model.document import Document
 from frappe.query_builder.custom import ConstantColumn
 from frappe.query_builder.functions import Abs, Sum
+from frappe.utils import get_date_str, get_first_day, get_last_day
 
 from india_compliance.gst_india.constants import GST_TAX_TYPES
 from india_compliance.gst_india.doctype.purchase_reconciliation_tool import (
     ReconciledData,
 )
+from india_compliance.gst_india.utils import get_month_or_quarter_dict
 
 
 class GSTR3BBeta(Document):
     @frappe.whitelist()
-    def fetch_invoice_data(self):
-        inward_supplies = self.get_all_inward_supplies()
-
-        invoice_data = {
-            invoice["name"]: {
-                "supplier_gstin": invoice["supplier_gstin"],
-                "supplier": invoice["supplier_name"],
-                "invoice_type": invoice["invoice_type"],
-                "invoice_status": invoice["invoice_status"],
-                "invoice_no": invoice["bill_no"],
-                "invoice_name": invoice["name"],
-                "invoice_date": invoice["bill_date"],
-                "company": invoice["company"],
-                "company_gstin": invoice["company_gstin"],
-                "match_status": invoice["match_status"],
-                "linked_doc": invoice["link_name"],
-                "is_dirty": 0,
+    def get_invoice_data(self):
+        month = get_month_or_quarter_dict().get(self.month)
+        filters = frappe._dict(
+            {
+                "company": self.company,
+                "company_gstin": self.company_gstin,
+                "from_date": get_date_str(get_first_day(f"{self.year}-{month}-01")),
+                "to_date": get_date_str(get_last_day(f"{self.year}-{month}-01")),
             }
-            for invoice in inward_supplies
-        }
+        )
+
+        inward_supplies = self.get_all_inward_supplies(filters=filters)
+        purchases_and_bill_of_entry = self.get_all_purchases(filters=filters)
+
+        invoice_data = []
+        for doc in inward_supplies:
+            invoice_data.append(
+                frappe._dict(
+                    {
+                        "_inward_supply": doc,
+                        "_purchase_invoice": purchases_and_bill_of_entry.pop(
+                            doc.link_name, frappe._dict()
+                        ),
+                    }
+                )
+            )
+
+        ReconciledData().process_data(invoice_data, retain_doc=True)
 
         return invoice_data
 
-    def before_save(self):
-        invoices_to_update = defaultdict(set)
-        for key, value in self.invoice_data.items():
-            if value["is_dirty"]:
-                invoices_to_update[value["invoice_status"]].add(key)
-
-        self.update_invoice_status(invoices_to_update)
-
-        self.invoice_data = ""
-
-    def update_invoice_status(self, invoices_to_update):
-        for action, invoices in invoices_to_update.items():
-            frappe.db.set_value(
-                "GST Inward Supply",
-                {"name": ("in", invoices)},
-                "invoice_status",
-                action,
-            )
-
     @frappe.whitelist()
-    def get_invoice_details(self, purchase_name, inward_supply_name):
+    def get_invoice_comparision(self, purchase_name, inward_supply_name):
         inward_supply = self.get_all_inward_supplies(name=inward_supply_name)
         purchases = self.get_all_purchases(purchase_name)
 
@@ -78,7 +69,7 @@ class GSTR3BBeta(Document):
 
         return reconciliation_data[0]
 
-    def get_all_inward_supplies(self, name=None):
+    def get_all_inward_supplies(self, name=None, filters=None):
         inward_supply = frappe.qb.DocType("GST Inward Supply")
         inward_supply_item = frappe.qb.DocType("GST Inward Supply Item")
         fields = GST_TAX_TYPES[:-1] + ("taxable_value",)
@@ -100,7 +91,7 @@ class GSTR3BBeta(Document):
                 inward_supply.link_doctype,
                 inward_supply.match_status,
                 inward_supply.invoice_status,
-                inward_supply.invoice_type,
+                inward_supply.supply_type,
                 inward_supply.name,
                 inward_supply.classification,
                 inward_supply.is_reverse_charge,
@@ -114,15 +105,22 @@ class GSTR3BBeta(Document):
         if name:
             query = query.where(inward_supply.name == name)
 
+        query = self.get_query_with_filters(inward_supply, query, filters)
+
+        if filters.get("from_date"):
+            query = query.where(
+                inward_supply.bill_date.between(filters.from_date, filters.to_date)
+            )
+
         return query.run(as_dict=True)
 
-    def get_all_purchases(self, name=None):
-        purchases = self.get_all_purchase_invoice(name)
-        purchases.extend(self.get_all_bill_of_entry(name))
+    def get_all_purchases(self, name=None, filters=None):
+        purchases = self.get_all_purchase_invoice(name, filters)
+        purchases.extend(self.get_all_bill_of_entry(name, filters))
 
         return {doc.name: doc for doc in purchases}
 
-    def get_all_purchase_invoice(self, name=None):
+    def get_all_purchase_invoice(self, name, filters):
         purchase = frappe.qb.DocType("Purchase Invoice")
         purchase_item = frappe.qb.DocType("Purchase Invoice Item")
         tax_fields = [
@@ -154,9 +152,16 @@ class GSTR3BBeta(Document):
         if name:
             query = query.where(purchase.name == name)
 
+        query = self.get_query_with_filters(purchase, query, filters)
+
+        if filters.get("from_date"):
+            query = query.where(
+                purchase.posting_date.between(filters.from_date, filters.to_date)
+            )
+
         return query.run(as_dict=True)
 
-    def get_all_bill_of_entry(self, name):
+    def get_all_bill_of_entry(self, name, filters):
         boe = frappe.qb.DocType("Bill of Entry")
         boe_item = frappe.qb.DocType("Bill of Entry Item")
         purchase_invoice = frappe.qb.DocType("Purchase Invoice")
@@ -192,7 +197,23 @@ class GSTR3BBeta(Document):
         if name:
             query = query.where(boe.name == name)
 
+        query = self.get_query_with_filters(boe, query, filters)
+
+        if filters.get("from_date"):
+            query = query.where(
+                boe.bill_of_entry_date.between(filters.from_date, filters.to_date)
+            )
+
         return query.run(as_dict=True)
 
     def query_tax_amount(self, doc, field):
         return Abs(Sum(getattr(doc, field)))
+
+    def get_query_with_filters(self, doc, query, filters):
+        if filters.get("company"):
+            query = query.where(doc.company == filters.company)
+
+        if filters.get("company_gstin"):
+            query = query.where(doc.company_gstin == filters.company_gstin)
+
+        return query
