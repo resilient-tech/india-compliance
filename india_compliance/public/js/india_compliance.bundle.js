69978d81fa89d92de086185991d6227d99bf42c7
Sanket Shah <113279972+Sanket322@users.noreply.github.com>
2024-12-12 18:19:01 +0530

    fix: add documentation links in help (#2810)
    * fix: add documentation links in help
    
    * fix: added e-waybill links
    
    * fix: formatting changes
    
    * fix: added links for company and gst settings
    
    * fix: remove e-waybill links and add blog links
    
    ---------
    
    Co-authored-by: Sanket322 <shahsanket322003.com>
    (cherry picked from commit 28d45b2cb5c418eba62e2111d0d5bd7a29ff7d64)
    
    # Conflicts:
    #	india_compliance/public/js/india_compliance.bundle.js

--- india_compliance/public/js/india_compliance.bundle.js
+++ india_compliance/public/js/india_compliance.bundle.js
@@ -5,3 +5,8 @@ import "./audit_trail_notification";
 import "./item_tax_template_notification";
 import "./quick_info_popover";
 import "./custom_number_card";
+<<<<<<< HEAD
+=======
+import "./taxes_controller";
+import "./help_links";
+>>>>>>> 28d45b2c (fix: add documentation links in help (#2810))
