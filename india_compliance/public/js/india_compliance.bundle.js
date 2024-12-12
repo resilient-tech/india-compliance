897466f257ec47334203d7376e2e459d5da59967
Smit Vora <smitvora203@gmail.com>
2024-12-12 18:45:33 +0530

    chore: resolve conflicts

--- india_compliance/public/js/india_compliance.bundle.js
+++ india_compliance/public/js/india_compliance.bundle.js
@@ -5,8 +5,4 @@ import "./audit_trail_notification";
 import "./item_tax_template_notification";
 import "./quick_info_popover";
 import "./custom_number_card";
-<<<<<<< HEAD
-=======
-import "./taxes_controller";
 import "./help_links";
->>>>>>> 28d45b2c (fix: add documentation links in help (#2810))
