c1413ebd2e20738da17c158950a0928c73910397
Sanket322 <shahsanket322003.com>
2025-01-08 11:38:12 +0530

    fix: created new doctype for multiple purchase invoices

---
+++ india_compliance/gst_india/doctype/boe_purchase_invoice/boe_purchase_invoice.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Resilient Tech and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class BOEPurchaseInvoice(Document):
+    pass
