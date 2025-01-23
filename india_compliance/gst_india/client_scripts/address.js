f025628beb9ec8e8b9eef5b89d5731ff363460e5
Sanket322 <shahsanket322003.com>
2025-01-23 17:47:10 +0530

    fix: Update Address button

--- india_compliance/gst_india/client_scripts/address.js
+++ india_compliance/gst_india/client_scripts/address.js
@@ -24,6 +24,8 @@ frappe.ui.form.on(DOCTYPE, {
     async refresh(frm) {
         india_compliance.set_state_options(frm);
 
+        update_address_info(frm);
+
         // set default values for GST fields
         if (!frm.is_new() || !frm.doc.links || !frm.doc.links.length || frm.doc.gstin) return;
 
@@ -52,3 +54,18 @@ frappe.ui.form.on(DOCTYPE, {
         frm.set_value("gst_category", doc.gst_category || "");
     },
 });
+
+function update_address_info(frm) {
+    frm.add_custom_button(__("Update Address"), function () {
+        const doc = frappe.get_doc(DOCTYPE, frm.doc.name);
+        doc._gstin = frm.doc.gstin;
+        doc._pincode = frm.doc.pincode;
+
+        const Address = frappe.ui.form.AddressQuickEntryForm;
+        frappe.ui.form.AddressQuickEntryForm = class extends frappe.ui.form.AddressQuickEntryForm {
+            get_dynamic_link_fields() { return []; }
+        }
+        frappe.ui.form.make_quick_entry(DOCTYPE,null,null,doc);
+        frappe.ui.form.AddressQuickEntryForm = Address;
+    });
+}
