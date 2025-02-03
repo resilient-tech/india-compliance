5b85eb8c2d4acb884629cf72f4629564132a92d9
Karm Soni <karmdsoni8159@gmail.com>
2025-02-03 15:54:57 +0530

    fix: add color in gstin status of the gstin report; refactor code; make formatting the value more dynamic;

--- india_compliance/gst_india/report/gstin_detailed/gstin_detailed.js
+++ india_compliance/gst_india/report/gstin_detailed/gstin_detailed.js
@@ -2,13 +2,13 @@
 // For license information, please see license.txt
 
 
-const GSTIN_FIELDNAME_TO_FIELDTYPE_MAPPING = {
-	"status": "Data",
-	"registration_date": "Date",
-	"last_updated_on": "Datetime",
-	"cancelled_date": "Date",
-	"is_blocked": "Data",
-}
+const GSTIN_FIELDNAME = [
+	"status",
+	"registration_date",
+	"last_updated_on",
+	"cancelled_date",
+	"is_blocked",
+]
 
 frappe.query_reports["GSTIN Detailed"] = {
 
@@ -43,17 +43,19 @@ frappe.query_reports["GSTIN Detailed"] = {
 	],
 
 	formatter: function (value, row, column, data, default_formatter) {
-		console.log(row)
-		console.log(column)
-		console.log(data)
 		if (data) {
 			value = default_formatter(value, row, column, data);
 
-			value = `<span fieldname="${column.fieldname}">${value}</span>`
-
-			if (column.fieldname == "update_gstin_details_btn") {
+			if (column.fieldname == "status"){
+				value = get_colored_status(value);
+			}
+			else if (column.fieldname == "update_gstin_details_btn") {
 				value = create_btn_with_gstin_attr(data.gstin);
 			}
+			else{
+				value = `<span fieldname="${column.fieldname}">${value}</span>`
+			}
+
 		}
 
 		return value;
@@ -62,7 +64,7 @@ frappe.query_reports["GSTIN Detailed"] = {
 	add_on_click_listner(gstin, default_formatter) {
 		toggle_gstin_update_btn(gstin, disabled=true);
 		const affectedElements = $(`div.dt-cell__content[title='${gstin}']`);
-		toggle_rows_opacity(affectedElements, opacity=0.5)
+		set_btn_text(gstin, "Updating")
 
 		frappe.call({
 			method: "india_compliance.gst_india.doctype.gstin.gstin.get_gstin_status",
@@ -75,30 +77,45 @@ frappe.query_reports["GSTIN Detailed"] = {
 					let data = r.message;
 					affectedElements.each(function () {
 						row = this.parentElement.attributes["data-row-index"].value;
-						for (let fieldname in GSTIN_FIELDNAME_TO_FIELDTYPE_MAPPING) {
-							update_value(row, fieldname, GSTIN_FIELDNAME_TO_FIELDTYPE_MAPPING[fieldname], data[fieldname]);
+						for (let fieldname of GSTIN_FIELDNAME) {
+							update_value(row, fieldname, data[fieldname]);
 						}
 					})
 				}
 				else{
 					toggle_gstin_update_btn(gstin, disabled=false);
 				}
-				toggle_rows_opacity(affectedElements, opacity=1)
+				set_btn_text(gstin, "Updated")
 			}
 
 		})
 	},
 };
+const STATUS_TO_COLOR_MAPPING = {
+    "Active": "green",       // Green with 70% opacity
+    "Cancelled": "red",    // Red with 70% opacity
+    "Inactive": "orange",    // Orange with 70% opacity
+    "Provisional": "yellow",  // Yellow with 70% opacity
+    "Suspended": "grey",  // Grey with 70% opacity
+};
 
-function toggle_rows_opacity(elements, opacity=null){
-	elements.each(function(){
-		let row = this.parentElement.parentElement;
-		if(opacity==null){
-			opacity = row.style["opacity"];
-			opacity = opacity===1?0.5:1;
-		}
-		row.style["opacity"] = opacity
-	})
+function get_colored_status(status) {
+    return `<span class="badge" style="
+        background-color: ${STATUS_TO_COLOR_MAPPING[status]};
+        color: white;
+        border-radius: 12px;
+        font-size: 12px;
+        font-weight: 500;
+        padding: 6px 12px;
+        display: inline-block;
+        backdrop-filter: blur(5px);
+		opacity: 0.7;
+    ">${status}</span>`;
+}
+
+function set_btn_text(gstin, text){
+	let btn = $(`button[data-gstin='${gstin}']`)
+	btn.text(text)
 }
 
 function toggle_gstin_update_btn(gstin, disabled=null){
@@ -112,18 +129,33 @@ function toggle_gstin_update_btn(gstin, disabled=null){
 }
 
 function create_btn_with_gstin_attr(gstin) {
-	const BUTTON_HTML = `<button data-fieldname="gstin_update_btn" class="btn btn-xs btn-primary center" data-gstin="${gstin}" onclick="frappe.query_reports['GSTIN Detailed'].add_on_click_listner('${gstin}')">Update</button>`;
+	const BUTTON_HTML = `<button
+		data-fieldname="gstin_update_btn"
+		class="btn btn-xs btn-primary center"
+		data-gstin="${gstin}"
+		onclick="frappe.query_reports['GSTIN Detailed'].add_on_click_listner('${gstin}')"
+	>
+		Update
+	</button>`;
 
 	return BUTTON_HTML
 }
 
-function update_value(row, fieldname,fieldtype, value) {
-	let ele = $(`.dt-row.dt-row-${row}.vrow > div > div > span[fieldname='${fieldname}']`);
+function update_value(row, fieldname, value) {
+	let ele = $(`.dt-row.dt-row-${row}.vrow > div > div > [fieldname='${fieldname}']`);
+
+	let column = frappe.query_report.columns.find((column)=>{
+		return column.fieldname == fieldname;
+	});
+	fieldtype = column.fieldtype;
+
 	const formatter = frappe.form.get_formatter(fieldtype);
 	value = formatter(value);
+
 	if (fieldname == "is_blocked"){
 		value = value==0?"No":"Yes";
 	}
+
 	ele.text(value);
 	ele.parent().attr("title", value);
 }
