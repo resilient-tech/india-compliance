afef3b818a3aa6a707cf6b10011fe96c4a2a6a94
Karm Soni <karmdsoni8159@gmail.com>
2025-02-11 13:55:03 +0530

    feat: make API Requests Report

---
+++ india_compliance/gst_india/report/api_requests/api_requests.js
@@ -0,0 +1,42 @@
+// Copyright (c) 2025, Resilient Tech and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["API Requests"] = {
+    filters: [
+        {
+            "fieldname": "report_by",
+            "label": __("Report by"),
+            "fieldtype": "Select",
+            "options": [
+                "Endpoint",
+                "Linked Document",
+                "Date"
+            ],
+            "default": "Endpoint"
+        },
+        {
+            "fieldname": "from_date",
+            "label": __("From"),
+            "fieldtype": "Date",
+            "reqd": 1,
+            "default": frappe.datetime.add_months(frappe.datetime.now_date(), -6)
+        },
+        {
+            "fieldname": "to_date",
+            "label": __("To"),
+            "fieldtype": "Date",
+            "reqd": 1,
+            "default": frappe.datetime.now_date()
+        },
+    ],
+
+    onload(query_report) {
+        query_report.filters.forEach(filter => {
+            if (filter.fieldtype === "Date") {
+                filter.datepicker.update(
+                    { maxDate: new Date() }
+                )
+            }
+        });
+    }
+};
---
+++ india_compliance/gst_india/report/api_requests/api_requests.py
@@ -0,0 +1,153 @@
+# Copyright (c) 2025, Resilient Tech and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe import _
+from frappe.query_builder.functions import Count, Date
+
+
+def execute(filters: dict | None = None):
+    report = APIRequestsReport(filters=filters)
+    columns = report.get_columns()
+    data = report.get_data()
+
+    return columns, data
+
+
+class APIRequestsReport:
+    def __init__(self, filters: dict | None = None):
+        self.from_data = filters.from_date
+        self.to_date = filters.to_date
+        self.report_by = filters.report_by
+
+    def get_columns(self) -> list[dict]:
+        if self.report_by == "Endpoint":
+            return self._get_columns_by_endpoint()
+
+        if self.report_by == "Date":
+            return self._get_columns_by_date()
+
+        return self._get_columns_by_linked_doctype()
+
+    def _get_columns_by_endpoint(self):
+        columns = [
+            {
+                "label": _("Endpoint"),
+                "fieldname": "endpoint",
+                "fieldtype": "Data",
+                "width": 400,
+            },
+            {
+                "label": _("API Requests Count"),
+                "fieldname": "api_requests_count",
+                "fieldtype": "Int",
+                "width": 200,
+            },
+        ]
+
+        return columns
+
+    def _get_columns_by_date(self):
+        columns = [
+            {
+                "label": _("Date"),
+                "fieldname": "date",
+                "fieldtype": "Date",
+                "width": 250,
+            },
+            {
+                "label": _("API Requests Count"),
+                "fieldname": "api_requests_count",
+                "fieldtype": "Int",
+                "width": 200,
+            },
+        ]
+
+        return columns
+
+    def _get_columns_by_linked_doctype(self):
+        columns = [
+            {
+                "label": _("Reference DocType"),
+                "fieldname": "reference_doctype",
+                "fieldtype": "Link",
+                "options": "DocType",
+                "width": 200,
+            },
+            {
+                "label": _("Reference Document"),
+                "fieldname": "reference_docname",
+                "fieldtype": "Dynamic Link",
+                "options": "reference_doctype",
+                "width": 200,
+            },
+            {
+                "label": _("API Requests Count"),
+                "fieldname": "api_requests_count",
+                "fieldtype": "Int",
+                "width": 200,
+            },
+        ]
+
+        return columns
+
+    def get_data(self) -> list[dict]:
+        if self.report_by == "Endpoint":
+            return self._get_data_by_endpoint()
+
+        if self.report_by == "Date":
+            return self._get_data_by_date()
+
+        return self._get_data_by_linked_doctype()
+
+    def _get_data_by_endpoint(self):
+        integration_requests = frappe.qb.DocType("Integration Request")
+
+        query = (
+            frappe.qb.from_(integration_requests)
+            .select(
+                integration_requests.url.as_("endpoint"),
+                Count("*").as_("api_requests_count"),
+            )
+            .where(integration_requests.creation >= self.from_data)
+            .where(integration_requests.creation <= self.to_date)
+            .groupby(integration_requests.url)
+        )
+
+        return query.run(as_dict=True)
+
+    def _get_data_by_date(self):
+        integration_requests = frappe.qb.DocType("Integration Request")
+
+        query = (
+            frappe.qb.from_(integration_requests)
+            .select(
+                Date(integration_requests.creation).as_("date"),
+                Count("*").as_("api_requests_count"),
+            )
+            .where(integration_requests.creation >= self.from_data)
+            .where(integration_requests.creation <= self.to_date)
+            .groupby(Date(integration_requests.creation))
+        )
+
+        return query.run(as_dict=True)
+
+    def _get_data_by_linked_doctype(self):
+        integration_requests = frappe.qb.DocType("Integration Request")
+
+        query = (
+            frappe.qb.from_(integration_requests)
+            .select(
+                integration_requests.reference_doctype.as_("reference_doctype"),
+                integration_requests.reference_docname.as_("reference_docname"),
+                Count("*").as_("api_requests_count"),
+            )
+            .where(integration_requests.creation >= self.from_data)
+            .where(integration_requests.creation <= self.to_date)
+            .groupby(
+                integration_requests.reference_doctype,
+                integration_requests.reference_docname,
+            )
+        )
+
+        return query.run(as_dict=True)
