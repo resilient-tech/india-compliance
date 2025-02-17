15673179afeb29eae578ae1c452c648dac6893bb
Sanket322 <shahsanket322003.com>
2025-02-17 22:38:04 +0530

    feat: added gstr_1_reconciliation doctype

---
+++ india_compliance/gst_india/doctype/gstr_1_reconciliation/gstr_1_reconciliation.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Resilient Tech and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("GSTR-1 Reconciliation", {
+// 	refresh(frm) {
+
+// 	},
+// });
