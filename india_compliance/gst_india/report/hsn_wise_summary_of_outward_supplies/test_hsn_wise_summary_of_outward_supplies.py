3080f09a8f29f0fa8d0f44ade7f612983126560b
Sanket Shah <113279972+Sanket322@users.noreply.github.com>
2025-03-04 08:41:11 +0530

    fix: Use `UOM` instead of `Stock UOM `for consistency (#3116)
    * fix: for gst sales register beta
    
    * fix: for GSTR-1 Report
    
    * fix: for GSTR-1 Books data
    
    * fix: resolve test case
    
    * fix: usage of stock_uom to uom in processing of invoice for gstr1 data
    
    ---------
    
    Co-authored-by: Sanket322 <shahsanket322003.com>
    Co-authored-by: Smit Vora <smitvora203@gmail.com>
    (cherry picked from commit f0b6f8b84b864441517dff0cbcce3c3da9f43090)

--- india_compliance/gst_india/report/hsn_wise_summary_of_outward_supplies/test_hsn_wise_summary_of_outward_supplies.py
+++ india_compliance/gst_india/report/hsn_wise_summary_of_outward_supplies/test_hsn_wise_summary_of_outward_supplies.py
@@ -50,6 +50,6 @@ class TestHSNWiseSummaryReport(TestCase):
         self.assertTrue(filtered_rows)
 
         hsn_row = filtered_rows[0]
-        self.assertEquals(hsn_row["stock_qty"], 6.0)
-        self.assertEquals(hsn_row["taxable_amount"], 600)
-        self.assertEquals(hsn_row["total_amount"], 708)  # 6 * 100 * 1.18
+        self.assertEquals(hsn_row["qty"], 3.0)
+        self.assertEquals(hsn_row["taxable_amount"], 300)
+        self.assertEquals(hsn_row["total_amount"], 354)  # 6 * 100 * 1.18
