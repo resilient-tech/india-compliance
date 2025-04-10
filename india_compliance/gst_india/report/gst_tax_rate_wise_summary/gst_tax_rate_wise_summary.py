e9d4dc1fa3c2e46ee8555aa5c31bc8b80ab65b51
Ninad1306 <ninad_1063@yahoo.com>
2025-04-10 17:44:57 +0530

    fix: handle bill of entry for purchases

--- india_compliance/gst_india/report/gst_tax_rate_wise_summary/gst_tax_rate_wise_summary.py
+++ india_compliance/gst_india/report/gst_tax_rate_wise_summary/gst_tax_rate_wise_summary.py
@@ -15,6 +15,24 @@ AMOUNT_FIELDS = (
     "cess_amount",
 )
 
+VOUCHER_TYPE_MAP = {
+    "Sales Invoice": {
+        "doctypes": ("Sales Invoice",),
+        "gstin_field": "billing_address_gstin",
+        "is_reverse_charge": 0,
+    },
+    "Purchase Invoice": {
+        "doctypes": ("Purchase Invoice", "Bill of Entry"),
+        "gstin_field": "supplier_gstin",
+        "is_reverse_charge": 0,
+    },
+    "Purchase Reverse Charge": {
+        "doctypes": ("Purchase Invoice",),
+        "gstin_field": "supplier_gstin",
+        "is_reverse_charge": 1,
+    },
+}
+
 
 def execute(filters: dict | None = None):
     filters = frappe._dict(filters or {})
@@ -22,7 +40,6 @@ def execute(filters: dict | None = None):
 
     columns = get_columns(filters)
     data = get_data(filters)
-    data = process_data(data)
 
     return columns, data
 
@@ -78,49 +95,13 @@ def get_columns(filters):
 
 
 def get_data(filters):
-    if filters.voucher_type == "Sales Invoice":
-        doctype = "Sales Invoice"
-        gstin_field = "billing_address_gstin"
-    else:
-        doctype = "Purchase Invoice"
-        gstin_field = "supplier_gstin"
+    filters.update(VOUCHER_TYPE_MAP.get(filters.voucher_type))
+    data = []
 
-    doc = frappe.qb.DocType(doctype)
-    doc_item = frappe.qb.DocType(f"{doctype} Item")
+    for doctype in filters.doctypes:
+        data.extend(get_doctype_wise_data(filters, doctype))
 
-    query = (
-        frappe.qb.from_(doc)
-        .join(doc_item)
-        .on(doc.name == doc_item.parent)
-        .select(
-            doc_item.taxable_value.as_("taxable_value"),
-            doc_item.cgst_amount.as_("cgst_amount"),
-            doc_item.sgst_amount.as_("sgst_amount"),
-            doc_item.igst_amount.as_("igst_amount"),
-            (doc_item.cess_amount + doc_item.cess_non_advol_amount).as_("cess_amount"),
-            doc_item.gst_treatment.as_("gst_treatment"),
-            (doc_item.igst_rate + doc_item.cgst_rate + doc_item.sgst_rate).as_(
-                "tax_rate"
-            ),
-        )
-        .where(
-            (doc.docstatus == 1)
-            & (doc.posting_date[filters.from_date : filters.to_date])
-            & (doc.company == filters.company)
-            & (doc.is_opening == "No")
-            & (doc.company_gstin != IfNull(doc[gstin_field], ""))
-        )
-    )
-
-    if filters.get("company_gstin"):
-        query = query.where(doc.company_gstin == filters.company_gstin)
-
-    if filters.voucher_type == "Purchase Reverse Charge":
-        query = query.where(doc.is_reverse_charge == 1)
-    else:
-        query = query.where(doc.is_reverse_charge == 0)
-
-    return query.run(as_dict=True)
+    return process_data(data)
 
 
 def process_data(data):
@@ -146,7 +127,6 @@ def process_data(data):
 
     # Sorting order
     sort_order = [rate for rate in GST_TAX_RATES] + [
-        "Taxable",
         "Zero-Rated",
         "Nil-Rated",
         "Exempted",
@@ -158,3 +138,42 @@ def process_data(data):
         invoice_wise_data.items(), key=lambda item: sort_order.index(item[0])
     )
     return [value for _, value in sorted_data]
+
+
+def get_doctype_wise_data(filters, doctype):
+    doc = frappe.qb.DocType(doctype)
+    doc_item = frappe.qb.DocType(f"{doctype} Item")
+
+    query = (
+        frappe.qb.from_(doc)
+        .join(doc_item)
+        .on(doc.name == doc_item.parent)
+        .select(
+            doc_item.taxable_value.as_("taxable_value"),
+            doc_item.cgst_amount.as_("cgst_amount"),
+            doc_item.sgst_amount.as_("sgst_amount"),
+            doc_item.igst_amount.as_("igst_amount"),
+            (doc_item.cess_amount + doc_item.cess_non_advol_amount).as_("cess_amount"),
+            doc_item.gst_treatment.as_("gst_treatment"),
+            (doc_item.igst_rate + doc_item.cgst_rate + doc_item.sgst_rate).as_(
+                "tax_rate"
+            ),
+        )
+        .where(
+            (doc.docstatus == 1)
+            & (doc.posting_date[filters.from_date : filters.to_date])
+            & (doc.company == filters.company)
+        )
+    )
+
+    if doctype != "Bill of Entry":
+        query = query.where(
+            (doc.is_opening == "No")
+            & (doc.company_gstin != IfNull(doc[filters.gstin_field], ""))
+            & (doc.is_reverse_charge == filters.is_reverse_charge)
+        )
+
+    if filters.get("company_gstin"):
+        query = query.where(doc.company_gstin == filters.company_gstin)
+
+    return query.run(as_dict=True)
