e1e31dd16ca00a0de8f50298197dd3ea1e5597a5
Ninad1306 <ninad_1063@yahoo.com>
2025-04-17 15:24:44 +0530

    fix: custom field for hsn bifurcation date

--- india_compliance/gst_india/setup/__init__.py
+++ india_compliance/gst_india/setup/__init__.py
@@ -215,6 +215,7 @@ def set_default_gst_settings():
         "auto_generate_e_invoice": 1,
         "generate_e_waybill_with_e_invoice": 1,
         "e_invoice_applicable_from": nowdate(),
+        "hsn_bifurcation_from": "2025-04-01",  # As per GST Notification
         "fetch_e_invoice_details_from_gst_portal": 1,
         "autofill_party_info": 1,
         "archive_party_info_days": 7,
