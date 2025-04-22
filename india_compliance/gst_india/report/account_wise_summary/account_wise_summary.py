b67e50fd21a6deb9e867048e61e9611751529a50
Ninad1306 <ninad_1063@yahoo.com>
2025-04-22 16:12:49 +0530

    feat: account-wise summary of sales and purchase

---
+++ india_compliance/gst_india/report/account_wise_summary/account_wise_summary.py
@@ -0,0 +1,231 @@
+# Copyright (c) 2025, Resilient Tech and contributors
+# For license information, please see license.txt
+
+from collections import defaultdict
+
+import frappe
+from frappe import _
+from frappe.query_builder.functions import IfNull
+
+from india_compliance.gst_india.constants import GST_TAX_TYPES
+from india_compliance.income_tax_india.overrides.tax_withholding_category import (
+    get_tax_withholding_accounts,
+)
+
+
+def execute(filters: dict | None = None):
+    filters = frappe._dict(filters or {})
+    filters.from_date, filters.to_date = filters.date_range
+
+    columns = get_columns()
+    data = get_data(filters)
+
+    return columns, data
+
+
+def get_columns():
+    return [
+        {
+            "label": _("Account Name"),
+            "fieldname": "account_name",
+            "fieldtype": "Data",
+            "width": 300,
+        },
+        {
+            "label": _("Total Amount"),
+            "fieldname": "total_amount",
+            "fieldtype": "Float",
+            "width": 200,
+        },
+        {
+            "label": _("Amount of Total ITC"),
+            "fieldname": "total_itc",
+            "fieldtype": "Float",
+            "width": 200,
+        },
+        {
+            "label": _("Amount of eligible ITC availed"),
+            "fieldname": "total_itc_availed",
+            "fieldtype": "Float",
+            "width": 250,
+        },
+    ]
+
+
+def get_data(filters):
+    account_summary = {}
+    invoices = get_invoices(filters)
+    tds_accounts = get_tax_withholding_accounts(filters.company)
+
+    for invoice in invoices:
+        additional_charges = 0
+        additional_tax = 0
+
+        for item in invoice["items"]:
+            account = item.expense_account
+            account_data = account_summary.setdefault(account, defaultdict(float))
+            account_data["account_name"] = account
+
+            account_data["total_amount"] += item.base_net_amount
+
+            proportional_tax = item.base_net_amount * (item.tax_rate / 100)
+            account_data["total_itc"] += proportional_tax
+            if not item.ineligibility_reason:
+                account_data["total_itc_availed"] += proportional_tax
+
+            additional_charges += item.taxable_value - item.base_net_amount
+            additional_tax += item.tax_amount - proportional_tax
+
+        for tax in invoice["taxes"]:
+            if tax.gst_tax_type in GST_TAX_TYPES:
+                if (
+                    tax.base_tax_amount_after_discount_amount
+                    and tax.charge_type == "On Previous Row Total"
+                ):
+                    break
+                continue
+
+            if tax.account_head in tds_accounts:
+                continue
+
+            account = tax.account_head
+            account_data = account_summary.setdefault(account, defaultdict(float))
+            account_data["account_name"] = account
+
+            account_data["total_amount"] += tax.base_tax_amount_after_discount_amount
+
+            if not additional_charges:
+                continue
+
+            proportion = tax.base_tax_amount_after_discount_amount / additional_charges
+            proportional_tax = proportion * additional_tax
+            account_data["total_itc"] += proportional_tax
+
+            if not tax.ineligibility_reason:
+                account_data["total_itc_availed"] += proportional_tax
+
+    return list(account_summary.values())
+
+
+def get_invoices(filters):
+    if filters.voucher_type == "Sales":
+        filters.doctype = "Sales Invoice"
+        filters["gstin_field"] = "billing_address_gstin"
+    else:
+        filters.doctype = "Purchase Invoice"
+        filters["gstin_field"] = "supplier_gstin"
+
+    compiled_docs = frappe._dict()
+    taxes = get_taxes_for_docs(filters)
+    items = get_items_for_docs(filters)
+
+    compile_docs(taxes, items, filters.doctype, compiled_docs)
+
+    return list(compiled_docs.values())
+
+
+def get_taxes_for_docs(filters):
+    taxes_doctype = (
+        "Sales Taxes and Charges"
+        if filters.doctype == "Sales Invoice"
+        else "Purchase Taxes and Charges"
+    )
+
+    doc = frappe.qb.DocType(filters.doctype)
+    taxes_doc = frappe.qb.DocType(taxes_doctype)
+
+    query = (
+        frappe.qb.from_(doc)
+        .join(taxes_doc)
+        .on((doc.name == taxes_doc.parent) & (taxes_doc.parenttype == filters.doctype))
+        .select(
+            taxes_doc.tax_amount,
+            taxes_doc.base_tax_amount_after_discount_amount,
+            taxes_doc.gst_tax_type,
+            taxes_doc.parent,
+            taxes_doc.account_head,
+            taxes_doc.charge_type,
+        )
+        .orderby(taxes_doc.idx)
+    )
+
+    if filters.doctype == "Purchase Invoice":
+        query = query.select(doc.ineligibility_reason)
+
+    query = get_query_with_common_filters(query, doc, filters)
+
+    return query.run(as_dict=True)
+
+
+def get_items_for_docs(filters):
+    doc = frappe.qb.DocType(filters.doctype)
+    item_doc = frappe.qb.DocType(f"{filters.doctype} Item")
+
+    query = (
+        frappe.qb.from_(doc)
+        .join(item_doc)
+        .on((doc.name == item_doc.parent) & (item_doc.parenttype == filters.doctype))
+        .select(
+            item_doc.name,
+            item_doc.parent,
+            item_doc.expense_account,
+            item_doc.item_code,
+            item_doc.item_name,
+            item_doc.qty,
+            item_doc.taxable_value,
+            item_doc.base_net_amount,
+            (item_doc.cgst_rate + item_doc.sgst_rate + item_doc.igst_rate).as_(
+                "tax_rate"
+            ),
+            (
+                item_doc.cgst_amount
+                + item_doc.sgst_amount
+                + item_doc.igst_amount
+                + item_doc.cess_amount
+                + item_doc.cess_non_advol_amount
+            ).as_("tax_amount"),
+        )
+    )
+
+    if filters.doctype == "Purchase Invoice":
+        query = query.select(doc.ineligibility_reason)
+
+    query = get_query_with_common_filters(query, doc, filters)
+
+    return query.run(as_dict=True)
+
+
+def compile_docs(taxes, items, doctype, compiled_docs):
+    """
+    Compile docs, so that each one could be accessed as if it's a single doc.
+    """
+    for tax in taxes:
+        if tax.parent not in compiled_docs:
+            compiled_docs[tax.parent] = frappe._dict(
+                taxes=[], items=[], doctype=doctype
+            )
+
+        compiled_docs[tax.parent]["taxes"].append(tax)
+
+    for item in items:
+        if item.parent not in compiled_docs:
+            compiled_docs[item.parent] = frappe._dict(
+                taxes=[], items=[], doctype=doctype
+            )
+
+        compiled_docs[item.parent]["items"].append(item)
+
+
+def get_query_with_common_filters(query, doc, filters):
+    query = query.where(
+        (doc.docstatus == 1)
+        & (doc.posting_date[filters.from_date : filters.to_date])
+        & (doc.company == filters.company)
+        & (doc.is_opening == "No")
+        & (doc.company_gstin != IfNull(doc[filters.gstin_field], ""))
+    )
+
+    if filters.get("company_gstin"):
+        query = query.where(doc.company_gstin == filters.company_gstin)
+
+    return query
