d69f313c49cc25b2c25c53e9f2cfc011ba66d670
Abdeali Chharchhoda <abdealiking786@gmail.com>
2025-05-16 13:27:22 +0530

    fix: update required version for Frappe to `15.50.0`

--- india_compliance/patches/check_version_compatibility.py
+++ india_compliance/patches/check_version_compatibility.py
@@ -13,7 +13,7 @@ VERSIONS_TO_COMPARE = [
     {
         "app_name": "Frappe",
         "current_version": version.parse(frappe.__version__),
-        "required_versions": {"version-14": "14.57.0", "version-15": "15.3.0"},
+        "required_versions": {"version-14": "14.57.0", "version-15": "15.50.0"},
     },
     {
         "app_name": "ERPNext",
