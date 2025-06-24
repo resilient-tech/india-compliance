2769540dcd6eb85ac95135787a92d0bcddd945b2
Karm Soni <karmdsoni8159@gmail.com>
2025-06-24 16:46:15 +0530

    fix: use Object.hasOwn to check property existence in DataTableManager

--- india_compliance/public/js/components/data_table_manager.js
+++ india_compliance/public/js/components/data_table_manager.js
@@ -238,7 +238,7 @@ india_compliance.DataTableManager = class DataTableManager {
                 cell.column.fieldtype === "Float"
             ) {
                 cell.content = data[fieldname] || 0.0;
-            } else if (data.hasOwnProperty(fieldname)) {
+            } else if (Object.hasOwn(data, fieldname)) {
                 cell.content = data[fieldname];
             }
 
