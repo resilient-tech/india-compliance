7f10a5541a40471d81050237d32b60288d063d66
Smit Vora <smitvora203@gmail.com>
2025-07-05 16:11:42 +0530

    refactor: remove redundunt condition
    (cherry picked from commit 257caef6895279c7760f6381a9775027b2dc2810)

--- india_compliance/gst_india/client_scripts/purchase_invoice.js
+++ india_compliance/gst_india/client_scripts/purchase_invoice.js
@@ -106,7 +106,12 @@ function toggle_reverse_charge(frm) {
     let is_read_only = 0;
     if (frm.doc.gst_category !== "Overseas") is_read_only = 0;
     // has_goods_item
-    else if (frm.doc.items.length > 0 && frm.doc.items.some(item => item?.item_code && item?.gst_hsn_code && !item.gst_hsn_code.startsWith("99")))
+    else if (
+        frm.doc.items.length > 0 &&
+        frm.doc.items.some(
+            item => item.gst_hsn_code && !item.gst_hsn_code.startsWith("99")
+        )
+    )
         is_read_only = 1;
 
     frm.set_df_property("is_reverse_charge", "read_only", is_read_only);
