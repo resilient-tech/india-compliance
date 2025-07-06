901cfad4e7f920b535dba0ed7b91f75f32dc410f
Smit Vora <smitvora203@gmail.com>
2025-07-06 14:45:01 +0530

    refactor: standard auth methods

--- india_compliance/gst_india/api_classes/nic/auth.py
+++ india_compliance/gst_india/api_classes/nic/auth.py
@@ -79,88 +79,101 @@ class StandardAuth(Auth):
         return self.client.session_expiry >= datetime.now()
 
     def _encrypt_request(self, request_args):
-        if not (data := request_args.get("json")):
+        """Encrypt request data using public key or session key based on content"""
+        json_data = request_args.get("json")
+        if not json_data:
             return
 
-        key = self.client.USER_NAME
-        if key in data:
-            data = frappe.as_json(data)
-            data = base64.b64encode(data.encode())
-
-            encrypted_data = encrypt_using_public_key(data, self._get_public_key())
+        # auth requests (contains USER_NAME) => use public key
+        if self.client.USER_NAME in json_data:
+            serialized_data = frappe.as_json(json_data)
+            encoded_data = base64.b64encode(serialized_data.encode())
+            encrypted_data = encrypt_using_public_key(
+                encoded_data, self._get_public_key()
+            )
 
         else:
-            encrypted_data = aes_encrypt_data(frappe.as_json(data), self.session_key)
+            # other requests => use session key
+            encrypted_data = aes_encrypt_data(
+                frappe.as_json(json_data), self.session_key
+            )
 
+        # update request
         request_args["json"] = {
             "Data": encrypted_data,
             **request_args.pop("params", {}),
         }
 
     def _decrypt_response(self, response):
-        key = self.client.DATA
-        data = response.get(key) or response
-        if not data:
+        """Route response to appropriate decryption method based on content"""
+        # get response data
+        response_data = response.get(self.client.DATA) or response
+        if not response_data:
             return
 
-        key = self.client.AUTH_TOKEN
+        # auth API responses contain auth token
+        is_auth_response = (
+            isinstance(response_data, dict) and self.client.AUTH_TOKEN in response_data
+        )
 
-        if isinstance(data, dict) and key in data:
-            self._decrypt_session_key(data)
-
+        if is_auth_response:
+            self._decrypt_session_key(response_data)
         else:
             self._decrypt_response_data(response)
 
     def _decrypt_session_key(self, response):
-        # For Auth API
+        """Decrypt and store authentication tokens from auth API response"""
         values = {}
-        key = self.client.AUTH_TOKEN
-        if response.get(key):
-            self.auth_token = response[key]
-            values["auth_token"] = response[key]
 
-        key = self.client.SEK
-        if response.get(key):
-            self.session_key = aes_decrypt_data(
-                response[key], base64.b64decode(self.client.app_key.encode())
-            )
+        # extract and store auth token
+        auth_token = response.get(self.client.AUTH_TOKEN)
+        if auth_token:
+            self.auth_token = auth_token
+            values["auth_token"] = auth_token
+
+        # decrypt and store session key
+        sek_data = response.get(self.client.SEK)
+        if sek_data:
+            app_key = base64.b64decode(self.client.app_key.encode())
+            self.session_key = aes_decrypt_data(sek_data, app_key)
             self.session_expiry = add_to_date(now_datetime(), hours=6)
 
             values["session_key"] = base64.b64encode(self.session_key).decode()
             values["session_expiry"] = self.session_expiry
 
+        # update credentials
         if values:
-            frappe.db.set_value(
-                "GST Credential",
-                {
-                    "gstin": self.client.company_gstin,
-                    "username": self.client.username,
-                    "service": "e-Waybill / e-Invoice",
-                },
-                values,
-            )
-
-            # cache of parent doctype GST Settings is not cleared by default so clear it manually
+            credential_filters = {
+                "gstin": self.client.company_gstin,
+                "username": self.client.username,
+                "service": "e-Waybill / e-Invoice",
+            }
+            frappe.db.set_value("GST Credential", credential_filters, values)
             frappe.clear_document_cache("GST Settings")
 
     def _decrypt_response_data(self, response):
-        # For Other APIs
-        decrypted_rek = None
+        """Decrypt response data from non-auth APIs and validate HMAC"""
+        # decrypt REK if present
+        rek_data = response.get(self.client.REK)
+        decrypted_rek = (
+            aes_decrypt_data(rek_data, self.session_key) if rek_data else None
+        )
 
-        key = self.client.REK
-        if response.get(key):
-            decrypted_rek = aes_decrypt_data(response[key], self.session_key)
-
-        key = self.client.DATA
-        if response.get(key) and isinstance(response[key], str):
+        # decrypt main response data
+        response_data = response.get(self.client.DATA)
+        if response_data and isinstance(response_data, str):
+            decryption_key = decrypted_rek or self.session_key
             decrypted_data = aes_decrypt_data(
-                response.pop(key), decrypted_rek or self.session_key
+                response.pop(self.client.DATA), decryption_key
             )
 
-            if response.get(self.client.HMAC):
-                hmac = aes_decrypt_data(base64.b64encode(decrypted_data), decrypted_rek)
-
-                if hmac != response[self.client.HMAC]:
+            # validate HMAC if present
+            expected_hmac = response.get(self.client.HMAC)
+            if expected_hmac:
+                computed_hmac = aes_decrypt_data(
+                    base64.b64encode(decrypted_data), decrypted_rek
+                )
+                if computed_hmac != expected_hmac:
                     frappe.throw(_("HMAC mismatch"))
 
             response.result = frappe.parse_json(decrypted_data.decode())
