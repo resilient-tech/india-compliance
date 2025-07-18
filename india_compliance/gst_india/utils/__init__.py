b67471820a0dc1867eaf0d33c7719b209d42459c
Karm Soni <karmdsoni8159@gmail.com>
2025-07-18 16:40:07 +0530

    fix: enhance permission error message to include page name
    (cherry picked from commit a2740f47571eba646aebdc535a03a906a8b61d29)

--- india_compliance/gst_india/utils/__init__.py
+++ india_compliance/gst_india/utils/__init__.py
@@ -1115,7 +1115,7 @@ def has_permission_of_page(page_name, throw=False):
             return False
 
         raise frappe.PermissionError(
-            _("You do not have permission to access this page.")
+            _("You do not have permission to access page: {0}").format(page_name)
         )
 
     return True
