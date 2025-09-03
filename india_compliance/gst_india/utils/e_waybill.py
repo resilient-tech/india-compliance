05dd216de3e0aea274cdaaba6947668824496c6c
Karm Soni <karmdsoni8159@gmail.com>
2025-09-03 12:11:32 +0530

    fix: refactor API instance creation in e-waybill generation for improved clarity
    (cherry picked from commit b64a82a523626cc4e508fe829feedf6929fb95ae)

--- india_compliance/gst_india/utils/e_waybill.py
+++ india_compliance/gst_india/utils/e_waybill.py
@@ -166,9 +166,12 @@ def _generate_e_waybill(doc, throw=True, force=False):
         data = EWaybillData(doc).get_data(with_irn=with_irn)
 
         api = EWaybillAPI if not with_irn else EInvoiceAPI
-        result = api.create(doc).generate_e_waybill(data)
+        api = api.create(doc)
+
+        result = api.generate_e_waybill(data)
 
         if result.error_code in ("3028", "3029"):
+            # if the code reaches here, than api will always be EInvoiceAPI instance
             gstin = GSTIN_FORMAT.search(result.message).group()
 
             response = api.sync_gstin_info(gstin)
@@ -177,7 +180,7 @@ def _generate_e_waybill(doc, throw=True, force=False):
                 frappe.throw(_("GSTIN {0} status is not Active").format(gstin))
 
         if result.error_code == "4002":
-            result = api.create(doc).get_e_waybill_by_irn(doc.get("irn"))
+            result = api.get_e_waybill_by_irn(doc.get("irn"))
 
         if result.error_code == "2148":
             with_irn = False
