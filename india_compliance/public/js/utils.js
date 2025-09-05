031d1ae277e04d83c44e2742e1636851eea2312e
Abdeali Chharchhoda <abdealiking786@gmail.com>
2025-09-05 17:32:03 +0530

    fix: set default value for force_update in set_gstin_status function
    (cherry picked from commit 6cc991411989055be50c89f45566515177f7f345)

--- india_compliance/public/js/utils.js
+++ india_compliance/public/js/utils.js
@@ -110,7 +110,7 @@ Object.assign(india_compliance, {
         return in_list(frappe.boot.sales_doctypes, doctype) ? "Customer" : "Supplier";
     },
 
-    async set_gstin_status(field, doc, force_update) {
+    async set_gstin_status(field, doc, force_update = false) {
         const gstin = field.value;
         if (!gstin || gstin.length !== 15) return field.set_description("");
 
