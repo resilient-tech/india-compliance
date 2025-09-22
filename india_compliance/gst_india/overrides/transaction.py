a2da7847c3c1f4cf807d436f439fcbdf05c958bd
ljain112 <ljain112@gmail.com>
2025-09-22 17:37:43 +0530

    chore: resolve conflicts

--- india_compliance/gst_india/overrides/transaction.py
+++ india_compliance/gst_india/overrides/transaction.py
@@ -1774,12 +1774,8 @@ def validate_transaction(doc, method=None):
     if ignore_gst_validations(doc):
         return False
 
-<<<<<<< HEAD
-=======
     set_gst_tax_type(doc)
-    validate_items(doc)
 
->>>>>>> e86d81ea (fix: move set_gst_tax_type from `before_validate` to `validate`)
     if doc.place_of_supply:
         validate_place_of_supply(doc)
     else:
