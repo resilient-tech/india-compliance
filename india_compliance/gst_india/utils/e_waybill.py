cebbe5ed40d07534004fcd5a830fd1682720fa19
Smit Vora <smitvora203@gmail.com>
2025-09-26 19:39:20 +0530

    fix: auto-suggest sub supply type as export
    (cherry picked from commit 565073f6a72c3823c60d149de5cc8cb7e9cdb7f7)

--- india_compliance/gst_india/utils/e_waybill.py
+++ india_compliance/gst_india/utils/e_waybill.py
@@ -1057,12 +1057,7 @@ def update_transaction(doc, values):
 
     doc.db_set(data)
 
-    if doc.doctype in (
-        "Delivery Note",
-        "Stock Entry",
-        "Subcontracting Receipt",
-        "Sales Invoice",
-    ):
+    if doc.doctype in ("Delivery Note", "Stock Entry", "Subcontracting Receipt"):
         doc._sub_supply_type = SUB_SUPPLY_TYPES[values.sub_supply_type]
     if doc.doctype in ("Delivery Note", "Stock Entry"):
         doc._sub_supply_desc = values.sub_supply_desc
@@ -1511,7 +1506,7 @@ class EWaybillData(GSTTransactionData):
             # Key: (doctype, is_return)
             ("Sales Invoice", 0): {
                 "supply_type": "O",
-                "sub_supply_type": doc.get("_sub_supply_type", ""),
+                "sub_supply_type": 1,  # Supply
                 "document_type": "INV",
             },
             ("Sales Invoice", 1): {
