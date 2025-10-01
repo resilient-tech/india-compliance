307bb4a72bee7bf30f4ef395469b967178311d7e
Karm Soni <karmdsoni8159@gmail.com>
2025-10-01 18:35:40 +0530

    fix: exclude specific document types from count in summary of Document Issued
    (cherry picked from commit fb70e1d38d160292b240618c1b31c6a5035066bc)
    
    # Conflicts:
    #	india_compliance/gst_india/doctype/gst_return_log/generate_gstr_1.py

--- india_compliance/gst_india/doctype/gst_return_log/generate_gstr_1.py
+++ india_compliance/gst_india/doctype/gst_return_log/generate_gstr_1.py
@@ -189,8 +189,22 @@ class SummarizeGSTR1:
 
     @staticmethod
     def count_doc_issue_summary(summary_row, data_row):
+<<<<<<< HEAD
         summary_row["no_of_records"] += data_row.get(df.TOTAL_COUNT, 0) - data_row.get(
             df.CANCELLED_COUNT, 0
+=======
+        if data_row.get(inv_f.DOC_TYPE) in (
+            "Excluded from Report (Invalid Invoice Number)",
+            "Excluded from Report (Same GSTIN Billing)",
+            "Excluded from Report (Is Opening Entry)",
+        ):
+            return
+
+        summary_row["no_of_records"] += (
+            data_row.get(inv_f.TOTAL_COUNT, 0)
+            - data_row.get(inv_f.CANCELLED_COUNT, 0)
+            - data_row.get(inv_f.DRAFT_COUNT, 0)
+>>>>>>> fb70e1d3 (fix: exclude specific document types from count in summary of Document Issued)
         )
 
     @staticmethod
