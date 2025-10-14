3ac84db24c310abbd151e9e6fae64ff499a5fe00
ljain112 <ljain112@gmail.com>
2025-10-14 13:46:19 +0530

    refactor: enhance ITC value setting for import rows in Excel export
    (cherry picked from commit 235a477dea4cf68d454b017f6647fb4c6cb0ba5b)

--- india_compliance/gst_india/doctype/gstr_3b_report/gstr_3b_report.py
+++ india_compliance/gst_india/doctype/gstr_3b_report/gstr_3b_report.py
@@ -1043,7 +1043,12 @@ class GSTR3BExcelExporter:
             itc_type = itc_entry.get("ty", "")
             if itc_type in self.ITC_AVAILABLE_TYPES:
                 row_key = self.ITC_AVAILABLE_TYPES[itc_type]
-                self._set_itc_values(row_key, itc_entry)
+
+                # Import of goods and services - columns D and E are read-only
+                if itc_type in ["IMPG", "IMPS"]:
+                    self._set_import_itc_values(row_key, itc_entry)
+                else:
+                    self._set_itc_values(row_key, itc_entry)
 
     def _set_itc_reversed_sections(self, itc_rev):
         """Set ITC Reversed sections using type-based mapping"""
@@ -1077,6 +1082,14 @@ class GSTR3BExcelExporter:
         """Set ITC values for Section 4"""
         row = self.ROWS[row_key]
         for key in ["iamt", "camt", "csamt"]:
+            if key in self.ITC_COLUMNS:
+                value = flt(data.get(key, 0), 2)
+                self._set_value(row, self.ITC_COLUMNS[key], value)
+
+    def _set_import_itc_values(self, row_key, data):
+        """Set ITC values for import rows (excludes columns D and E - camt)"""
+        row = self.ROWS[row_key]
+        for key in ["iamt", "csamt"]:  # Exclude camt (column D)
             if key in self.ITC_COLUMNS:
                 value = flt(data.get(key, 0), 2)
                 self._set_value(row, self.ITC_COLUMNS[key], value)
