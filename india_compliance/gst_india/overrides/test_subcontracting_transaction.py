491be47bab67cecd585f9cd01f7cc873e591a8a8
Karm Soni <karmdsoni8159@gmail.com>
2025-11-07 18:09:27 +0530

    fix(test): update subcontracting order total taxes assertion to check for zero
    (cherry picked from commit 8884f565fc46f90e09fd781c5591fb4455a669dc)

--- india_compliance/gst_india/overrides/test_subcontracting_transaction.py
+++ india_compliance/gst_india/overrides/test_subcontracting_transaction.py
@@ -251,7 +251,7 @@ class TestSubcontractingTransaction(FrappeTestCase):
         )
 
         sco = create_subcontracting_order(po_name=po.name)
-        self.assertEqual(sco.total_taxes, None)
+        self.assertEqual(sco.total_taxes, 0.0)
 
         rm_items = get_rm_items(sco.supplied_items)
         args = {
@@ -271,7 +271,7 @@ class TestSubcontractingTransaction(FrappeTestCase):
         se = make_stock_entry()
         se.save()
 
-        self.assertEqual(se.total_taxes, None)
+        self.assertEqual(se.total_taxes, 0.0)
 
     def test_subcontracting_validations(self):
         po = create_purchase_order(
