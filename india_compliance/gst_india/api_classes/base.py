876a93507582417937d20f0f8d25d0b4bf30f87e
Sagar Vora <16315650+sagarvora@users.noreply.github.com>
2025-11-12 12:04:15 +0530

    fix: try to log response content even if not JSON (#3787)
    (cherry picked from commit d5c8bc27b5b859ce17edd83755483cf80edcd0c2)

--- india_compliance/gst_india/api_classes/base.py
+++ india_compliance/gst_india/api_classes/base.py
@@ -141,6 +141,7 @@ class BaseAPI:
                     "body": json_data,
                 }
 
+        response = None
         response_json = None
 
         try:
@@ -186,6 +187,11 @@ class BaseAPI:
         finally:
             if response_json:
                 log.output = response_json.copy()
+            elif response:
+                log.output = {
+                    "status_code": response.status_code,
+                    "content": response.text,
+                }
 
             self.mask_sensitive_info(log)
 
