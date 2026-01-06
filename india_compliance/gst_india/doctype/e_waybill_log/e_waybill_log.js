137a34c7fe1089a9681819e0b5b5f3689120d142
Karm Soni <karmdsoni8159@gmail.com>
2026-01-06 15:11:16 +0530

    feat: add button to fetch latest e-Waybill data
    (cherry picked from commit 90958f6927a9c1f7c75c6825de8db550e231895a)

--- india_compliance/gst_india/doctype/e_waybill_log/e_waybill_log.js
+++ india_compliance/gst_india/doctype/e_waybill_log/e_waybill_log.js
@@ -1,8 +1,13 @@
 // Copyright (c) 2022, Resilient Tech and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on('e-Waybill Log', {
-	// refresh: function(frm) {
-
-	// }
-// });
+frappe.ui.form.on("e-Waybill Log", {
+    refresh: function (frm) {
+        frm.add_custom_button(__("Fetch Latest"), () =>
+            fetch_e_waybill_data(frm, { force: true }, () => {
+                frm.refresh();
+                frappe.show_alert(__("Latest e-Waybill fetched successfully"));
+            })
+        );
+    },
+});
