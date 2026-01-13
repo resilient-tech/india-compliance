1a0e4da9f79b12fdd9bee817793e80d4b54ff4fa
ljain112 <ljain112@gmail.com>
2026-01-13 14:17:13 +0530

    fix: codacy issues and import issue

--- .github/helper/setup_post_install_data.py
+++ .github/helper/setup_post_install_data.py
@@ -2,7 +2,7 @@
 import frappe
 import os
 from frappe.utils import get_bench_path
-from india_compliance.tests import setup_post_install_test_data
+
 
 def main():
     # Initialize frappe for the site
@@ -11,9 +11,12 @@ def main():
     frappe.init(site="test_site", sites_path=sites_path)
     frappe.connect()
 
+    from india_compliance.tests import setup_post_install_test_data
+
     print("Running setup for India Company...")
     setup_post_install_test_data()
     print("Setup complete.")
+
 
 if __name__ == "__main__":
     main()
