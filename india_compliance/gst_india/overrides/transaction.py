37c5838f6b32a1bd2dff509d3056cb5b85426c7d
ljain112 <ljain112@gmail.com>
2026-02-05 14:13:15 +0530

    chore: comment position
    (cherry picked from commit ea24fed155e11626a4539062fc27ed067b6ce2dc)

--- india_compliance/gst_india/overrides/transaction.py
+++ india_compliance/gst_india/overrides/transaction.py
@@ -971,12 +971,13 @@ def get_party_details_for_subcontracting(
     )
 
 
+# nosemgrep: frappe-semgrep-rules.rules.security.missing-argument-type-hint
 @frappe.whitelist()
 def get_gst_details(
     party_details: str | dict,
     doctype: str,
     company: str,
-    *,  # nosemgrep: frappe-semgrep-rules.rules.security.missing-argument-type-hint
+    *,
     update_place_of_supply: bool = False,
 ):
     """
