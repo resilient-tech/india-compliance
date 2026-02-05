9e5df13c5ef9451ce95e97e470dc7704a94264d9
ljain112 <ljain112@gmail.com>
2026-02-05 14:30:10 +0530

    chore: add type hint for doc parameter in handle_duplicate_irn_error function and scheduled_time parameter in schedule_ewaybill_for_extension function
    (cherry picked from commit 3fe2b214f99211ec071d18b4e07f2dfe5486a7b8)

--- india_compliance/gst_india/utils/e_waybill.py
+++ india_compliance/gst_india/utils/e_waybill.py
@@ -1,5 +1,6 @@
 import json
 import os
+from datetime import datetime
 
 import frappe
 from frappe import _
@@ -720,7 +721,7 @@ def validate_data_before_schedule(doc, values):
 
 @frappe.whitelist()
 def schedule_ewaybill_for_extension(
-    doctype: str, docname: str, values: str | dict, scheduled_time: str
+    doctype: str, docname: str, values: str | dict, scheduled_time: str | datetime
 ):
     """Permission check not required as load_doc checks permissions."""
     values = frappe.parse_json(values)
