fa15cea63f90fe801b468fe9a1969aca5fba0b84
ljain112 <ljain112@gmail.com>
2026-02-06 13:40:50 +0530

    fix: add project and other dimensions  to item gL entries in Bill of Entry
    (cherry picked from commit 23e71bb0535e8bd4f6ba4f66cc85b87b0b3c75c7)
    
    # Conflicts:
    #	india_compliance/gst_india/doctype/bill_of_entry/test_bill_of_entry.py

--- india_compliance/gst_india/doctype/bill_of_entry/test_bill_of_entry.py
+++ india_compliance/gst_india/doctype/bill_of_entry/test_bill_of_entry.py
@@ -229,3 +229,106 @@ class TestBillofEntry(FrappeTestCase):
         )
 
         boe.save()
+<<<<<<< HEAD
+=======
+
+    def test_pending_boe_qty(self):
+        pi = create_purchase_invoice(supplier="_Test Foreign Supplier", update_stock=1)
+
+        self.assertDocumentEqual(
+            {
+                "pending_boe_qty": 1,
+            },
+            pi.items[0],
+        )
+
+        # Create BOE
+        boe = make_bill_of_entry(pi.name)
+        boe.bill_of_entry_no = "123"
+        boe.bill_of_entry_date = today()
+        boe.save()
+        boe.submit()
+
+        pi.reload()
+        self.assertDocumentEqual(
+            {
+                "pending_boe_qty": 0,
+            },
+            pi.items[0],
+        )
+
+        boe.cancel()
+        pi.reload()
+
+        self.assertDocumentEqual(
+            {
+                "pending_boe_qty": 1,
+            },
+            pi.items[0],
+        )
+
+        # with partial boe
+        pi = create_purchase_invoice(
+            supplier="_Test Foreign Supplier", update_stock=1, qty=2
+        )
+        boe = make_bill_of_entry(pi.name)
+        boe.bill_of_entry_no = "123"
+        boe.bill_of_entry_date = today()
+        boe.items[0].qty = 1
+        boe.save()
+        boe.submit()
+
+        pi.reload()
+        self.assertDocumentEqual(
+            {
+                "pending_boe_qty": 1,
+            },
+            pi.items[0],
+        )
+
+        boe.cancel()
+        pi.reload()
+        self.assertDocumentEqual(
+            {
+                "pending_boe_qty": 2,
+            },
+            pi.items[0],
+        )
+
+    def test_project_in_gl_entries(self):
+        """Test that project from Purchase Invoice is auto-copied to Bill of Entry and passed to GL Entry"""
+        from erpnext.projects.doctype.project.test_project import make_project
+
+        project = make_project(
+            {
+                "project_name": "_Test BOE Project",
+                "company": "_Test Indian Registered Company",
+            }
+        )
+
+        pi = create_purchase_invoice(
+            supplier="_Test Foreign Supplier", update_stock=1, do_not_submit=True
+        )
+        pi.items[0].project = project.name
+        pi.submit()
+
+        boe = make_bill_of_entry(pi.name)
+        self.assertEqual(boe.items[0].project, project.name)
+
+        boe.items[0].customs_duty = 100
+        boe.bill_of_entry_no = "456"
+        boe.bill_of_entry_date = today()
+        boe.save()
+        boe.submit()
+
+        gl_entry = frappe.get_value(
+            "GL Entry",
+            {
+                "voucher_type": "Bill of Entry",
+                "voucher_no": boe.name,
+                "account": boe.customs_expense_account,
+            },
+            "project",
+        )
+        self.assertEqual(gl_entry, project.name)
+>>>>>>> 23e71bb0 (fix: add project and other dimensions  to item gL entries in Bill of Entry)
