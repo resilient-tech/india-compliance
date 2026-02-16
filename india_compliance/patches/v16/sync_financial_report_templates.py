709b8f99ee608eb82ba7be80a58a86da4611649a
Abdeali Chharchhoda <abdealiking786@gmail.com>
2026-02-16 14:58:04 +0530

    fix: patch for financial report templates

---
+++ india_compliance/patches/v16/sync_financial_report_templates.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2025, Frappe Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+
+from erpnext.accounts.doctype.financial_report_template.financial_report_template import (
+    _sync_templates_for as sync_financial_report_templates,
+)
+
+from india_compliance.hooks import app_name
+
+
+def execute():
+    sync_financial_report_templates(app_name)
