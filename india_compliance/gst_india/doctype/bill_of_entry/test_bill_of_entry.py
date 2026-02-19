4190dcc9e5878c98c0ff4f96a56e4121ec8a20b5
ljain112 <ljain112@gmail.com>
2026-02-19 18:37:44 +0530

    test: fix correct name
    (cherry picked from commit 8030d0a92cf287737a1cc9d0512773c9e00acc49)

--- india_compliance/gst_india/doctype/bill_of_entry/test_bill_of_entry.py
+++ india_compliance/gst_india/doctype/bill_of_entry/test_bill_of_entry.py
@@ -336,7 +336,7 @@ class TestBillofEntry(FrappeTestCase):
         pi2 = create_purchase_invoice(
             supplier="_Test Foreign Supplier", update_stock=1, do_not_submit=True
         )
-        pi2.project = project.name
+        pi2.project = project
         pi2.items[0].project = None
         pi2.submit()
 
