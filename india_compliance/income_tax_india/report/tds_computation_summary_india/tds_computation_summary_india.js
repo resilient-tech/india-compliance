b3b565fc10054a49773e57c08f72328f1b46cad4
ljain112 <ljain112@gmail.com>
2026-04-20 18:27:23 +0530

    feat: add Tax Withholding Details and TDS Computation Summary reports for India

---
+++ india_compliance/income_tax_india/report/tds_computation_summary_india/tds_computation_summary_india.js
@@ -0,0 +1,7 @@
+// Copyright (c) 2026, Resilient Tech and contributors
+// For license information, please see license.txt
+
+/* eslint-disable */
+{% include "erpnext/accounts/report/tds_computation_summary/tds_computation_summary.js" %}
+
+frappe.query_reports["TDS Computation Summary-India"] = frappe.query_reports["TDS Computation Summary"];
