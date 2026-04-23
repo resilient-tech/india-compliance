226c0ca71c9d6011671b94f63236eb79ddd9a6d2
ljain112 <ljain112@gmail.com>
2026-04-23 19:01:47 +0530

    fix: correct flag position

--- india_compliance/public/js/utils.js
+++ india_compliance/public/js/utils.js
@@ -470,8 +470,6 @@ Object.assign(india_compliance, {
 
         await frappe.after_ajax();
 
-        frm.__updating_itc_claim_period = false;
-
         const { message: valid_periods } = await frappe.call({
             method: "india_compliance.gst_india.utils.itc_claim.get_itc_period_options",
             args: {
@@ -479,6 +477,8 @@ Object.assign(india_compliance, {
                 posting_date: frm.doc.posting_date,
             },
         });
+
+        frm.__updating_itc_claim_period = false;
 
         const current_period = frm.doc.itc_claim_period;
         if (current_period && valid_periods?.includes(current_period)) return;
