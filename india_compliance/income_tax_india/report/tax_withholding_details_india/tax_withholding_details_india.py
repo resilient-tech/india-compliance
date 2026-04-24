3a341ee848f9ee6ef8968411b63544ad6f902eff
ljain112 <ljain112@gmail.com>
2026-04-24 11:19:40 +0530

    refactor: changes as per erpnext

--- india_compliance/income_tax_india/report/tax_withholding_details_india/tax_withholding_details_india.py
+++ india_compliance/income_tax_india/report/tax_withholding_details_india/tax_withholding_details_india.py
@@ -1,37 +1,69 @@
 # Copyright (c) 2026, Resilient Tech and contributors
 # For license information, please see license.txt
 
+import frappe
 from erpnext.accounts.report.tax_withholding_details.tax_withholding_details import (
-    _execute,
+    execute as _execute,
 )
 from frappe import _
 
+INDIA_COLUMNS = [
+    {
+        "label": _("New Income Tax Section"),
+        "fieldname": "tds_section",
+        "fieldtype": "Data",
+        "width": 120,
+    },
+    {
+        "label": _("Old Income Tax Section"),
+        "fieldname": "old_income_tax_section",
+        "fieldtype": "Data",
+        "width": 150,
+    },
+    {
+        "label": _("Entity Type"),
+        "fieldname": "entity_type",
+        "fieldtype": "Data",
+        "width": 120,
+    },
+]
+
+
+def update_columns(columns):
+    columns[1:1] = INDIA_COLUMNS
+
+    return columns
+
+
+def update_data(data):
+    twc_info = _get_twc_info(data)
+    for row in data:
+        twc = twc_info.get(row.get("tax_withholding_category"), {})
+        row.update(**twc)
+
+    return data
+
+
+def _get_twc_info(data):
+    categories = {row.get("tax_withholding_category") for row in data if row.get("tax_withholding_category")}
+    if not categories:
+        return {}
+
+    fields = [row.fieldname for row in INDIA_COLUMNS]
+
+    twc = frappe.qb.DocType("Tax Withholding Category")
+    rows = (
+        frappe.qb.from_(twc)
+        .select(twc.name, *[twc[field] for field in fields])
+        .where(twc.name.isin(categories))
+        .run(as_dict=True)
+    )
+    return {row.pop("name"): row for row in rows}
+
 
 def execute(filters=None):
-    return _execute(filters, additional_table_columns=get_additional_table_columns())
+    _columns, _data = _execute(filters)
+    data = update_data(_data)
+    columns = update_columns(_columns)
 
-
-def get_additional_table_columns():
-    return [
-        {
-            "label": _("New Income Tax Section"),
-            "fieldname": "tds_section",
-            "fieldtype": "Data",
-            "width": 120,
-            "_doctype": "Tax Withholding Category",
-        },
-        {
-            "label": _("Old Income Tax Section"),
-            "fieldname": "old_income_tax_section",
-            "fieldtype": "Data",
-            "width": 150,
-            "_doctype": "Tax Withholding Category",
-        },
-        {
-            "label": _("Entity Type"),
-            "fieldname": "entity_type",
-            "fieldtype": "Data",
-            "width": 120,
-            "_doctype": "Tax Withholding Category",
-        },
-    ]
+    return columns, data
--- india_compliance/income_tax_india/report/tds_computation_summary_india/tds_computation_summary_india.py
+++ india_compliance/income_tax_india/report/tds_computation_summary_india/tds_computation_summary_india.py
@@ -2,13 +2,17 @@
 # For license information, please see license.txt
 
 from erpnext.accounts.report.tds_computation_summary.tds_computation_summary import (
-    _execute,
+    execute as _execute,
 )
 
 from india_compliance.income_tax_india.report.tax_withholding_details_india.tax_withholding_details_india import (
-    get_additional_table_columns,
+    update_columns,
+    update_data,
 )
 
 
 def execute(filters=None):
-    return _execute(filters, additional_table_columns=get_additional_table_columns())
+    _columns, _data = _execute(filters)
+    data = update_data(_data)
+    columns = update_columns(_columns)
+    return columns, data
