0c7fe8d0e93529639074befeb5ceb057f30c460d
ljain112 <ljain112@gmail.com>
2026-05-01 16:02:25 +0530

    fix: auto set company gstin in jv
    (cherry picked from commit 9ff34eebb59bce2f821d6e4e589f0f32471e5a3a)

--- india_compliance/gst_india/overrides/journal_entry.py
+++ india_compliance/gst_india/overrides/journal_entry.py
@@ -4,7 +4,7 @@ from frappe import _
 from india_compliance.gst_india.overrides.transaction import (
     is_indian_registered_company,
 )
-from india_compliance.gst_india.utils import get_gst_account_gst_tax_type_map
+from india_compliance.gst_india.utils import get_gst_account_gst_tax_type_map, get_gstin_list
 
 
 def set_gst_tax_type(doc, method=None):
@@ -26,14 +26,18 @@ def validate(doc, method=None):
 
     set_gst_tax_type(doc)
 
-    if doc.company_gstin:
-        return
+    if not doc.company_gstin and has_gst_accounts(doc):
+        set_or_validate_company_gstin(doc)
 
-    contains_gst_account = False
-    for row in doc.accounts:
-        if row.gst_tax_type:
-            contains_gst_account = True
-            break
 
-    if contains_gst_account:
+def set_or_validate_company_gstin(doc):
+    gstin_list = get_gstin_list(doc.company)
+
+    if len(gstin_list) == 1:
+        doc.company_gstin = gstin_list[0]
+    else:
         frappe.throw(_("Company GSTIN is mandatory if any GST account is present."))
+
+
+def has_gst_accounts(doc):
+    return any(row.gst_tax_type for row in doc.accounts)
