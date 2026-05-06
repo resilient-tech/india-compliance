c909395986f949f021f23ef288215608a8a87537
ljain112 <ljain112@gmail.com>
2026-05-06 15:27:37 +0530

    fix: correct report name for tax withholding details

--- india_compliance/income_tax_india/report/tax_withholding_details_india/tax_withholding_details_india.js
+++ india_compliance/income_tax_india/report/tax_withholding_details_india/tax_withholding_details_india.js
@@ -2,6 +2,6 @@
 // For license information, please see license.txt
 
 /* eslint-disable */
-{% include "erpnext/accounts/report/tax_withholding_details/tax_withholding_details.js" %}
+{% include "erpnext/accounts/report/tds_payable_monthly/tds_payable_monthly.js" %}
 
-frappe.query_reports["Tax Withholding Details-India"] = frappe.query_reports["Tax Withholding Details"];
+frappe.query_reports["Tax Withholding Details-India"] = frappe.query_reports["TDS Payable Monthly"];
