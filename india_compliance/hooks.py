aa946d51aca1478ece792f28dec7a641b6753dea
ljain112 <ljain112@gmail.com>
2026-05-14 19:03:49 +0530

    fix: add after_mapping event for delivery note and purchase receipt for inter company transactions
    (cherry picked from commit e0737b74571072beb5b173a2707affe82ed9073f)
    
    # Conflicts:
    #	india_compliance/gst_india/overrides/test_transaction.py

--- india_compliance/hooks.py
+++ india_compliance/hooks.py
@@ -134,6 +134,7 @@ doc_events = {
         "before_submit": "india_compliance.gst_india.overrides.transaction.update_gst_details",
         "before_cancel": "india_compliance.gst_india.utils.e_waybill.before_cancel",
         "validate": "india_compliance.gst_india.overrides.delivery_note.validate",
+        "after_mapping": "india_compliance.gst_india.overrides.transaction.after_mapping",
     },
     "Email Template": {
         "after_rename": "india_compliance.gst_india.overrides.email_template.after_rename",
@@ -203,6 +204,7 @@ doc_events = {
             "india_compliance.gst_india.overrides.transaction.update_gst_details",
         ],
         "before_cancel": "india_compliance.gst_india.utils.e_waybill.before_cancel",
+        "after_mapping": "india_compliance.gst_india.overrides.transaction.after_mapping",
     },
     "Sales Invoice": {
         "onload": [
