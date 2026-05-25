37d0380886e1229d32e239b7a4eb112c7653d505
ljain112 <ljain112@gmail.com>
2026-05-25 18:35:16 +0530

    fix: remove unused update_onload function call in validate_transaction because it gets reset
    (cherry picked from commit aea217dc57d3802ac7372d073e9662c45419152c)

--- india_compliance/gst_india/overrides/transaction.py
+++ india_compliance/gst_india/overrides/transaction.py
@@ -35,7 +35,6 @@ from india_compliance.gst_india.utils import (
     has_gst_taxes,
     is_overseas_doc,
     join_list_with_custom_separators,
-    update_onload,
     validate_gst_category,
     validate_gstin,
 )
@@ -1662,7 +1661,6 @@ def validate_transaction(doc, method=None):
     update_taxable_values(doc)
     validate_item_wise_tax_detail(doc)
     set_gst_breakup(doc)
-    update_onload(doc, "_gst_breakup_table", doc.gst_breakup_table)
 
 
 def before_print(doc, method=None, print_settings=None):
