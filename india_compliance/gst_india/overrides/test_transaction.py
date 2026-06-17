998e0f4b098e6d068fed33bb9e067f91140a3ed2
ljain112 <ljain112@gmail.com>
2026-06-17 12:37:30 +0530

    chore: resolve conflicts

--- india_compliance/gst_india/overrides/test_transaction.py
+++ india_compliance/gst_india/overrides/test_transaction.py
@@ -29,12 +29,8 @@ from india_compliance.gst_india.overrides.transaction import (
     ADDRESS_DEPENDENT_FIELDS,
     DOCTYPES_WITH_GST_DETAIL,
     ItemGSTDetails,
-<<<<<<< HEAD
-=======
-    _is_multicurrency_doc,
     sync_address_dependent_fields_on_submit,
     sync_gst_details_from_address,
->>>>>>> a40dc37e (fix: allow editing address/GSTIN fields on submitted doctypes (#4300))
     validate_gst_refund_accounts,
     validate_item_tax_template,
 )
