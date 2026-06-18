c9780eac42cb2651ec79ba3a2e8653261ba7bbf2
ljain112 <ljain112@gmail.com>
2026-06-18 14:55:15 +0530

    fix: implement tax category validation  and ux
    (cherry picked from commit ca2523d8532a81661ef30621c09036bd5ac66c65)

--- india_compliance/gst_india/overrides/tax_category.py
+++ india_compliance/gst_india/overrides/tax_category.py
@@ -3,9 +3,30 @@ from frappe import _
 
 
 def validate(doc, method=None):
-    if doc.get("gst_state") and frappe.db.get_value(
+    if doc.get("is_india_compliance_default") and doc.get("gst_state"):
+        frappe.throw(_("India Compliance Default Tax Category cannot have a Source State"))
+
+    if doc.get("is_india_compliance_default"):
+        if frappe.db.exists(
+            "Tax Category",
+            {
+                "name": ["!=", doc.name],
+                "is_india_compliance_default": 1,
+                "is_inter_state": doc.is_inter_state,
+                "is_reverse_charge": doc.is_reverse_charge,
+            },
+        ):
+            frappe.throw(
+                _(
+                    "An India Compliance Default Tax Category already exists for this"
+                    " Inter State / Reverse Charge combination"
+                )
+            )
+
+    elif doc.get("gst_state") and frappe.db.exists(
         "Tax Category",
         {
+            "name": ["!=", doc.name],
             "gst_state": doc.gst_state,
             "is_inter_state": doc.is_inter_state,
             "is_reverse_charge": doc.is_reverse_charge,
