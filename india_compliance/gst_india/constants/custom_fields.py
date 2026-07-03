a96ea34291a6070afc06ec6840b864771b46037a
Lakshit Jain <ljain112@gmail.com>
2026-07-03 11:12:03 +0530

    feat: e-waybill support for Subcontracting Inward (#3822)
    closes: https://github.com/resilient-tech/india-compliance/issues/3765
    closes: https://github.com/frappe/erpnext/issues/50686
    
    ### Todo
    
    - [x] Test Cases
    - [x] Documentation
    
    Adds e-Waybill generation support for new ERPNext v16 Stock Entry purposes related to the Subcontracting Inward feature:
    - Subcontracting Delivery - Delivering finished goods back to the customer after job work
    - Return Raw Material to Customer - Returning unused raw materials to the customer
    - Currently, not allowing receive form customer
    
    Changes:
    
    - Enable e-Waybill applicability for new Stock Entry purposes
    - Add sub supply type options (Others) with appropriate descriptions
    - Add address field mapping for customer billing
    - Add the `additional_taxable_value` field to the Stock Entry Detail for calculating taxable value
    - Calculate customer-provided material value for GST taxable value computation
    
    docs: https://github.com/resilient-tech/india-compliance-docs/pull/70
    (cherry picked from commit a8439f8da85dc3cc408271fa25ce738940bb4469)

--- india_compliance/gst_india/constants/custom_fields.py
+++ india_compliance/gst_india/constants/custom_fields.py
@@ -746,6 +746,20 @@ CUSTOM_FIELDS = {
             "print_hide": 1,
             "hidden": 0,
         },
+        {
+            "fieldname": "additional_taxable_value",
+            "label": "Additional Taxable Value",
+            "fieldtype": "Currency",
+            "insert_after": "taxable_value",
+            "options": "Company:company:default_currency",
+            "read_only": 1,
+            "translatable": 0,
+            "no_copy": 1,
+            "print_hide": 1,
+            "hidden": 0,
+            "depends_on": "eval:india_compliance.SUBCONTRACTING_INWARD_PURPOSES.includes(parent.purpose)",
+            "description": "Value of customer-provided materials for Subcontracting Inward",
+        },
     ],
     "Subcontracting Receipt Item": [
         {
