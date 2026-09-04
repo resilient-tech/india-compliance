4e52a98f46a3460d9ab3644f0d28146e2a9d73d6
Karm Soni <karmdsoni8159@gmail.com>
2026-09-04 17:22:43 +0530

    fix: validate party type in GSTIN Status report

--- india_compliance/gst_india/report/gstin_status/gstin_status.py
+++ india_compliance/gst_india/report/gstin_status/gstin_status.py
@@ -24,7 +24,15 @@ def execute(filters: dict | None = None):
 class GSTINDetailedReport:
     def __init__(self, filters: dict | None = None):
         self.filters = frappe._dict(filters or {})
+        if self.filters.party_type and self.filters.party_type not in ["Customer", "Supplier"]:
+            frappe.throw(
+                _("Party Type must be either 'Customer' or 'Supplier'. You provided: {0}").format(
+                    self.filters.party_type
+                )
+            )
+
         self.doctypes = [self.filters.party_type] if self.filters.party_type else ["Customer", "Supplier"]
+
         self.is_naming_series = "Naming Series" in (
             frappe.db.get_single_value("Buying Settings", "supp_master_name"),
             frappe.db.get_single_value("Selling Settings", "cust_master_name"),
