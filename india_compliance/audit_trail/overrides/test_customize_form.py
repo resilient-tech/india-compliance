8c9bd7585586006e52f61edef9f679ccdab47148
Abdeali Chharchhoda <abdealiking786@gmail.com>
2026-09-10 16:26:17 +0530

    test: cover Ignore Versioning guard

--- india_compliance/audit_trail/overrides/test_customize_form.py
+++ india_compliance/audit_trail/overrides/test_customize_form.py
@@ -20,6 +20,15 @@ class TestCustomizeFormAuditTrail(IntegrationTestCase):
         )
         frappe.db.set_single_value("Accounts Settings", "enable_audit_trail", 0)
 
+    def test_audit_trail_enabled_onload_covers_child_tables(self):
+        frappe.db.set_single_value("Accounts Settings", "enable_audit_trail", 1)
+        self.addCleanup(frappe.db.set_single_value, "Accounts Settings", "enable_audit_trail", 0)
+
+        # versioning enforcement covers child tables, unlike `track_changes`
+        self.assertTrue(self.get_customize_form("Sales Invoice").get_onload("audit_trail_enabled"))
+        self.assertTrue(self.get_customize_form("Sales Invoice Item").get_onload("audit_trail_enabled"))
+        self.assertFalse(self.get_customize_form("ToDo").get_onload("audit_trail_enabled"))
+
     def get_customize_form(self, doctype=None):
         d = frappe.get_doc("Customize Form")
         if doctype:
