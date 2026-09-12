1102255dd3b20d50e5d646db4c65cc5e63b549c9
Abdeali Chharchhoda <abdealiking786@gmail.com>
2026-09-12 18:12:16 +0530

    refactor: resolve Audit Trail child DocTypes lazily

--- india_compliance/audit_trail/utils.py
+++ india_compliance/audit_trail/utils.py
@@ -7,30 +7,44 @@ def is_audit_trail_enabled():
     return bool(frappe.db.get_single_value("Accounts Settings", "enable_audit_trail"))
 
 
-@frappe.whitelist()
-def get_audit_trail_doctypes(include_child: bool = False):
+def get_audit_trail_doctypes(include_child: bool = False) -> set[str]:
+    return set(iter_audit_trail_doctypes(include_child=include_child))
+
+
+def iter_audit_trail_doctypes(include_child: bool = False):
     doctypes = set(frappe.get_hooks("audit_trail_doctypes"))
+    yield from doctypes
 
     if not include_child:
-        return doctypes
+        return
 
-    child_doctypes = {
-        df.options for doctype in doctypes for df in frappe.get_meta(doctype).get_table_fields()
-    }
+    for doctype in doctypes:
+        yield from get_child_doctypes(doctype)
 
-    return doctypes | child_doctypes
+
+def get_child_doctypes(doctype: str):
+    meta = frappe.get_meta(doctype)
+    if meta.istable:
+        return ()
+
+    return meta._non_computed_table_doctypes.values()
 
 
 def is_audit_trail_enabled_for(doctype: str) -> bool:
-    return is_audit_trail_enabled() and doctype in get_audit_trail_doctypes(include_child=True)
+    if not is_audit_trail_enabled():
+        return False
+
+    # Check configured DocTypes before resolving their child tables
+    return doctype in iter_audit_trail_doctypes(include_child=True)
 
 
 def throw_cannot_enable_ignore_versioning(doctype: str):
     frappe.throw(
         _(
-            "Cannot enable Ignore Versioning for {0}, since versioning is required to maintain Audit Trail"
+            "Cannot enable Ignore Versioning for fields in {0}, since versioning is required"
+            " to maintain Audit Trail"
         ).format(frappe.bold(_(doctype))),
-        title=_("Audit Trail"),
+        title=_("Audit Trail Restriction"),
     )
 
 
