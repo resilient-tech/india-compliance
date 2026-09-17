c73295e8f07951105eb8c08d6b95d2c2300599fc
Anish <anish31442@gmail.com>
2026-09-17 14:58:23 +0530

    refactor: update sync button visibility and styling in invoice detail comparison

--- india_compliance/public/js/reconciliation_components/tabs.js
+++ india_compliance/public/js/reconciliation_components/tabs.js
@@ -567,7 +567,7 @@ reconciliation.detail_view_dialog = class DetailViewDialog {
 
             $(row)
                 .find("[data-field]")
-                .addClass("sync-detail")
+                .removeClass("d-none")
                 .on("click", () => this._sync_field(field));
         });
     }
