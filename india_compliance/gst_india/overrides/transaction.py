0afa3edfd84e2663d419e8e3733fc6c629b921b5
ljain112 <ljain112@gmail.com>
2026-09-21 12:34:20 +0530

    fix: allow rcm and forward transaction both for OIDAR

--- india_compliance/gst_india/overrides/transaction.py
+++ india_compliance/gst_india/overrides/transaction.py
@@ -8,7 +8,7 @@ from frappe import _, bold
 from frappe.contacts.doctype.address.address import get_default_address
 from frappe.model.meta import get_field_precision
 from frappe.model.utils import get_fetch_values
-from frappe.utils import cint, flt, format_date
+from frappe.utils import cint, flt, format_date, get_link_to_form
 
 from india_compliance.gst_india.constants import (
     CUSTOM_ADDRESS_FIELDS_DOCTYPES,
@@ -44,7 +44,6 @@ from india_compliance.gst_india.utils import (
     has_gst_taxes,
     is_import_transaction,
     is_inward_transaction,
-    is_oidar_gstin,
     is_overseas_doc,
     is_same_gstin_allowed,
     join_list_with_custom_separators,
@@ -403,25 +402,12 @@ class GSTAccounts:
                 ).format(self.first_gst_idx)
             )
 
-        if not self.doc.is_reverse_charge:
-            if not self.doc.supplier_gstin:
-                self._throw(
-                    _("Cannot charge GST in Row #{0} since purchase is from a Supplier without GSTIN").format(
-                        self.first_gst_idx
-                    )
+        if not self.doc.is_reverse_charge and not self.doc.supplier_gstin:
+            self._throw(
+                _("Cannot charge GST in Row #{0} since purchase is from a Supplier without GSTIN").format(
+                    self.first_gst_idx
                 )
-
-            # An OIDAR supplier is a non-resident registered under the Simplified Registration
-            # Scheme. Tax on such supplies to a registered recipient is payable by the recipient
-            # under Reverse Charge, so Input GST cannot be charged by the supplier.
-            if is_oidar_gstin(self.doc.supplier_gstin):
-                self._throw(
-                    _(
-                        "Cannot charge GST in Row #{0} since the Supplier is registered as a"
-                        " Non-Resident Online Services Provider. Tax on such supplies is payable"
-                        " under Reverse Charge. Please enable Reverse Charge for this transaction."
-                    ).format(self.first_gst_idx)
-                )
+            )
 
     def validate_for_invalid_account_type(self):
         """
@@ -898,13 +884,7 @@ def get_gst_details(
             not is_sales_transaction
             and (
                 party_details.get(gst_category_field) == "Registered Composition"
-                or (
-                    not party_details.is_reverse_charge
-                    and (
-                        not party_details.get(party_gstin_field)
-                        or is_oidar_gstin(party_details.get(party_gstin_field))
-                    )
-                )
+                or (not party_details.is_reverse_charge and not party_details.get(party_gstin_field))
             )
         )
     ):
